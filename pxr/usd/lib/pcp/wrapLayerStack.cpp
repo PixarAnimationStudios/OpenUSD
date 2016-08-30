392e7ed72f4cff93afb01af6df32880951139f32
blevin <blevin@users.noreply.github.com>
2016-08-30 10:07:38 -0700

    Pcp: fix relocates update bug
    PcpLayerStack already records the paths where it discovered
    relocation opinions.  Use that to invalidate the relocations
    when doing a significant prim resync.
    
    (Internal change: 1650665)

--- pxr/usd/lib/pcp/wrapLayerStack.cpp
+++ pxr/usd/lib/pcp/wrapLayerStack.cpp
@@ -71,29 +71,27 @@ void wrapLayerStack()
         .add_property("layers", 
                       make_function(&_GetLayerStackLayers,
                                     return_value_policy<TfPySequenceToList>()))
-
         .add_property("layerOffsets",
                       make_function(&_GetLayerOffsets,
                                     return_value_policy<TfPySequenceToList>()))
-
         .add_property("layerTree", 
                       make_function(&PcpLayerStack::GetLayerTree,
                                     return_value_policy<return_by_value>()))
-
         .add_property("resolvedAssetPaths",
                       make_function(&_GetResolvedAssetPaths,
                                     return_value_policy<TfPySequenceToList>()))
-
         .add_property("relocatesSourceToTarget",
                       make_function(&PcpLayerStack::GetRelocatesSourceToTarget,
                                     return_value_policy<return_by_value>()))
-
         .add_property("relocatesTargetToSource",
                       make_function(&PcpLayerStack::GetRelocatesTargetToSource,
                                     return_value_policy<return_by_value>()))
         .add_property("localErrors", 
                       make_function(&PcpLayerStack::GetLocalErrors,
                                     return_value_policy<TfPySequenceToList>()))
+        .add_property("pathsToPrimsWithRelocates", 
+              make_function(&PcpLayerStack::GetPathsToPrimsWithRelocates,
+                            return_value_policy<TfPySequenceToList>()))
         // TODO: repr, eq, etc.
         ;
 }
