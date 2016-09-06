c68f28c2af70594d7c3947b3420779055c25bc3f
Jamie Kenyon <jamie.kenyon@thefoundry.co.uk>
2016-09-06 18:17:59 +0100

    Make garch work with multiple contexts.

--- pxr/imaging/lib/garch/glPlatformContextWindows.cpp
+++ pxr/imaging/lib/garch/glPlatformContextWindows.cpp
@@ -10,19 +10,11 @@
 // GarchGLWContextState
 //
 
-static HDC g_device;
-static HGLRC g_context;
-
 GarchGLWContextState::GarchGLWContextState() :
+	device(wglGetCurrentDC()),
+	context(wglGetCurrentContext()),
    _defaultCtor(true)
 {
-    if (!g_device)
-        g_device = wglGetCurrentDC();
-
-    if (!g_context)
-        g_context =  wglGetCurrentContext();
-    context = g_context;
-    device = g_device;
 }
 
 GarchGLWContextState::GarchGLWContextState(HDC device_, HGLRC context_):
