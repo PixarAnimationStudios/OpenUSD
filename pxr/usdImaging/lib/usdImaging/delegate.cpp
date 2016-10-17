eca6ca8209bdac34d0c78eaa3f3507ba0737cafe
c64kernal <c64kernal@users.noreply.github.com>
2016-10-17 15:25:05 -0700

    Remove build dependencies on GL, GLU and GLEW from usdImaging.
    (Internal change: 1663896)

--- pxr/usdImaging/lib/usdImaging/delegate.cpp
+++ pxr/usdImaging/lib/usdImaging/delegate.cpp
@@ -21,8 +21,6 @@
 // KIND, either express or implied. See the Apache License for the specific
 // language governing permissions and limitations under the Apache License.
 //
-#include "pxr/imaging/glf/glew.h"
-
 #include "pxr/usdImaging/usdImaging/delegate.h"
 
 #include "pxr/usdImaging/usdImaging/adapterRegistry.h"
