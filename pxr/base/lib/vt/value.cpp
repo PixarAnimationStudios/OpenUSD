b7ea4ead678130dd5e6630d6b4c6aa22fe1b29a3
jloy <jloy@users.noreply.github.com>
2017-01-13 13:00:33 -0800

    vt: slightly reduce overhead of cast registry.
    Use std::type_index instead of constructing std::strings from the
    type_info's name and store conversions in a single map keyed
    by (src, dst) type pairs instead of nested maps.
    
    Because the cast registry is never erased from, we can simply
    use concurrent_unordered_map to support concurrent clients.
    A spin rw lock with a normal hash map might be faster but I
    don't currently have strong evidence pointing one way or
    the other.
    
    (Internal change: 1703401)

--- pxr/base/lib/vt/value.cpp
+++ pxr/base/lib/vt/value.cpp
@@ -39,11 +39,12 @@
 #include <boost/preprocessor.hpp>
 #include <boost/numeric/conversion/cast.hpp>
 #include <tbb/spin_mutex.h>
+#include <tbb/concurrent_unordered_map.h>
 
 #include <iostream>
 #include <map>
-#include <mutex>
 #include <string>
+#include <typeindex>
 #include <typeinfo>
 #include <vector>
 
@@ -84,54 +85,42 @@ class Vt_CastRegistry {
                   type_info const &to,
                   VtValue (*castFn)(VtValue const &))
     {
-        string const &dst = to.name();
-        string const &src = from.name();
+        std::type_index src = from;
+        std::type_index dst = to;
 
-        std::lock_guard<std::mutex> lock(_mutex);
-        _Conversions::iterator c = _conversions.find(dst);
-        if (c != _conversions.end()) {
-            _CastMap::iterator m = c->second.find(src);
-            if (m != c->second.end()) {
-                // This happens at startup if there's a bug in the code.
-                TF_CODING_ERROR("VtValue cast already registered from "
-                                "'%s' to '%s'.  New cast will be ignored.",
-                                ArchGetDemangled(src).c_str(),
-                                ArchGetDemangled(dst).c_str());
-                return;
-            }
+        bool isNewEntry = _conversions.insert(
+            std::make_pair(_ConversionSourceToTarget(src, dst), castFn)).second;
+        if (!isNewEntry) {
+            // This happens at startup if there's a bug in the code.
+            TF_CODING_ERROR("VtValue cast already registered from "
+                            "'%s' to '%s'.  New cast will be ignored.",
+                            ArchGetDemangled(from).c_str(),
+                            ArchGetDemangled(to).c_str());
+            return;
         }
-        _conversions[dst][src] = castFn;
     }
 
     VtValue PerformCast(type_info const &to, VtValue const &val) {
         if (val.IsEmpty())
             return val;
 
-        string const &dst = to.name();
-        string const &src = val.GetTypeid().name();
+        std::type_index src = val.GetTypeid();
+        std::type_index dst = to;
 
         VtValue (*castFn)(VtValue const &) = NULL;
-        {
-            std::lock_guard<std::mutex> lock(_mutex);
-            _Conversions::iterator c = _conversions.find(dst);
-            if (c != _conversions.end()) {
-                _CastMap::iterator m = c->second.find(src);
-                if (m != c->second.end()) {
-                    castFn = m->second;
-                }
-            }
+
+        _Conversions::iterator c = _conversions.find({src, dst});
+        if (c != _conversions.end()) {
+            castFn = c->second;
         }
         return castFn ? castFn(val) : VtValue();
     }
 
     bool CanCast(type_info const &from, type_info const &to) {
-        string const &dst = to.name();
-        string const &src = from.name();
+        std::type_index src = from;
+        std::type_index dst = to;
 
-        std::lock_guard<std::mutex> lock(_mutex);
-        _Conversions::iterator c = _conversions.find(dst);
-        return c != _conversions.end() &&
-            c->second.find(src) != c->second.end();
+        return _conversions.find({src, dst}) != _conversions.end();
     }
 
   private:
@@ -275,11 +264,25 @@ class Vt_CastRegistry {
         VtValue::RegisterCast<std::string, TfToken>(_TfStringToToken);
     } 
 
-    typedef map<string, VtValue (*)(VtValue const &)> _CastMap;
-    typedef map<string, _CastMap> _Conversions;
+    using _ConversionSourceToTarget =
+        std::pair<std::type_index, std::type_index>;
+
+    struct _ConversionSourceToTargetHash
+    {
+        std::size_t operator()(_ConversionSourceToTarget p) const
+        {
+            std::size_t h = p.first.hash_code();
+            boost::hash_combine(h, p.second.hash_code());
+            return h;
+        }
+    };
+
+    using _Conversions = tbb::concurrent_unordered_map<
+        _ConversionSourceToTarget,
+        VtValue (*)(VtValue const &),
+        _ConversionSourceToTargetHash>;
 
     _Conversions _conversions;
-    std::mutex _mutex;
     
 };
 TF_INSTANTIATE_SINGLETON(Vt_CastRegistry);
