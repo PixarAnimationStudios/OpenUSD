marsupial/USD

Add inlined ArchEnviron function for OS X.

pxr/base/lib/arch/debugger.cpp
@@ -25,6 +25,7 @@
 
 #include "pxr/base/arch/debugger.h"
 #include "pxr/base/arch/daemon.h"
+#include "pxr/base/arch/environ.h"
 #include "pxr/base/arch/error.h"
 #include "pxr/base/arch/export.h"
 #include "pxr/base/arch/stackTrace.h"
@@ -322,16 +323,12 @@ Arch_DebuggerRunUnrelatedProcessPosix(bool (*cb)(void*), void* data)
     _exit(0);
 }
 
-
-extern char** environ;
-
-
 static
 bool
 Arch_DebuggerAttachExecPosix(void* data)
 {
     char** args = (char**)data;
-    execve(args[0], args, environ);
+    execve(args[0], args, ArchEnviron());
     return false;
 }
 

pxr/base/lib/arch/stackTrace.cpp
@@ -25,6 +25,7 @@
 #include "pxr/base/arch/debugger.h"
 #include "pxr/base/arch/defines.h"
 #include "pxr/base/arch/demangle.h"
+#include "pxr/base/arch/environ.h"
 #include "pxr/base/arch/error.h"
 #include "pxr/base/arch/export.h"
 #include "pxr/base/arch/fileSystem.h"
@@ -198,10 +199,6 @@ static const char* const* stackTraceArgv = nullptr;
 static time_t _GetAppElapsedTime();
 
 
-// asgetenv() want's this but we can't declare it inside the namespace.
-extern char **environ;
-
-
 namespace {
 
 // Return the length of s.
@@ -260,7 +257,7 @@ const char* asgetenv(const char* name)
 {
     if (name) {
         const size_t len = asstrlen(name);
-        for (char** i = environ; *i; ++i) {
+        for (char** i = ArchEnviron(); *i; ++i) {
             const char* var = *i;
             if (asstrneq(var, name, len)) {
                 if (var[len] == '=') {

PixarAnimationStudios/OpenUSD

pxr/base/lib/arch/debugger.cpp
@@ -26,6 +26,7 @@
 #include "pxr/pxr.h"
 #include "pxr/base/arch/debugger.h"
 #include "pxr/base/arch/daemon.h"
+#include "pxr/base/arch/env.h"
 #include "pxr/base/arch/error.h"
 #include "pxr/base/arch/export.h"
 #include "pxr/base/arch/stackTrace.h"
@@ -325,18 +326,12 @@ Arch_DebuggerRunUnrelatedProcessPosix(bool (*cb)(void*), void* data)
     _exit(0);
 }
 
-PXR_NAMESPACE_CLOSE_SCOPE
-
-extern char** environ;
-
-PXR_NAMESPACE_OPEN_SCOPE
-
 static
 bool
 Arch_DebuggerAttachExecPosix(void* data)
 {
     char** args = (char**)data;
-    execve(args[0], args, environ);
+    execve(args[0], args, ArchEnviron());
     return false;
 }
 

pxr/base/lib/arch/stackTrace.cpp
@@ -28,6 +28,7 @@
 #include "pxr/base/arch/debugger.h"
 #include "pxr/base/arch/defines.h"
 #include "pxr/base/arch/demangle.h"
+#include "pxr/base/arch/env.h"
 #include "pxr/base/arch/error.h"
 #include "pxr/base/arch/errno.h"
 #include "pxr/base/arch/export.h"
@@ -223,13 +224,6 @@ static const char* const* stackTraceArgv = nullptr;
 
 static long _GetAppElapsedTime();
 
-PXR_NAMESPACE_CLOSE_SCOPE
-
-// asgetenv() want's this but we can't declare it inside the namespace.
-extern char **environ;
-
-PXR_NAMESPACE_OPEN_SCOPE
-
 namespace {
 
 // Return the length of s.
@@ -292,7 +286,7 @@ const char* asgetenv(const char* name)
 {
     if (name) {
         const size_t len = asstrlen(name);
-        for (char** i = environ; *i; ++i) {
+        for (char** i = ArchEnviron(); *i; ++i) {
             const char* var = *i;
             if (asstrneq(var, name, len)) {
                 if (var[len] == '=') {