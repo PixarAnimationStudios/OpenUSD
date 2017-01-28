ca9c8967974a0b0a38f4a594f020996e6b6cf354
superfunc <superfunc@users.noreply.github.com>
2017-01-27 17:18:53 -0800

    [Usd] SchemaGen update.
    (Internal change: 1707093)

--- extras/usd/examples/usdSchemaExamples/complex.cpp
+++ extras/usd/examples/usdSchemaExamples/complex.cpp
@@ -21,7 +21,6 @@
 // KIND, either express or implied. See the Apache License for the specific
 // language governing permissions and limitations under the Apache License.
 //
-#include "pxr/pxr.h"
 #include "./complex.h"
 #include "pxr/usd/usd/schemaRegistry.h"
 #include "pxr/usd/usd/typed.h"
--- extras/usd/examples/usdSchemaExamples/wrapParamsAPI.cpp
+++ extras/usd/examples/usdSchemaExamples/wrapParamsAPI.cpp
@@ -21,7 +21,6 @@
 // KIND, either express or implied. See the Apache License for the specific
 // language governing permissions and limitations under the Apache License.
 //
-#include "pxr/pxr.h"
 #include "./paramsAPI.h"
 #include "pxr/usd/usd/schemaBase.h"
 #include "pxr/usd/usd/conversions.h"
