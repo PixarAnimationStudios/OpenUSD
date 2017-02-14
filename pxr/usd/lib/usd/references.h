d81076eea4417d7fdef01c0eb1c374f2f411112d
spiffmon <spiffmon@users.noreply.github.com>
2017-02-13 16:29:55 -0800

    Doc enhancements for References:
    * Make root-prim restriction even more salient
    * Fix up doc for recent API changes
    * Clean up structure to use subsections, with better linking
    
    Notes:
    * Could be further enhaced with usda examples, but one
    step at a time.
    
    (Internal change: 1713535)

--- pxr/usd/lib/usd/references.h
+++ pxr/usd/lib/usd/references.h
@@ -45,82 +45,100 @@ SDF_DECLARE_HANDLES(SdfPrimSpec);
 /// references in Usd.
 ///
 /// References are the primary operator for "encapsulated aggregation" of
-/// scene description. \em aggregation means that references allow us to
-/// build up rich scenes by instancing scene description recorded in a
-/// (most often) different file.  When we say "instancing" here, we mean
-/// that we can reference the same file many times at different locations
-/// in a scene's namespace.  Referenced scene description can be overridden
-/// in the referencing (or stronger) layers, allowing each instance to be
-/// directly specialized. \em Encapsulated means that regardless of how much
-/// scene description is in the referenced file, only the scene description
-/// under the targetted prim will be composed into the aggregate scene.
+/// scene description. \em aggregation means that references let us
+/// build up rich scenes by composing scene description recorded in a (most
+/// often) different layer.  A scene can reference the same layer many times at
+/// different locations in a scene's namespace.  Referenced scene description
+/// can be overridden in the referencing (or stronger) layers, allowing each
+/// instance of the reference to be directly customized/overridden.
+/// \em Encapsulated means that regardless of how much scene description is in
+/// the referenced layer, only the scene description under and composed from
+/// (via other composition arcs in the referenced layer) the targeted prim
+/// will be composed into the aggregate scene.  Multiple references to the
+/// same layer will result in the layer being opened and retained in memory
+/// only once, although each referencing prim will compose unique
+/// \ref PcpPrimIndex "prim indices" for the tree rooted at the referenced prim.
 ///
-/// Some important facts about references and use of the UsdReferences API:
+/// \section Usd_References Important Qualities and Effective Use of References
+///
 /// \li Any prim can host zero, one or multiple references 
-/// \li References are \em list \em editable, that is, they compose differently
-/// than ordinary properties and metadata.  In any given LayerStack, each 
-/// authored reference operation at the same SdfPath location in each layer 
-/// (i.e. on the same prim) will compose into an aggregate result by
-/// adding to, removing from, or stomping "weaker" references.
-/// \li References can only target <b>root prims</b> in referenced files; this
-/// is validated at authoring-time.  Note that references may omit the root prim
-/// path if they target layers that have the 'defaultPrim' layer metadata set.
-/// See SdfLayer::GetDefaultPrim() and SdfLayer::SetDefaultPrim().  In this
-/// case, the reference targets the 'defaultPrim' in the referenced layer.
-/// \li References can target the same layer in which they are authored,
-/// as long as doing so does not introduce a cycle in the composition graph
+///
+/// \li References are \ref SdfListOp "list editable"; that is, they compose
+/// differently than ordinary properties and metadata.  In any given
+/// LayerStack, each authored reference operation at the same SdfPath
+/// location in each layer (i.e. on the same prim) will compose into an
+/// aggregate result by adding to, removing from, or replacing "weaker"
+/// references.
+///
+/// \li <b>References must target root prims</b> in referenced layers;
+/// this is validated at authoring-time as well as during runtime
+/// composition.  We make this restriction because it keeps the resulting
+/// compositions reasonably straightforward to debug and understand - were we
+/// to allow sub-root references, we can create what is referred to in the
+/// composition engine as "spooky" behavior: resolved opinions that are the
+/// result of composition arcs that are not present for instrospection
+/// anywhere on the composed stage. See \ref Usd_DefaultPrim_References below
+/// for details on how to author and target "defaultPrim" references,
+/// i.e. references that do not express a prim target on the reference.
+///
+/// \li References can target the same LayerStack in which they are authored,
+/// as long as doing so does not introduce a cycle in the composition graph.
+/// See \ref Usd_Internal_References
+///
 /// \li The \c identifier component of a reference in the provided API
-/// can be a resolvable asset-path to some external file, empty, in which case 
+/// can be a resolvable asset-path to some external layer, empty, in which case 
 /// the reference targets the root layer of the LayerStack containing the 
 /// referencing layer, or the identifer of an existing anonymous, in-memory-only
 /// SdfLayer.  Care should be exercised in the latter case: calling Export() on 
 /// an anonymous layer to serialize it to a file will not attempt to replace 
 /// any references to anonymous layers with references to file-backed layers.
+///
 /// \li Opinions brought in by reference on an ancestor prim are weaker than
 /// opinions brought in by references on a descendant prim.
 ///
-/// \anchor Usd_DefaultPrim_References
-/// <b>Expressing references without prim paths.</b>
+/// \subsection Usd_DefaultPrim_References Expressing references without prim paths
 ///
 /// References may omit the target prim path if the referenced layer has the
-/// 'defaultPrim' metadata set.  See SdfLayer::GetDefaultPrim(),
-/// SdfLayer::SetDefaultPrim().  In that case, such a reference targets the
-/// layer's specified default prim.
+/// 'defaultPrim' metadata set.  In this case, the reference targets the
+/// 'defaultPrim' in the referenced layer. A layer's defaultPrim can be
+/// authored and accessed on a UsdStage whose root layer is the layer in
+/// question: see UsdStage::GetDefaultPrim() and UsdStage::SetDefaultPrim().
+/// One can also author defaultPrim directly on an SdfLayer - see
+/// SdfLayer::GetDefaultPrim(), SdfLayer::SetDefaultPrim().
 ///
-/// \anchor Usd_Internal_References
-/// <b>Expressing references to the containing LayerStack.</b>
+/// \subsection Usd_Internal_References Expressing "internal" references to the containing LayerStack
 ///
 /// References may omit the identifier specifying the referenced layer.  This
 /// creates an "internal" reference. During composition, the referenced layer 
 /// will be resolved to the root layer of the LayerStack containing the 
-/// layer where the reference was authored.
+/// layer where the reference was authored.  See AppendInternalReference().
 ///
-/// \anchor Usd_Failing_References
-/// <b>Reasons why adding a reference may fail, why adding a reference may
-/// succeed but still generate errors, and what it all means.</b>
+/// \subsection Usd_Failing_References Reasons why adding a reference may fail, why adding a reference may succeed but still generate errors, and what it all means
 ///
-/// Add() and SetItems() can each fail for a number of reasons.  If one of
-/// the specified prim targets for one of the references is not a root prim,
-/// we will generate an error, fail to author any scene description, and return
-/// \c false.  If anything goes wrong in attempting to write the reference,
-/// we also return false, and the reference will also remain unauthored.
-/// Otherwise, if the reference was successfully authored, we will return true.
-/// <b>A succesful reference authoring operation may still generate composition
-/// errors!</b>  Just because the reference you specified was syntactically
-/// correct and therefore successfully authored, does not imply it was
-/// meaningful. If you wish to ensure that the reference you are about to
-/// author will be meaningfully consumable by your stage, you are strongly
-/// encouraged to <b>ensure it will resolve to an actual file by using
-/// UsdStage::ResolveIdentifierToEditTarget() before authoring the reference.</b>
+/// AppendReference() and SetReferences() can each fail for a number of
+/// reasons.  If one of the specified prim targets for one of the references
+/// is not a root prim, we will generate an error, fail to author any scene
+/// description, and return \c false.  If anything goes wrong in attempting
+/// to write the reference, we also return false, and the reference will also
+/// remain unauthored.  Otherwise, if the reference was successfully
+/// authored, we will return \c true.  <b>A succesful reference authoring
+/// operation may still generate composition errors!</b> Just because the
+/// reference you specified was syntactically correct and therefore
+/// successfully authored, does not imply it was meaningful. If you wish to
+/// ensure that the reference you are about to author will be meaningfully
+/// consumable by your stage, you are strongly encouraged to <b>ensure it
+/// will resolve to an actual file by using
+/// UsdStage::ResolveIdentifierToEditTarget() before authoring the
+/// reference.</b>
 ///
 /// Immediately upon successful authoring of the reference (before returning
-/// from Add(), Remove(), Clear(), or SetItems()), the UsdStage on which the
-/// reference was authored will recompose the subtree rooted at the prim
-/// hosting the reference.  If the provided identifer does not resolve to a
-/// layer that is already opened or that can be opened in the usd format,
-/// \em or if the provided primPath is not an actual root prim in that layer,
-/// the stage's recomposition will fail, and pass on composition errors to the
-/// client.
+/// from AppendReference(), RemoveReference(), ClearReferences(), or
+/// SetReferences()), the UsdStage on which the reference was authored will
+/// recompose the subtree rooted at the prim hosting the reference.  If the
+/// provided identifer does not resolve to a layer that is already opened or
+/// that can be opened in the usd format, \em or if the provided primPath is
+/// not an actual root prim in that layer, the stage's recomposition will
+/// fail, and pass on composition errors to the client.
 ///
 class UsdReferences {
     friend class UsdPrim;
