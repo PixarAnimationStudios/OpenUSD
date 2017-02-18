b5b6c0484fc5a8bd116ef01a86ed892f2dc30dd9
c64kernal <c64kernal@users.noreply.github.com>
2017-02-17 16:27:42 -0800

    Fix invalid buffer reads from usdview's GL refengine when we encounter invalid color primvars.
    (Internal change: 1714491)

--- pxr/usdImaging/lib/usdImagingGL/refEngine.cpp
+++ pxr/usdImaging/lib/usdImagingGL/refEngine.cpp
@@ -810,15 +810,29 @@ UsdImagingGLRefEngine::_HandleCurves(const UsdPrim& prim)
 
     _ProcessGprimColor(&curvesSchema, prim, &doubleSided, &color,
                        &colorInterpolation);
-    if (color.size() < 1) {
-        // set default
-        color = VtArray<GfVec3f>(1);
-        color[0] = GfVec3f(0.5, 0.5, 0.5);
-        colorInterpolation = UsdGeomTokens->constant;
-    }
 
     VtVec3fArray pts;
     curvesSchema.GetPointsAttr().Get(&pts, _params.frame);
+
+
+    if (color.size() < 1) {
+
+        // set default
+        color = VtArray<GfVec3f>(1);
+        color[0] = GfVec3f(0.5f, 0.5f, 0.5f);
+        colorInterpolation = UsdGeomTokens->constant;
+
+    // Check for error condition for vertex colors
+    } else if (colorInterpolation == UsdGeomTokens->vertex &&
+               color.size() != pts.size()) {
+
+        // fallback to default
+        color = VtArray<GfVec3f>(1);
+        color[0] = GfVec3f(0.5f, 0.5f, 0.5f);
+        colorInterpolation = UsdGeomTokens->constant;
+        TF_WARN("Color primvar error on prim '%s'", prim.GetPath().GetText());
+    }
+
     int index = 0;
     TF_FOR_ALL(itr, pts) {
         GfVec3f pt = _ctm.Transform(*itr);
