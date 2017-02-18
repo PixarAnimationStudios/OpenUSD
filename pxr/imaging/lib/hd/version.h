f2c11be3ae196ccdca8049e8d0cb42208d192e56
superfunc <superfunc@users.noreply.github.com>
2017-02-17 16:28:39 -0800

    [Hd, Hdx] Rename currently unused HD_API macros.
    These macros are currently not used. The macros being used for symbol
    visibility on windows conflicts with this naming convention, so its
    being changed.
    
    (Internal change: 1714850)

--- pxr/imaging/lib/hd/version.h
+++ pxr/imaging/lib/hd/version.h
@@ -42,11 +42,11 @@
 #define HD_SHADER_API 2
 
 // 1 -> 2: Changes "doubleShaded" API to "doubleSided".
-#define HD_CHANGETRACKER_API 2
-#define HD_ENGINE_API 1
-#define HD_RPRIMCOLLECTION_API 1
+#define HD_CHANGETRACKER_API_VERSION 2
+#define HD_ENGINE_API_VERSION 1
+#define HD_RPRIMCOLLECTION_API_VERSION 1
 
 // 2 -> 3: Changes "doubleShaded" API to "doubleSided".
-#define HD_TOKENS_API   3
+#define HD_TOKENS_API_VERSION  3
 
 #endif // HD_VERSION_H
