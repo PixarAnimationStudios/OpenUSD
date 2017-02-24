9a74fabfd820513dd15a967938383f3a7f54b663
pixar-oss <pixar-oss@users.noreply.github.com>
2017-02-24 11:43:21 -0800

    Fix Windows build of WorkArenaDispatcher.
    A c'tor was made non-inline and so needed WORK_API added.
    
    (Internal change: 1717057)

--- pxr/base/lib/work/arenaDispatcher.h
+++ pxr/base/lib/work/arenaDispatcher.h
@@ -57,7 +57,7 @@ class WorkArenaDispatcher
 public:
     /// Constructs a new dispatcher. The internal arena will mirror the
     /// global concurrency limit setting.
-    WorkArenaDispatcher();
+    WORK_API WorkArenaDispatcher();
 
     /// Wait() for any pending tasks to complete, then destroy the dispatcher.
     WORK_API ~WorkArenaDispatcher();
