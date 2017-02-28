014dc847e65d33d5bcc68143c8e011c4e597f689
sunyab <sunyab@users.noreply.github.com>
2017-02-28 10:14:04 -0800

    Add namespace support to point instancer op
    (Internal change: 1718953)

--- third_party/katana/plugin/pxrUsdInShipped/pointInstancer.cpp
+++ third_party/katana/plugin/pxrUsdInShipped/pointInstancer.cpp
@@ -23,10 +23,13 @@
 //
 #include "pxrUsdInShipped/declareCoreOps.h"
 
+#include "pxr/pxr.h"
 #include "usdKatana/attrMap.h"
 #include "usdKatana/readPointInstancer.h"
 
 #include "pxr/usd/usdGeom/pointInstancer.h"
+
+PXR_NAMESPACE_USING_DIRECTIVE
 
 PXRUSDKATANA_USDIN_PLUGIN_DEFINE(PxrUsdInCore_PointInstancerOp, privateData, interface)
 {
