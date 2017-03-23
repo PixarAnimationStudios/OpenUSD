26878fd26ebc6dfc4b074a3cc7e9d1153ebd429b
pixar-oss <pixar-oss@users.noreply.github.com>
2017-03-23 11:37:20 -0700

    Fixed link of alembic tests on Windows.
    While a plugin doesn't normally need any exported symbols, for
    testing purposes we need to export two symbols.
    
    (Internal change: 1727297)

--- pxr/usd/plugin/usdAbc/api.h
+++ pxr/usd/plugin/usdAbc/api.h
@@ -1,5 +1,5 @@
 //
-// Copyright 2016 Pixar
+// Copyright 2017 Pixar
 //
 // Licensed under the Apache License, Version 2.0 (the "Apache License")
 // with the following modification; you may not use this file except in
@@ -21,26 +21,27 @@
 // KIND, either express or implied. See the Apache License for the specific
 // language governing permissions and limitations under the Apache License.
 //
-#ifndef USDABC_ALEMBICTEST_H
-#define USDABC_ALEMBICTEST_H
+#ifndef USDABC_API_H
+#define USDABC_API_H
 
-#include "pxr/pxr.h"
-#include "pxr/usd/usdAbc/api.h"
-#include <string>
+#include "pxr/base/arch/export.h"
 
-PXR_NAMESPACE_OPEN_SCOPE
+#if defined(USDABC_STATIC)
+#   define USDABC_API
+#   define USDABC_API_TEMPLATE_CLASS(...)
+#   define USDABC_API_TEMPLATE_STRUCT(...)
+#   define USDABC_LOCAL
+#else
+#   if defined(USDABC_EXPORTS)
+#       define USDABC_API ARCH_EXPORT
+#       define USDABC_API_TEMPLATE_CLASS(...) ARCH_EXPORT_TEMPLATE(class, __VA_ARGS__)
+#       define USDABC_API_TEMPLATE_STRUCT(...) ARCH_EXPORT_TEMPLATE(struct, __VA_ARGS__)
+#   else
+#       define USDABC_API ARCH_IMPORT
+#       define USDABC_API_TEMPLATE_CLASS(...) ARCH_IMPORT_TEMPLATE(class, __VA_ARGS__)
+#       define USDABC_API_TEMPLATE_STRUCT(...) ARCH_IMPORT_TEMPLATE(struct, __VA_ARGS__)
+#   endif
+#   define USDABC_LOCAL ARCH_HIDDEN
+#endif
 
-
-/// Test Alembic conversion.
-USDABC_API
-bool UsdAbc_TestAlembic(const std::string& pathname);
-
-/// Read Usd file from \p srcPathname and write as Alembic to \p dstPathname.
-USDABC_API
-bool UsdAbc_WriteAlembic(const std::string& srcPathname,
-                         const std::string& dstPathname);
-
-
-PXR_NAMESPACE_CLOSE_SCOPE
-
-#endif // USDABC_ALEMBICTEST_H
+#endif
