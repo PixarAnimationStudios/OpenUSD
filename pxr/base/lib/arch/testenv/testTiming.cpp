58307c342f1a47e4725b0162c04e1b91acd895d8
pixar-oss <pixar-oss@users.noreply.github.com>
2017-04-04 14:30:15 -0700

    Fixed testArchTiming.
    Previously this test would call ArchGetTickTime() and perform
    a number of math operations to verify expected behavior.
    However, if the result was large enough then integer overflow
    would cause the verifications to fail.  In addition, any
    failure is unreproducible.
    
    Now the test tests a fixed arbitrary range of tick counts
    starting at zero and tests each separately.  It also calls
    ArchGetTickTime() twice separated by a sleep and verifies
    that the number of ticks approximately corresponds to the
    sleep time.
    
    This indicated that the code to find the clock frequency on
    Windows was broken, off by about a factor of 1000.  This code
    was replaced with code that counts the number of ticks in some
    measured interval.
    
    (Internal change: 1732260)

--- pxr/base/lib/arch/testenv/testTiming.cpp
+++ pxr/base/lib/arch/testenv/testTiming.cpp
@@ -25,19 +25,34 @@
 #include "pxr/pxr.h"
 #include "pxr/base/arch/timing.h"
 #include "pxr/base/arch/error.h"
+#include <chrono>
+#include <thread>
 
 PXR_NAMESPACE_USING_DIRECTIVE
 
 int main()
 {
-    uint64_t ticks = ArchGetTickTime();
-    ARCH_AXIOM( (uint64_t) ArchTicksToNanoseconds(ticks) == 
-        uint64_t(static_cast<double>(ticks)*ArchGetNanosecondsPerTick() + .5));
+    // Verify conversions for many tick counts.
+    for (size_t ticks = 0ul; ticks != 1ul << 24u; ++ticks) {
+        ARCH_AXIOM( (uint64_t) ArchTicksToNanoseconds(ticks) == 
+            uint64_t(static_cast<double>(ticks)*ArchGetNanosecondsPerTick() + .5));
 
-    double nanos = double(ArchTicksToNanoseconds(ticks)) / 1e9;
-    double secs = ArchTicksToSeconds(ticks);
-    double epsilon = 0.0001;
-    ARCH_AXIOM( (nanos - epsilon <= secs) && (nanos + epsilon >= secs) );
+        double nanos = double(ArchTicksToNanoseconds(ticks)) / 1e9;
+        double secs = ArchTicksToSeconds(ticks);
+        double epsilon = 0.0001;
+        ARCH_AXIOM( (nanos - epsilon <= secs) && (nanos + epsilon >= secs) );
+    }
+
+    // Compute some time delta.
+    const auto t1 = ArchGetTickTime();
+    std::this_thread::sleep_for(std::chrono::milliseconds(1500));
+    const auto t2 = ArchGetTickTime();
+    const auto delta = t2 - t1;
+
+    // Verify the delta is reasonable.  We allow a lot of leeway on the top
+    // end in case of heavy machine load.
+    ARCH_AXIOM(ArchTicksToSeconds(delta) > 1.4);
+    ARCH_AXIOM(ArchTicksToSeconds(delta) < 5.0);
 
     return 0;
 }
