2b1c809fc4d3edaa11e3f117656eaf2b69ee4868
sunyab <sunyab@users.noreply.github.com>
2017-04-20 11:25:14 -0700

    Enable C++ namespaces by default in the open source build.
    (Internal change: 1737660)

--- cmake/defaults/Options.cmake
+++ cmake/defaults/Options.cmake
@@ -33,7 +33,7 @@ option(PXR_BUILD_DOCUMENTATION "Generate doxygen documentation" OFF)
 option(PXR_ENABLE_MULTIVERSE_SUPPORT "Enable Multiverse backend in the Alembic plugin for USD" OFF)
 option(PXR_ENABLE_PTEX_SUPPORT "Enable Ptex support" ON)
 option(PXR_MAYA_TBB_BUG_WORKAROUND "Turn on linker flag (-Wl,-Bsymbolic) to work around a Maya TBB bug" OFF)
-option(PXR_ENABLE_NAMESPACES "Enable C++ namespaces." OFF)
+option(PXR_ENABLE_NAMESPACES "Enable C++ namespaces." ON)
 
 # Precompiled headers are a win on Windows, not on gcc.
 set(pxr_enable_pch "OFF")
