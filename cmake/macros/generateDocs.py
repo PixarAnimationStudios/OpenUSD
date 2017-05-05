ecd4dde818bbe24154601dd3489c097c2da34a6e
pixar-oss <pixar-oss@users.noreply.github.com>
2017-05-05 16:26:26 -0700

    Fixes for documentation on Windows.
    We need quotes around @DOT_EXECUTABLE@ in case the path has
    spaces it (which is almost certain on Windows and possible on
    Unix).
    
    Using TemporaryFile() as an output appears to be problematic
    on Windows, possibly due to exclusive file locking.  This
    changes the doxygen invokation to collect output via a pipe.
    
    (Internal change: 1744307)

--- cmake/macros/generateDocs.py
+++ cmake/macros/generateDocs.py
@@ -93,10 +93,13 @@ def _generateDocs(pxrSourceRoot, pxrBuildRoot, installLoc,
 
     os.chdir(installLoc)
     cmd = [doxygenBin, doxyConfigDest] 
-    with tempfile.TemporaryFile() as tmpFile:
-        if subprocess.call(cmd, stdout=tmpFile, stderr=tmpFile) != 0:
-            sys.stderr.write(tmpFile.read())
-            sys.exit('Error: doxygen failed to complete.')
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                                 stderr=subprocess.STDOUT)
+    output = proc.communicate()[0]
+    if proc.wait() != 0:
+        print >>sys.stderr, output.replace('\r\n', '\n')
+        sys.exit('Error: doxygen failed to complete; '
+                 'exit code %d.' % proc.returncode)
 
 def _checkPath(path, ident, perm):
     if not os.path.exists(path):
