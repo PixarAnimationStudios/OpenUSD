42b2aa3a20ab6bbe9a2f42971fe73819dc314099
dakrunch <dakrunch@users.noreply.github.com>
2017-05-05 01:39:41 -0700

    Handle instanceProxy (master) paths in specialize arcs.
    (Internal change: 1742486)

--- pxr/usd/lib/usdShade/material.cpp
+++ pxr/usd/lib/usdShade/material.cpp
@@ -430,10 +430,20 @@ UsdShadeMaterial::GetBaseMaterialPath() const
         }
     }
 
-    return FindBaseMaterialPathInPrimIndex(
-            GetPrim().GetPrimIndex(),
-            [=](const SdfPath &p)
-                {return bool(_GetMaterialAtPath(GetPrim(), p));});
+    SdfPath parentMaterialPath = FindBaseMaterialPathInPrimIndex(
+        GetPrim().GetPrimIndex(), [=](const SdfPath &p) {
+            return bool(_GetMaterialAtPath(GetPrim(), p));
+        });
+
+    if (parentMaterialPath != SdfPath::EmptyPath()) {
+        UsdPrim p = GetPrim().GetStage()->GetPrimAtPath(parentMaterialPath);
+        if (p.IsInstanceProxy()) {
+            // this looks like an instance but it's acting as the master path.
+            // Return the master path
+            parentMaterialPath = p.GetPrimInMaster().GetPath();
+        }
+    }
+    return parentMaterialPath;
 }
 
 /* static */
@@ -443,8 +453,7 @@ UsdShadeMaterial::FindBaseMaterialPathInPrimIndex(
         const PathPredicate & pathIsMaterialPredicate)
 {
     for(const PcpNodeRef &node: primIndex.GetNodeRange()) {
-        if (PcpIsSpecializesArc(node.GetArcType()))
-        {
+        if (PcpIsSpecializesArc(node.GetArcType())) {
             // We only consider children of the prim's root node because any
             // specializes arc we care about that is authored inside referenced
             // scene description will "imply" up into the root layer stack.
