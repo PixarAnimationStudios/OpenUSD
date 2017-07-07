abb3725228924dfe582aaf7d75b21dbfe85aafcd
blevin <blevin@users.noreply.github.com>
2017-07-07 13:49:10 -0700

    Pcp: Update outdated & misleading comment
    (Internal change: 1764815)

--- pxr/usd/lib/pcp/primIndex.cpp
+++ pxr/usd/lib/pcp/primIndex.cpp
@@ -3660,11 +3660,8 @@ _EvalNodePayload(
     SdfPath const &payloadPath = defaultPayloadPath.IsEmpty() ?
         payload.GetPrimPath() : defaultPayloadPath;
 
-    // Layer offsets that apply to the site where the payload was
-    // introduced should apply to the payload as well. We have to
-    // manually bake in this offset because the new payload node 
-    // will be a child of the root node, which is not necessarily where
-    // the payload was authored.
+    // Incorporate any layer offset from this site to the sublayer
+    // where the payload was expressed.
     const SdfLayerOffset *maybeOffset =
         node.GetSite().layerStack->
         GetLayerOffsetForLayer(payloadSpecLayer);
