89dae6ce56cc9fca1b8b6543629e96d32c198fee
blevin <blevin@users.noreply.github.com>
2017-07-07 13:47:24 -0700

    usdcat: add --mask, matching usdview --mask
    (Internal change: 1764087)
    (Internal change: 1764146)
    (Internal change: 1764348)

--- pxr/usd/bin/usdcat/usdcat.py
+++ pxr/usd/bin/usdcat/usdcat.py
@@ -31,10 +31,17 @@ def GetUsdData(filePath):
     from pxr import Sdf
     return Sdf.Layer.FindOrOpen(filePath)
 
-def GetFlattenedUsdData(filePath):
+def GetFlattenedUsdData(filePath, populationMaskPaths):
     from pxr import Ar, Usd
     Ar.GetResolver().ConfigureResolverForAsset(filePath)
-    return Usd.Stage.Open(filePath)
+    popMask = (None if populationMaskPaths is None
+               else Usd.StagePopulationMask())
+    if popMask:
+        for path in populationMaskPaths:
+            popMask.Add(path)
+        return Usd.Stage.OpenMasked(filePath, popMask)
+    else:
+        return Usd.Stage.Open(filePath)
 
 def main():
     parser = argparse.ArgumentParser(
@@ -55,6 +62,15 @@ def main():
     parser.add_argument(
         '-f', '--flatten', action='store_true', help='Compose stages with the '
         'input files as root layers and write their flattened content.')
+    parser.add_argument('--mask', action='store',
+                        dest='populationMask',
+                        metavar='PRIMPATH[,PRIMPATH...]',
+                        help='Limit stage population to these prims, '
+                        'their descendants and ancestors.  To specify '
+                        'multiple paths, either use commas with no spaces '
+                        'or quote the argument and separate paths by '
+                        'commas and/or spaces.  Requires --flatten.')
+
 
     args = parser.parse_args()
 
@@ -85,6 +101,14 @@ def main():
                  % (parser.prog, ext))
             return 1
 
+    # split args.populationMask into paths.
+    if args.populationMask:
+        if not args.flatten:
+            # You can only mask a stage, not a layer.
+            _Err("%s: error: --mask requires --flatten" % parser.prog)
+            return 1
+        args.populationMask = args.populationMask.replace(',', ' ').split()
+
     from pxr import Usd
 
     exitCode = 0
@@ -106,8 +130,8 @@ def main():
         # --flatten was specified.  Note that 'usdData' will be either a
         # Usd.Stage or an Sdf.Layer.
         try:
-            usdData = (GetFlattenedUsdData(inputFile) if args.flatten
-                       else GetUsdData(inputFile))
+            usdData = (GetFlattenedUsdData(inputFile, args.populationMask)
+                       if args.flatten else GetUsdData(inputFile))
             if not usdData:
                 raise Exception("Unknown error")
         except Exception as e:
