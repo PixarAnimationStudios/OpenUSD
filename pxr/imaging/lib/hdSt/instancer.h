1331a6aed702f3b908569267e3bc0a3a4acd2c81
Bruce Mitchener <bruce.mitchener@gmail.com>
2017-07-21 15:01:57 +0700

    Fix mismatched tags warnings under clang.
    Clang issues a warning when forward declarations don't match the
    actual declaration about whether something is a struct or class.

--- pxr/imaging/lib/hdSt/instancer.h
+++ pxr/imaging/lib/hdSt/instancer.h
@@ -37,7 +37,7 @@
 PXR_NAMESPACE_OPEN_SCOPE
 
 class HdDrawItem;
-class HdRprimSharedData;
+struct HdRprimSharedData;
 
 typedef boost::shared_ptr<class HdBufferArrayRange> HdBufferArrayRangeSharedPtr;
 
