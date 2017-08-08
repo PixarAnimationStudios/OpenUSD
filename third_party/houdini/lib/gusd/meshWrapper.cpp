1b916911e83e1465b3cb15d53da7cbeed3df5023
pixar-oss <pixar-oss@users.noreply.github.com>
2017-08-08 11:38:33 -0700

    Fix problems with setting orientation when overlaying mesh topology.
    (Internal change: 1773140)

--- third_party/houdini/lib/gusd/meshWrapper.cpp
+++ third_party/houdini/lib/gusd/meshWrapper.cpp
@@ -219,7 +219,7 @@ initialize( const GusdContext& ctxt,
             // otherwise just write the left handled verts directly.
             TfToken orientation;
             usdAttr.Get(&orientation, UsdTimeCode::Default());
-            if( (m_forceCreateNewGeo || !ctxt.writeOverlay) && orientation == UsdGeomTokens->rightHanded ) {
+            if( orientation == UsdGeomTokens->rightHanded ) {
                 usdAttr.Set(UsdGeomTokens->leftHanded, UsdTimeCode::Default());
             }
         }
