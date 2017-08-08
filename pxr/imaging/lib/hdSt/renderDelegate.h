03a8d7d8424776e329c02e3b96101b91cbd274a2
poljere <poljere@users.noreply.github.com>
2017-08-08 13:48:12 -0700

    Thread safe initialization of the render delegates that share a resource registry.
    (Internal change: 1773180)

--- pxr/imaging/lib/hdSt/renderDelegate.h
+++ pxr/imaging/lib/hdSt/renderDelegate.h
@@ -27,6 +27,8 @@
 #include "pxr/imaging/hdSt/api.h"
 #include "pxr/imaging/hd/renderDelegate.h"
 
+#include <mutex>
+
 PXR_NAMESPACE_OPEN_SCOPE
 
 
@@ -100,6 +102,7 @@ private:
     static const TfTokenVector SUPPORTED_BPRIM_TYPES;
 
     /// Resource registry used in this render delegate
+    static std::mutex _mutexResourceRegistry;
     static std::atomic_int _counterResourceRegistry;
     static HdResourceRegistrySharedPtr _resourceRegistry;
 
