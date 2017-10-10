fccb65de25c0615f3a8304e44414efee58df14fd
sunyab <sunyab@users.noreply.github.com>
2017-10-10 13:35:37 -0700

    Allow fallback values for most metadata in schemas
    UsdSchemaRegistry now allows fallback values for all
    metadata fields, except for a few fields whose fallback
    values wouldn't be used (e.g. composition arcs, time
    samples). This allows users to specify fallback values
    for custom metadata fields in schemas.
    
    To provide better feedback to schema authors,
    usdGenSchema will also error out if it encounters
    any fields in UsdSchemaRegistry's blacklist,
    
    (Internal change: 1792266)

---
+++ pxr/usd/lib/usd/testenv/testUsdSchemaRegistry.py
@@ -0,0 +1,75 @@
+#!/pxrpythonsubst
+#
+# Copyright 2017 Pixar
+#
+# Licensed under the Apache License, Version 2.0 (the "Apache License")
+# with the following modification; you may not use this file except in
+# compliance with the Apache License and the following modification to it:
+# Section 6. Trademarks. is deleted and replaced with:
+#
+# 6. Trademarks. This License does not grant permission to use the trade
+#    names, trademarks, service marks, or product names of the Licensor
+#    and its affiliates, except as required to comply with Section 4(c) of
+#    the License and to reproduce the content of the NOTICE file.
+#
+# You may obtain a copy of the Apache License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the Apache License with the above modification is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the Apache License for the specific
+# language governing permissions and limitations under the Apache License.
+
+import os, unittest
+from pxr import Plug, Usd, Vt
+
+class TestUsdSchemaRegistry(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pr = Plug.Registry()
+        testPlugins = pr.RegisterPlugins(os.path.abspath("resources"))
+        assert len(testPlugins) == 1, \
+            "Failed to load expected test plugin"
+        assert testPlugins[0].name == "testUsdSchemaRegistry", \
+            "Failed to load expected test plugin"
+    
+    def test_PrimMetadata(self):
+        primDef = Usd.SchemaRegistry.GetPrimDefinition(
+            "TestUsdSchemaRegistryMetadataTest")
+        self.assertTrue(primDef)
+
+        self.assertEqual(primDef.GetInfo("documentation"),
+                         "Testing documentation metadata")
+        self.assertEqual(primDef.GetInfo("hidden"), True)
+        self.assertEqual(primDef.GetInfo("testCustomMetadata"), "garply")
+
+    def test_AttributeMetadata(self):
+        attrDef = Usd.SchemaRegistry.GetAttributeDefinition(
+            "TestUsdSchemaRegistryMetadataTest", "testAttr")
+        self.assertTrue(attrDef)
+
+        self.assertEqual(attrDef.GetInfo("allowedTokens"),
+                         Vt.TokenArray(["bar", "baz"]))
+        self.assertEqual(attrDef.GetInfo("displayGroup"), "Display Group")
+        self.assertEqual(attrDef.GetInfo("displayName"), "Display Name")
+        self.assertEqual(attrDef.GetInfo("documentation"),
+                         "Testing documentation metadata")
+        self.assertEqual(attrDef.GetInfo("hidden"), True)
+        self.assertEqual(attrDef.GetInfo("testCustomMetadata"), "garply")
+
+    def test_RelationshipMetadata(self):
+        relDef = Usd.SchemaRegistry.GetRelationshipDefinition(
+            "TestUsdSchemaRegistryMetadataTest", "testRel")
+        self.assertTrue(relDef)
+
+        self.assertEqual(relDef.GetInfo("displayGroup"), "Display Group")
+        self.assertEqual(relDef.GetInfo("displayName"), "Display Name")
+        self.assertEqual(relDef.GetInfo("documentation"),
+                         "Testing documentation metadata")
+        self.assertEqual(relDef.GetInfo("hidden"), True)
+        self.assertEqual(relDef.GetInfo("testCustomMetadata"), "garply")
+
+if __name__ == "__main__":
+    unittest.main()
