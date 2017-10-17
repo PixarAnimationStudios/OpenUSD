90e002086135276ac8d65c99e86aea91ec8b747d
sdao <sdao@users.noreply.github.com>
2017-10-16 17:23:23 -0700

    Solve issue with getting the wrong UV values in MayaMeshWriter_PrimVars because we were using MItMeshFaceVertex::getUV (inMesh values) instead of getUVIndex (outMesh values)
    To properly get the value of the UVs in the outMesh, we need to get the UV array from MFnMesh::getUVs and the index from MItMeshFaceVertex::getUVIndex, and the index into the array.
    
    _GetMeshUVSetData has been updated to use the same pattern as _GetMeshNormals.
    
    (Internal change: 1793131)

--- third_party/maya/lib/usdMaya/MayaMeshWriter_PrimVars.cpp
+++ third_party/maya/lib/usdMaya/MayaMeshWriter_PrimVars.cpp
@@ -62,6 +62,16 @@ MayaMeshWriter::_GetMeshUVSetData(
         return false;
     }
 
+    // using itFV.getUV() does not always give us the right answer, so
+    // instead, we have to use itFV.getUVIndex() and use that to index into the
+    // UV set.
+    MFloatArray uArray;
+    MFloatArray vArray;
+    mesh.getUVs(uArray, vArray, &uvSetName);
+    if (uArray.length() != vArray.length()) {
+        return false;
+    }
+
     // We'll populate the assignment indices for every face vertex, but we'll
     // only push values into the data if the face vertex has a value. All face
     // vertices are initially unassigned/unauthored.
@@ -78,10 +88,13 @@ MayaMeshWriter::_GetMeshUVSetData(
             continue;
         }
 
-        float2 uv;
-        itFV.getUV(uv, &uvSetName);
+        int uvIndex;
+        itFV.getUVIndex(uvIndex, &uvSetName);
+        if (uvIndex < 0 || static_cast<size_t>(uvIndex) >= uArray.length()) {
+            return false;
+        }
 
-        GfVec2f value(uv[0], uv[1]);
+        GfVec2f value(uArray[uvIndex], vArray[uvIndex]);
         uvArray->push_back(value);
         (*assignmentIndices)[fvi] = uvArray->size() - 1;
     }
