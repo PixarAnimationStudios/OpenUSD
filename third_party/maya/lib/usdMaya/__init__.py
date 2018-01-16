157962cd218ad8d08877862d5a0997f77e7a8d39
mattyjams <mattyjams@users.noreply.github.com>
2018-01-16 10:12:59 -0800

    [usdMaya] add Python methods for managing USD reference assemblies
    This change adds the following methods for getting the names of, loading,
    unloading, and changing the active representation of USD reference
    assembly nodes in the Maya scene:
    - GetReferenceAssemblies()
    - LoadReferenceAssemblies()
    - UnloadReferenceAssemblies()
    - ExpandReferenceAssemblies()
    - CollapseReferenceAssemblies()
    
    (Internal change: 1818001)

--- third_party/maya/lib/usdMaya/__init__.py
+++ third_party/maya/lib/usdMaya/__init__.py
@@ -33,3 +33,121 @@ try:
     del __DOC
 except Exception:
     pass
+
+
+from maya import cmds
+from maya import mel
+
+
+def GetReferenceAssemblies(parentNodes=None):
+    """
+    Gets the names of USD reference assembly nodes in the scene.
+
+    If parentNodes is a Maya node name or a list of Maya node names, all
+    assemblies that match or are descendants of those nodes will be returned.
+    Otherwise, all assemblies in the scene will be returned.
+    """
+    ASSEMBLY_NODE_TYPE = 'pxrUsdReferenceAssembly'
+
+    if parentNodes:
+        refAssemblies = cmds.ls(parentNodes, dagObjects=True, long=True,
+            type=ASSEMBLY_NODE_TYPE)
+    else:
+        refAssemblies = cmds.ls(dagObjects=True, long=True,
+            type=ASSEMBLY_NODE_TYPE)
+
+    return refAssemblies or []
+
+def LoadReferenceAssemblies(parentNodes=None):
+    """
+    Loads USD reference assembly nodes in the scene.
+
+    USD reference assemblies that do not already have a representation
+    activated will have their "Collapsed" representation activated.
+    """
+    refAssemblies = GetReferenceAssemblies(parentNodes=parentNodes)
+    numRefAssemblies = len(refAssemblies)
+    if numRefAssemblies < 1:
+        return
+
+    gMainProgressBar = mel.eval('$tmp = $gMainProgressBar')
+    cmds.progressBar(gMainProgressBar, edit=True, beginProgress=True,
+        isInterruptable=True, minValue=0, maxValue=numRefAssemblies,
+        status='Loading USD reference assemblies...')
+
+    for i in xrange(numRefAssemblies):
+        shouldStop = cmds.progressBar(gMainProgressBar, query=True,
+            isCancelled=True)
+        if shouldStop:
+            cmds.warning('Cancelled loading USD reference assemblies')
+            break
+
+        refAssembly = refAssemblies[i]
+
+        cmds.progressBar(gMainProgressBar, edit=True,
+            status='Loading "%s" (%d of %d)' % (refAssembly, i, numRefAssemblies))
+        cmds.progressBar(gMainProgressBar, edit=True, step=1)
+
+        activeRep = cmds.assembly(refAssembly, query=True, active=True)
+        if activeRep:
+            continue
+
+        try:
+            cmds.assembly(refAssembly, edit=True, active='Collapsed')
+        except:
+            cmds.warning('Failed to load USD reference assembly: %s' %
+                refAssembly)
+
+    cmds.progressBar(gMainProgressBar, edit=True, endProgress=True)
+
+def UnloadReferenceAssemblies(parentNodes=None):
+    """
+    Unloads USD reference assembly nodes in the scene.
+    """
+    refAssemblies = GetReferenceAssemblies(parentNodes=parentNodes)
+    for refAssembly in refAssemblies:
+        if not cmds.objExists(refAssembly):
+            # Unloading a parent assembly may have caused the child assembly
+            # to go away.
+            continue
+
+        try:
+            cmds.assembly(refAssembly, edit=True, active='')
+        except:
+            cmds.warning('Failed to unload USD reference assembly: %s' %
+                refAssembly)
+
+def ExpandReferenceAssemblies(parentNodes=None):
+    """
+    Activates the "Expanded" representation of USD reference assembly nodes.
+    """
+    refAssemblies = GetReferenceAssemblies(parentNodes=parentNodes)
+    for refAssembly in refAssemblies:
+        try:
+            cmds.assembly(refAssembly, edit=True, active='Expanded')
+        except:
+            cmds.warning('Failed to expand USD reference assembly: %s' %
+                refAssembly)
+
+def CollapseReferenceAssemblies(parentNodes=None):
+    """
+    Activates the "Collapsed" representation of USD reference assembly nodes.
+
+    Note that assemblies that have no representation activated are ignored.
+    """
+    refAssemblies = GetReferenceAssemblies(parentNodes=parentNodes)
+    for refAssembly in refAssemblies:
+        if not cmds.objExists(refAssembly):
+            # Collapsing a parent assembly may have caused the child assembly
+            # to go away.
+            continue
+
+        activeRep = cmds.assembly(refAssembly, query=True, active=True)
+        if not activeRep:
+            continue
+
+        try:
+            cmds.assembly(refAssembly, edit=True, active='Collapsed')
+        except:
+            cmds.warning('Failed to collapse USD reference assembly: %s' %
+                refAssembly)
