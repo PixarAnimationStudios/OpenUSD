7e99a92a50be75632ebb09f925daf8c9c8a60ac3
mattyjams <mattyjams@users.noreply.github.com>
2018-03-07 10:38:20 -0800

    [usdMaya] add unit test for exporting locators with transforms/shapes underneath
    (Internal change: 1836504)

--- third_party/maya/lib/usdMaya/testenv/testUsdExportLocator.py
+++ third_party/maya/lib/usdMaya/testenv/testUsdExportLocator.py
@@ -40,48 +40,72 @@ class testUsdExportLocator(unittest.TestCase):
     def setUpClass(cls):
         standalone.initialize('usd')
 
+        cmds.loadPlugin('pxrUsd')
+
         mayaFile = os.path.abspath('SingleLocator.ma')
         cmds.file(mayaFile, open=True, force=True)
 
-        cmds.loadPlugin('pxrUsd')
-
-        cls._usdFilePathMerged = os.path.abspath(
+        cls._singleLocatorUsdFilePathMerged = os.path.abspath(
             'SingleLocator_MERGED.usda')
-        cmds.usdExport(file=cls._usdFilePathMerged,
-            mergeTransformAndShape=True)
+        cmds.usdExport(file=cls._singleLocatorUsdFilePathMerged,
+            mergeTransformAndShape=True, shadingMode="none")
 
-        cls._usdFilePathUnmerged = os.path.abspath(
+        cls._singleLocatorUsdFilePathUnmerged = os.path.abspath(
             'SingleLocator_UNMERGED.usda')
-        cmds.usdExport(file=cls._usdFilePathUnmerged,
-            mergeTransformAndShape=False)
+        cmds.usdExport(file=cls._singleLocatorUsdFilePathUnmerged,
+            mergeTransformAndShape=False, shadingMode="none")
 
-        cls._usdFilePathMergedRanged = os.path.abspath(
+        cls._singleLocatorUsdFilePathMergedRanged = os.path.abspath(
             'SingleLocator_MERGED_RANGED.usda')
-        cmds.usdExport(file=cls._usdFilePathMergedRanged,
-            mergeTransformAndShape=True, frameRange=(1, 1))
+        cmds.usdExport(file=cls._singleLocatorUsdFilePathMergedRanged,
+            mergeTransformAndShape=True, shadingMode="none",
+            frameRange=(1, 1))
 
-        cls._usdFilePathUnmergedRanged = os.path.abspath(
+        cls._singleLocatorUsdFilePathUnmergedRanged = os.path.abspath(
             'SingleLocator_UNMERGED_RANGED.usda')
-        cmds.usdExport(file=cls._usdFilePathUnmergedRanged,
-            mergeTransformAndShape=False, frameRange=(1, 1))
+        cmds.usdExport(file=cls._singleLocatorUsdFilePathUnmergedRanged,
+            mergeTransformAndShape=False, shadingMode="none",
+            frameRange=(1, 1))
+
+        mayaFile = os.path.abspath('CubeUnderLocator.ma')
+        cmds.file(mayaFile, open=True, force=True)
+
+        cls._locatorParentUsdFilePathMerged = os.path.abspath(
+            'CubeUnderLocator_MERGED.usda')
+        cmds.usdExport(file=cls._locatorParentUsdFilePathMerged,
+            mergeTransformAndShape=True, shadingMode="none")
+
+        cls._locatorParentUsdFilePathUnmerged = os.path.abspath(
+            'CubeUnderLocator_UNMERGED.usda')
+        cmds.usdExport(file=cls._locatorParentUsdFilePathUnmerged,
+            mergeTransformAndShape=False, shadingMode="none")
+
+        cls._locatorParentUsdFilePathMergedRanged = os.path.abspath(
+            'CubeUnderLocator_MERGED_RANGED.usda')
+        cmds.usdExport(file=cls._locatorParentUsdFilePathMergedRanged,
+            mergeTransformAndShape=True, shadingMode="none",
+            frameRange=(1, 1))
+
+        cls._locatorParentUsdFilePathUnmergedRanged = os.path.abspath(
+            'CubeUnderLocator_UNMERGED_RANGED.usda')
+        cmds.usdExport(file=cls._locatorParentUsdFilePathUnmergedRanged,
+            mergeTransformAndShape=False, shadingMode="none",
+            frameRange=(1, 1))
 
     @classmethod
     def tearDownClass(cls):
         standalone.uninitialize()
 
-    def _ValidateXformPrim(self, stage, xformPrimPath):
-        xformPrim = stage.GetPrimAtPath(xformPrimPath)
-        self.assertTrue(xformPrim)
+    def _ValidateXformOps(self, prim, expectedTranslation=None):
+        xformable = UsdGeom.Xformable(prim)
+        self.assertTrue(xformable)
 
-        xformSchema = UsdGeom.Xform(xformPrim)
-        self.assertTrue(xformSchema)
+        xformOps = xformable.GetOrderedXformOps()
 
-        return xformSchema
+        if expectedTranslation is None:
+            self.assertEqual(len(xformOps), 0)
+            return xformable
 
-    def _ValidateLocatorPrim(self, stage, locatorPrimPath):
-        xformSchema = self._ValidateXformPrim(stage, locatorPrimPath)
-
-        xformOps = xformSchema.GetOrderedXformOps()
         self.assertEqual(len(xformOps), 1)
 
         translateOp = xformOps[0]
@@ -89,28 +113,54 @@ class testUsdExportLocator(unittest.TestCase):
         self.assertEqual(translateOp.GetOpName(), 'xformOp:translate')
         self.assertEqual(translateOp.GetOpType(), UsdGeom.XformOp.TypeTranslate)
 
-        expectedTranslation = Gf.Vec3d(1.0, 2.0, 3.0)
         self.assertTrue(Gf.IsClose(translateOp.Get(), expectedTranslation, 1e-6))
+
+        return xformable
+
+    def _ValidateXformPrim(self, stage, xformPrimPath,
+            expectedTranslation=None):
+        xformPrim = stage.GetPrimAtPath(xformPrimPath)
+        self.assertTrue(xformPrim)
+
+        xformSchema = UsdGeom.Xform(xformPrim)
+        self.assertTrue(xformSchema)
+
+        self._ValidateXformOps(xformPrim, expectedTranslation)
+
+        return xformSchema
+
+    def _ValidateMeshPrim(self, stage, meshPrimPath, expectedTranslation=None):
+        meshPrim = stage.GetPrimAtPath(meshPrimPath)
+        self.assertTrue(meshPrim)
+
+        meshSchema = UsdGeom.Mesh(meshPrim)
+        self.assertTrue(meshSchema)
+
+        self._ValidateXformOps(meshPrim, expectedTranslation)
+
+        return meshSchema
 
     def testExportSingleLocatorMerged(self):
         """
         Tests that exporting a single Maya locator works correctly when
         exporting with merge transforms and shapes enabled.
         """
-        stage = Usd.Stage.Open(self._usdFilePathMerged)
+        stage = Usd.Stage.Open(self._singleLocatorUsdFilePathMerged)
         self.assertTrue(stage)
 
-        self._ValidateLocatorPrim(stage, '/SingleLocator')
+        expectedTranslation = Gf.Vec3d(1.0, 2.0, 3.0)
+        self._ValidateXformPrim(stage, '/SingleLocator', expectedTranslation)
 
     def testExportSingleLocatorUnmerged(self):
         """
         Tests that exporting a single Maya locator works correctly when
         exporting with merge transforms and shapes disabled.
         """
-        stage = Usd.Stage.Open(self._usdFilePathUnmerged)
+        stage = Usd.Stage.Open(self._singleLocatorUsdFilePathUnmerged)
         self.assertTrue(stage)
 
-        self._ValidateLocatorPrim(stage, '/SingleLocator')
+        expectedTranslation = Gf.Vec3d(1.0, 2.0, 3.0)
+        self._ValidateXformPrim(stage, '/SingleLocator', expectedTranslation)
 
         self._ValidateXformPrim(stage, '/SingleLocator/SingleLocatorShape')
 
@@ -120,10 +170,11 @@ class testUsdExportLocator(unittest.TestCase):
         exporting with merge transforms and shapes enabled and with a (trivial)
         frame range specified.
         """
-        stage = Usd.Stage.Open(self._usdFilePathMergedRanged)
+        stage = Usd.Stage.Open(self._singleLocatorUsdFilePathMergedRanged)
         self.assertTrue(stage)
 
-        self._ValidateLocatorPrim(stage, '/SingleLocator')
+        expectedTranslation = Gf.Vec3d(1.0, 2.0, 3.0)
+        self._ValidateXformPrim(stage, '/SingleLocator', expectedTranslation)
 
     def testExportSingleLocatorUnmergedRanged(self):
         """
@@ -131,12 +182,95 @@ class testUsdExportLocator(unittest.TestCase):
         exporting with merge transforms and shapes disabled and with a (trivial)
         frame range specified.
         """
-        stage = Usd.Stage.Open(self._usdFilePathUnmergedRanged)
+        stage = Usd.Stage.Open(self._singleLocatorUsdFilePathUnmergedRanged)
         self.assertTrue(stage)
 
-        self._ValidateLocatorPrim(stage, '/SingleLocator')
+        expectedTranslation = Gf.Vec3d(1.0, 2.0, 3.0)
+        self._ValidateXformPrim(stage, '/SingleLocator', expectedTranslation)
 
         self._ValidateXformPrim(stage, '/SingleLocator/SingleLocatorShape')
+
+    def testExportLocatorParentMerged(self):
+        """
+        Tests that exporting a cube parented underneath a Maya locator works
+        correctly when exporting with merge transforms and shapes enabled.
+        """
+        stage = Usd.Stage.Open(self._locatorParentUsdFilePathMerged)
+        self.assertTrue(stage)
+
+        expectedTranslation = Gf.Vec3d(1.0, 2.0, 3.0)
+        self._ValidateXformPrim(stage, '/LocatorParent', expectedTranslation)
+
+        # The locator does *not* get node collapsed because it has a cube
+        # underneath it, so we should have a blank Xform for the shape.
+        self._ValidateXformPrim(stage, '/LocatorParent/LocatorParentShape')
+
+        expectedTranslation = Gf.Vec3d(-2.0, -3.0, -4.0)
+        self._ValidateMeshPrim(stage, '/LocatorParent/CubeChild',
+            expectedTranslation)
+
+    def testExportLocatorParentUnmerged(self):
+        """
+        Tests that exporting a cube parented underneath a Maya locator works
+        correctly when exporting with merge transforms and shapes disabled.
+        """
+        stage = Usd.Stage.Open(self._locatorParentUsdFilePathUnmerged)
+        self.assertTrue(stage)
+
+        expectedTranslation = Gf.Vec3d(1.0, 2.0, 3.0)
+        self._ValidateXformPrim(stage, '/LocatorParent', expectedTranslation)
+
+        # The locator does *not* get node collapsed because it has a cube
+        # underneath it, so we should have a blank Xform for the shape.
+        self._ValidateXformPrim(stage, '/LocatorParent/LocatorParentShape')
+
+        expectedTranslation = Gf.Vec3d(-2.0, -3.0, -4.0)
+        self._ValidateXformPrim(stage, '/LocatorParent/CubeChild',
+            expectedTranslation)
+
+        self._ValidateMeshPrim(stage, '/LocatorParent/CubeChild/CubeChildShape')
+
+    def testExportLocatorParentMergedRanged(self):
+        """
+        Tests that exporting a cube parented underneath a Maya locator works
+        correctly when exporting with merge transforms and shapes enabled and
+        with a (trivial) frame range specified.
+        """
+        stage = Usd.Stage.Open(self._locatorParentUsdFilePathMergedRanged)
+        self.assertTrue(stage)
+
+        expectedTranslation = Gf.Vec3d(1.0, 2.0, 3.0)
+        self._ValidateXformPrim(stage, '/LocatorParent', expectedTranslation)
+
+        # The locator does *not* get node collapsed because it has a cube
+        # underneath it, so we should have a blank Xform for the shape.
+        self._ValidateXformPrim(stage, '/LocatorParent/LocatorParentShape')
+
+        expectedTranslation = Gf.Vec3d(-2.0, -3.0, -4.0)
+        self._ValidateMeshPrim(stage, '/LocatorParent/CubeChild',
+            expectedTranslation)
+
+    def testExportLocatorParentUnmergedRanged(self):
+        """
+        Tests that exporting a cube parented underneath a Maya locator works
+        correctly when exporting with merge transforms and shapes disabled and
+        with a (trivial) frame range specified.
+        """
+        stage = Usd.Stage.Open(self._locatorParentUsdFilePathUnmergedRanged)
+        self.assertTrue(stage)
+
+        expectedTranslation = Gf.Vec3d(1.0, 2.0, 3.0)
+        self._ValidateXformPrim(stage, '/LocatorParent', expectedTranslation)
+
+        # The locator does *not* get node collapsed because it has a cube
+        # underneath it, so we should have a blank Xform for the shape.
+        self._ValidateXformPrim(stage, '/LocatorParent/LocatorParentShape')
+
+        expectedTranslation = Gf.Vec3d(-2.0, -3.0, -4.0)
+        self._ValidateXformPrim(stage, '/LocatorParent/CubeChild',
+            expectedTranslation)
+
+        self._ValidateMeshPrim(stage, '/LocatorParent/CubeChild/CubeChildShape')
 
 
 if __name__ == '__main__':
