3e0ac4e618177f67aba23525516d435ef0ed0825
gitamohr <gitamohr@users.noreply.github.com>
2018-03-14 12:18:11 -0700

    plug: Make the plug task arena more transient, leads to a little boost removal too.
    (Internal change: 1837713)

--- pxr/base/lib/plug/registry.h
+++ pxr/base/lib/plug/registry.h
@@ -37,9 +37,6 @@
 #include "pxr/base/tf/weakBase.h"
 #include "pxr/base/tf/weakPtr.h"
 
-#include <boost/noncopyable.hpp>
-#include <boost/scoped_ptr.hpp>
-
 #include <mutex>
 #include <string>
 #include <vector>
@@ -336,7 +333,9 @@ struct Plug_RegistrationMetadata;
 /// \endcode
 ///
 
-class PlugRegistry : public TfWeakBase, boost::noncopyable {
+class PlugRegistry : public TfWeakBase {
+    PlugRegistry(PlugRegistry const &) = delete;
+    PlugRegistry &operator=(PlugRegistry const &) = delete;
 public:
     typedef PlugRegistry This;
     typedef std::vector<TfType> TypeVector;
@@ -458,8 +457,6 @@ private:
 
 private:
     TfHashSet<std::string, TfHash> _registeredPluginPaths;
-
-    boost::scoped_ptr<class Plug_TaskArena> _dispatcher;
 
     std::mutex _mutex;
 };
