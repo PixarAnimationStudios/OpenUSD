05739555c0457833d78ba866cf90477aa24b7f4e
unhyperbolic <unhyperbolic@users.noreply.github.com>
2018-03-20 16:29:54 -0700

    Simplifying GfPlane::IntersectsPositiveHalfSpace.
    (Internal change: 1838978)

--- pxr/base/lib/gf/plane.cpp
+++ pxr/base/lib/gf/plane.cpp
@@ -93,45 +93,23 @@ GfPlane::IntersectsPositiveHalfSpace(const GfRange3d &box) const
 {
     if (box.IsEmpty())
 	return false;
-    
-    // Test each vertex of the box against the positive half
-    // space. Since the box is aligned with the coordinate axes, we
-    // can test for a quick accept/reject at each stage.
 
-// This macro tests one corner using the given inequality operators.
-#define _GF_CORNER_TEST(X, Y, Z, XOP, YOP, ZOP)                               \
-    if (X + Y + Z >= _distance)                                               \
-        return true;                                                          \
-    else if (_normal[0] XOP 0.0 && _normal[1] YOP 0.0 && _normal[2] ZOP 0.0)  \
-        return false
+    // The maximum of the inner product between the normal and any point in the
+    // box.
+    double d = 0.0;
+    for (int i = 0; i < 3; i++) {
+        // Add the contributions each component makes to the inner product
+        // as the maximum of 
+        // _normal[i] * box.GetMin()[i] and _normal[i] * box.GetMax()[i].
+        // Depending on the sign of _normal[i], this will be the first or the
+        // second term.
+        const double b = (_normal[i] >= 0) ? box.GetMax()[i] : box.GetMin()[i];
+        d += _normal[i] * b;
+    }
 
-    // The sum of these values is GfDot(box.GetMin(), _normal)
-    double xmin = _normal[0] * box.GetMin()[0];
-    double ymin = _normal[1] * box.GetMin()[1];
-    double zmin = _normal[2] * box.GetMin()[2];
-
-    // We can do the all-min corner test right now.
-    _GF_CORNER_TEST(xmin, ymin, zmin, <=, <=, <=);
-
-    // The sum of these values is GfDot(box.GetMax(), _normal)
-    double xmax = _normal[0] * box.GetMax()[0];
-    double ymax = _normal[1] * box.GetMax()[1];
-    double zmax = _normal[2] * box.GetMax()[2];
-
-    // Do the other 7 corner tests.
-    _GF_CORNER_TEST(xmax, ymax, zmax, >=, >=, >=);
-    _GF_CORNER_TEST(xmin, ymin, zmax, <=, <=, >=);
-    _GF_CORNER_TEST(xmin, ymax, zmin, <=, >=, <=);
-    _GF_CORNER_TEST(xmin, ymax, zmax, <=, >=, >=);
-    _GF_CORNER_TEST(xmax, ymin, zmin, >=, <=, <=);
-    _GF_CORNER_TEST(xmax, ymin, zmax, >=, <=, >=);
-    _GF_CORNER_TEST(xmax, ymax, zmin, >=, >=, <=);
-
-    // CODE_COVERAGE_OFF - We should never get here, but just in case...
-    return false;
-    // CODE_COVERAGE_ON
-
-#undef _GF_CORNER_TEST
+    // If this inner product is larger than distance, we are in the positive
+    // half space.
+    return d >= _distance;
 }
 
 std::ostream &
