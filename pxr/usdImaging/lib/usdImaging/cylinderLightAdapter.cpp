a0541584d4c70acba183a9a37d16bd9840127a52
blevin <blevin@users.noreply.github.com>
2018-04-16 23:57:14 -0700

    UsdImaging: Disk, Distant, and Cylinder light adapaters
    (Internal change: 1844927)

---
+++ pxr/usdImaging/lib/usdImaging/cylinderLightAdapter.cpp
@@ -0,0 +1,72 @@
+//
+// Copyright 2017 Pixar
+//
+// Licensed under the Apache License, Version 2.0 (the "Apache License")
+// with the following modification; you may not use this file except in
+// compliance with the Apache License and the following modification to it:
+// Section 6. Trademarks. is deleted and replaced with:
+//
+// 6. Trademarks. This License does not grant permission to use the trade
+//    names, trademarks, service marks, or product names of the Licensor
+//    and its affiliates, except as required to comply with Section 4(c) of
+//    the License and to reproduce the content of the NOTICE file.
+//
+// You may obtain a copy of the Apache License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the Apache License with the above modification is
+// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied. See the Apache License for the specific
+// language governing permissions and limitations under the Apache License.
+//
+#include "pxr/usdImaging/usdImaging/cylinderLightAdapter.h"
+#include "pxr/usdImaging/usdImaging/delegate.h"
+#include "pxr/usdImaging/usdImaging/tokens.h"
+
+#include "pxr/imaging/hd/tokens.h"
+
+PXR_NAMESPACE_OPEN_SCOPE
+
+
+bool _IsEnabledSceneLights();
+
+TF_REGISTRY_FUNCTION(TfType)
+{
+    typedef UsdImagingCylinderLightAdapter Adapter;
+    TfType t = TfType::Define<Adapter, TfType::Bases<Adapter::BaseAdapter> >();
+    t.SetFactory< UsdImagingPrimAdapterFactory<Adapter> >();
+}
+
+UsdImagingCylinderLightAdapter::~UsdImagingCylinderLightAdapter() 
+{
+}
+
+bool
+UsdImagingCylinderLightAdapter::IsSupported(UsdImagingIndexProxy const* index) const
+{
+    return _IsEnabledSceneLights() &&
+           index->IsSprimTypeSupported(HdPrimTypeTokens->cylinderLight);
+}
+
+SdfPath
+UsdImagingCylinderLightAdapter::Populate(UsdPrim const& prim, 
+                            UsdImagingIndexProxy* index,
+                            UsdImagingInstancerContext const* instancerContext)
+{
+    index->InsertSprim(HdPrimTypeTokens->cylinderLight, prim.GetPath(), prim);
+    HD_PERF_COUNTER_INCR(UsdImagingTokens->usdPopulatedPrimCount);
+
+    return prim.GetPath();
+}
+
+void
+UsdImagingCylinderLightAdapter::_RemovePrim(SdfPath const& cachePath,
+                                         UsdImagingIndexProxy* index)
+{
+    index->RemoveSprim(HdPrimTypeTokens->cylinderLight, cachePath);
+}
+
+
+PXR_NAMESPACE_CLOSE_SCOPE
