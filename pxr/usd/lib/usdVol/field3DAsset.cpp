47d08e4c0663778367b4258e15a2181483f19383
Mark Tucker <mtucker@sidefx.com>
2018-04-25 23:42:07 -0400

    Adding first draft of usdVol schemas.

---
+++ pxr/usd/lib/usdVol/field3DAsset.cpp
@@ -0,0 +1,174 @@
+//
+// Copyright 2016 Pixar
+//
+// Licensed under the Apache License, Version 2.0 (the "Apache License")
+// with the following modification; you may not use this file except in
+// compliance with the Apache License and the following modification to it:
+// Section 6. Trademarks. is deleted and replaced with:
+//
+// 6. Trademarks. This License does not grant permission to use the trade
+//    names, trademarks, service marks, or product names of the Licensor
+//    and its affiliates, except as required to comply with Section 4(c) of
+//    the License and to reproduce the content of the NOTICE file.
+//
+// You may obtain a copy of the Apache License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the Apache License with the above modification is
+// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied. See the Apache License for the specific
+// language governing permissions and limitations under the Apache License.
+//
+#include "./field3DAsset.h"
+#include "pxr/usd/usd/schemaRegistry.h"
+#include "pxr/usd/usd/typed.h"
+
+#include "pxr/usd/sdf/types.h"
+#include "pxr/usd/sdf/assetPath.h"
+
+PXR_NAMESPACE_OPEN_SCOPE
+
+// Register the schema with the TfType system.
+TF_REGISTRY_FUNCTION(TfType)
+{
+    TfType::Define<UsdVolField3DAsset,
+        TfType::Bases< UsdVolFieldAsset > >();
+    
+    // Register the usd prim typename as an alias under UsdSchemaBase. This
+    // enables one to call
+    // TfType::Find<UsdSchemaBase>().FindDerivedByName("Field3DAsset")
+    // to find TfType<UsdVolField3DAsset>, which is how IsA queries are
+    // answered.
+    TfType::AddAlias<UsdSchemaBase, UsdVolField3DAsset>("Field3DAsset");
+}
+
+/* virtual */
+UsdVolField3DAsset::~UsdVolField3DAsset()
+{
+}
+
+/* static */
+UsdVolField3DAsset
+UsdVolField3DAsset::Get(const UsdStagePtr &stage, const SdfPath &path)
+{
+    if (!stage) {
+        TF_CODING_ERROR("Invalid stage");
+        return UsdVolField3DAsset();
+    }
+    return UsdVolField3DAsset(stage->GetPrimAtPath(path));
+}
+
+/* static */
+UsdVolField3DAsset
+UsdVolField3DAsset::Define(
+    const UsdStagePtr &stage, const SdfPath &path)
+{
+    static TfToken usdPrimTypeName("Field3DAsset");
+    if (!stage) {
+        TF_CODING_ERROR("Invalid stage");
+        return UsdVolField3DAsset();
+    }
+    return UsdVolField3DAsset(
+        stage->DefinePrim(path, usdPrimTypeName));
+}
+
+/* static */
+const TfType &
+UsdVolField3DAsset::_GetStaticTfType()
+{
+    static TfType tfType = TfType::Find<UsdVolField3DAsset>();
+    return tfType;
+}
+
+/* static */
+bool 
+UsdVolField3DAsset::_IsTypedSchema()
+{
+    static bool isTyped = _GetStaticTfType().IsA<UsdTyped>();
+    return isTyped;
+}
+
+/* virtual */
+const TfType &
+UsdVolField3DAsset::_GetTfType() const
+{
+    return _GetStaticTfType();
+}
+
+UsdAttribute
+UsdVolField3DAsset::GetFieldNameAttr() const
+{
+    return GetPrim().GetAttribute(UsdVolTokens->fieldName);
+}
+
+UsdAttribute
+UsdVolField3DAsset::CreateFieldNameAttr(VtValue const &defaultValue, bool writeSparsely) const
+{
+    return UsdSchemaBase::_CreateAttr(UsdVolTokens->fieldName,
+                       SdfValueTypeNames->Token,
+                       /* custom = */ false,
+                       SdfVariabilityVarying,
+                       defaultValue,
+                       writeSparsely);
+}
+
+UsdAttribute
+UsdVolField3DAsset::GetFieldIndexAttr() const
+{
+    return GetPrim().GetAttribute(UsdVolTokens->fieldIndex);
+}
+
+UsdAttribute
+UsdVolField3DAsset::CreateFieldIndexAttr(VtValue const &defaultValue, bool writeSparsely) const
+{
+    return UsdSchemaBase::_CreateAttr(UsdVolTokens->fieldIndex,
+                       SdfValueTypeNames->Int,
+                       /* custom = */ false,
+                       SdfVariabilityVarying,
+                       defaultValue,
+                       writeSparsely);
+}
+
+namespace {
+static inline TfTokenVector
+_ConcatenateAttributeNames(const TfTokenVector& left,const TfTokenVector& right)
+{
+    TfTokenVector result;
+    result.reserve(left.size() + right.size());
+    result.insert(result.end(), left.begin(), left.end());
+    result.insert(result.end(), right.begin(), right.end());
+    return result;
+}
+}
+
+/*static*/
+const TfTokenVector&
+UsdVolField3DAsset::GetSchemaAttributeNames(bool includeInherited)
+{
+    static TfTokenVector localNames = {
+        UsdVolTokens->fieldName,
+        UsdVolTokens->fieldIndex,
+    };
+    static TfTokenVector allNames =
+        _ConcatenateAttributeNames(
+            UsdVolFieldAsset::GetSchemaAttributeNames(true),
+            localNames);
+
+    if (includeInherited)
+        return allNames;
+    else
+        return localNames;
+}
+
+PXR_NAMESPACE_CLOSE_SCOPE
+
+// ===================================================================== //
+// Feel free to add custom code below this line. It will be preserved by
+// the code generator.
+//
+// Just remember to wrap code in the appropriate delimiters:
+// 'PXR_NAMESPACE_OPEN_SCOPE', 'PXR_NAMESPACE_CLOSE_SCOPE'.
+// ===================================================================== //
+// --(BEGIN CUSTOM CODE)--
---
+++ pxr/usd/lib/usdVol/fieldAsset.h
@@ -0,0 +1,161 @@
+//
+// Copyright 2016 Pixar
+//
+// Licensed under the Apache License, Version 2.0 (the "Apache License")
+// with the following modification; you may not use this file except in
+// compliance with the Apache License and the following modification to it:
+// Section 6. Trademarks. is deleted and replaced with:
+//
+// 6. Trademarks. This License does not grant permission to use the trade
+//    names, trademarks, service marks, or product names of the Licensor
+//    and its affiliates, except as required to comply with Section 4(c) of
+//    the License and to reproduce the content of the NOTICE file.
+//
+// You may obtain a copy of the Apache License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the Apache License with the above modification is
+// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied. See the Apache License for the specific
+// language governing permissions and limitations under the Apache License.
+//
+#ifndef USDVOL_GENERATED_FIELDASSET_H
+#define USDVOL_GENERATED_FIELDASSET_H
+
+/// \file usdVol/fieldAsset.h
+
+#include "pxr/pxr.h"
+#include "./api.h"
+#include "./fieldBase.h"
+#include "pxr/usd/usd/prim.h"
+#include "pxr/usd/usd/stage.h"
+#include "./tokens.h"
+
+#include "pxr/base/vt/value.h"
+
+#include "pxr/base/gf/vec3d.h"
+#include "pxr/base/gf/vec3f.h"
+#include "pxr/base/gf/matrix4d.h"
+
+#include "pxr/base/tf/token.h"
+#include "pxr/base/tf/type.h"
+
+PXR_NAMESPACE_OPEN_SCOPE
+
+class SdfAssetPath;
+
+// -------------------------------------------------------------------------- //
+// FIELDASSET                                                                 //
+// -------------------------------------------------------------------------- //
+
+/// \class UsdVolFieldAsset
+///
+/// Base class for field primitives defined by an external file.
+///
+class UsdVolFieldAsset : public UsdVolFieldBase
+{
+public:
+    /// Compile-time constant indicating whether or not this class corresponds
+    /// to a concrete instantiable prim type in scene description.  If this is
+    /// true, GetStaticPrimDefinition() will return a valid prim definition with
+    /// a non-empty typeName.
+    static const bool IsConcrete = false;
+
+    /// Compile-time constant indicating whether or not this class inherits from
+    /// UsdTyped. Types which inherit from UsdTyped can impart a typename on a
+    /// UsdPrim.
+    static const bool IsTyped = true;
+
+    /// Construct a UsdVolFieldAsset on UsdPrim \p prim .
+    /// Equivalent to UsdVolFieldAsset::Get(prim.GetStage(), prim.GetPath())
+    /// for a \em valid \p prim, but will not immediately throw an error for
+    /// an invalid \p prim
+    explicit UsdVolFieldAsset(const UsdPrim& prim=UsdPrim())
+        : UsdVolFieldBase(prim)
+    {
+    }
+
+    /// Construct a UsdVolFieldAsset on the prim held by \p schemaObj .
+    /// Should be preferred over UsdVolFieldAsset(schemaObj.GetPrim()),
+    /// as it preserves SchemaBase state.
+    explicit UsdVolFieldAsset(const UsdSchemaBase& schemaObj)
+        : UsdVolFieldBase(schemaObj)
+    {
+    }
+
+    /// Destructor.
+    USDVOL_API
+    virtual ~UsdVolFieldAsset();
+
+    /// Return a vector of names of all pre-declared attributes for this schema
+    /// class and all its ancestor classes.  Does not include attributes that
+    /// may be authored by custom/extended methods of the schemas involved.
+    USDVOL_API
+    static const TfTokenVector &
+    GetSchemaAttributeNames(bool includeInherited=true);
+
+    /// Return a UsdVolFieldAsset holding the prim adhering to this
+    /// schema at \p path on \p stage.  If no prim exists at \p path on
+    /// \p stage, or if the prim at that path does not adhere to this schema,
+    /// return an invalid schema object.  This is shorthand for the following:
+    ///
+    /// \code
+    /// UsdVolFieldAsset(stage->GetPrimAtPath(path));
+    /// \endcode
+    ///
+    USDVOL_API
+    static UsdVolFieldAsset
+    Get(const UsdStagePtr &stage, const SdfPath &path);
+
+
+private:
+    // needs to invoke _GetStaticTfType.
+    friend class UsdSchemaRegistry;
+    USDVOL_API
+    static const TfType &_GetStaticTfType();
+
+    static bool _IsTypedSchema();
+
+    // override SchemaBase virtuals.
+    USDVOL_API
+    virtual const TfType &_GetTfType() const;
+
+public:
+    // --------------------------------------------------------------------- //
+    // FILEPATH 
+    // --------------------------------------------------------------------- //
+    /// An asset path attribute that points to a file on disk.
+    ///
+    /// \n  C++ Type: SdfAssetPath
+    /// \n  Usd Type: SdfValueTypeNames->Asset
+    /// \n  Variability: SdfVariabilityVarying
+    /// \n  Fallback Value: No Fallback
+    USDVOL_API
+    UsdAttribute GetFilePathAttr() const;
+
+    /// See GetFilePathAttr(), and also 
+    /// \ref Usd_Create_Or_Get_Property for when to use Get vs Create.
+    /// If specified, author \p defaultValue as the attribute's default,
+    /// sparsely (when it makes sense to do so) if \p writeSparsely is \c true -
+    /// the default for \p writeSparsely is \c false.
+    USDVOL_API
+    UsdAttribute CreateFilePathAttr(VtValue const &defaultValue = VtValue(), bool writeSparsely=false) const;
+
+public:
+    // ===================================================================== //
+    // Feel free to add custom code below this line, it will be preserved by 
+    // the code generator. 
+    //
+    // Just remember to: 
+    //  - Close the class declaration with }; 
+    //  - Close the namespace with PXR_NAMESPACE_CLOSE_SCOPE
+    //  - Close the include guard with #endif
+    // ===================================================================== //
+    // --(BEGIN CUSTOM CODE)--
+};
+
+PXR_NAMESPACE_CLOSE_SCOPE
+
+#endif
