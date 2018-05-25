55f04cdd29651a0b48c505816598e4e1c1e799fe
hauvalerie <hauvalerie@users.noreply.github.com>
2018-05-25 11:04:43 -0700

    Checking cropping parameters in uvTextureData.cpp's Read function to avoid calling GlfImage::ReadCropped on invalid cropping parameters.  Valid crop paramters are those such that the resulting cropped image's width and height are both greater than 0 and less than the input image's Width and Height, respectively.
    (Internal change: 1859193)
    (Internal change: 1859268)

--- pxr/imaging/lib/glf/uvTextureData.cpp
+++ pxr/imaging/lib/glf/uvTextureData.cpp
@@ -258,6 +258,20 @@ GlfUVTextureData::_ReadDegradedImageInput(bool generateMipmap,
         numMipLevels-1, numMipLevels);
 }
 
+static bool
+_IsValidCrop(GlfImageSharedPtr image,
+             int cropTop, int cropBottom, int cropLeft, int cropRight)
+{
+    int cropImageWidth = image->GetWidth() - (cropLeft + cropRight);
+    int cropImageHeight = image->GetHeight() - (cropTop + cropBottom);
+    return (cropTop >= 0 && 
+            cropBottom >= 0 &&
+            cropLeft >= 0 &&
+            cropRight >= 0 &&
+            cropImageWidth > 0 &&
+            cropImageHeight > 0); 
+}
+
 bool
 GlfUVTextureData::Read(int degradeLevel, bool generateMipmap)
 {   
@@ -320,6 +334,12 @@ GlfUVTextureData::Read(int degradeLevel, bool generateMipmap)
             cropBottom = ceil(_params.cropBottom * degradedImage.scaleY);
             cropLeft   = ceil(_params.cropLeft * degradedImage.scaleX);
             cropRight  = ceil(_params.cropRight * degradedImage.scaleX);
+
+            //Check that cropping parameters are valid
+            if (!_IsValidCrop(image, cropTop, cropBottom, cropLeft, cropRight)) {
+                TF_CODING_ERROR("Failed to load Texture - Invalid crop");
+                return false;
+            }
 
             _resizedWidth = std::max(0, _resizedWidth - (cropLeft + cropRight));
             _resizedHeight = std::max(0, _resizedHeight - (cropTop + cropBottom));
