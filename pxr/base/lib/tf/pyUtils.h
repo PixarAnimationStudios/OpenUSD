e21387bc1128e9febb37e0f22dde52d9055ee677
jloy <jloy@users.noreply.github.com>
2018-08-30 12:24:12 -0700

    tf: add a result converter for Python sets
    A number of APIs return Python lists for no reason other than
    TfPySequenceToList is easy to use and TfPySequenceToSet didn't exist
    because these conversions pre-date Python's builtin set type.  This
    change implements TfPySequenceToSet along with some tests for the
    existing sequence converters.
    
    (Internal change: 1883470)

--- pxr/base/lib/tf/pyUtils.h
+++ pxr/base/lib/tf/pyUtils.h
@@ -42,6 +42,7 @@
 
 #include <boost/python/dict.hpp>
 #include <boost/python/extract.hpp>
+#include <boost/python/handle.hpp>
 #include <boost/python/object.hpp>
 #include <boost/python/type_id.hpp>
 
@@ -241,6 +242,26 @@ boost::python::list TfPyCopySequenceToList(Seq const &seq) {
          i != seq.end(); ++i)
         l.append(*i);
     return l; 
+}
+
+/// Create a python set from an iterable sequence.
+///
+/// If Seq::value_type is not hashable, TypeError is raised via throwing
+/// boost::python::error_already_set.
+template <class Seq>
+boost::python::object TfPyCopySequenceToSet(Seq const &seq) {
+    TfPyLock lock;
+    boost::python::handle<> set{boost::python::allow_null(PySet_New(nullptr))};
+    if (!set) {
+        boost::python::throw_error_already_set();
+    }
+    for (auto const& item : seq) {
+        boost::python::object obj(item);
+        if (PySet_Add(set.get(), obj.ptr()) == -1) {
+            boost::python::throw_error_already_set();
+        }
+    }
+    return boost::python::object(set);
 }
 
 template<class Seq>
