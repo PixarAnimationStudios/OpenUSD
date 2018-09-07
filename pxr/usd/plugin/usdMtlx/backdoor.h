bea2a7afb42ec6c551c91c2139ff9f629119604b
c64kernal <c64kernal@users.noreply.github.com>
2018-09-07 12:09:19 -0700

    A few small changes to get the MaterialX plugin to build on Windows.
    Fixes #611
    
    (Internal change: 1891094)

--- pxr/usd/plugin/usdMtlx/backdoor.h
+++ pxr/usd/plugin/usdMtlx/backdoor.h
@@ -25,6 +25,9 @@
 #define USDMTLX_BACKDOOR_H
  
 #include "pxr/pxr.h"
+
+#include "pxr/usd/usdMtlx/api.h"
+
 #include "pxr/base/tf/declarePtrs.h"
 #include <string>
 
@@ -35,12 +38,14 @@ TF_DECLARE_WEAK_AND_REF_PTRS(UsdStage);
 /// Return MaterialX XML in \p buffer converted to a USD stage.
 /// This is to allow testing from Python.  If \p nodeGraphs is true
 /// then only node graphs are read, otherwise everything else is read.
+USDMTLX_API
 UsdStageRefPtr UsdMtlx_TestString(const std::string& buffer,
                                   bool nodeGraphs = false);
 
 /// Return MaterialX XML in file at \p pathname converted to a USD stage.
 /// This is to allow testing from Python.  If \p nodeGraphs is true
 /// then only node graphs are read, otherwise everything else is read.
+USDMTLX_API
 UsdStageRefPtr UsdMtlx_TestFile(const std::string& pathname,
                                 bool nodeGraphs = false);
 
