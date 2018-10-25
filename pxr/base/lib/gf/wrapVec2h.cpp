ec213443aef5d67bc36c22086b1115b5a166e365
pixar-oss <pixar-oss@users.noreply.github.com>
2018-10-24 17:38:22 -0700

    Remove duplicated include file from GfVec*.
    (Internal change: 1904685)

--- pxr/base/lib/gf/wrapVec2h.cpp
+++ pxr/base/lib/gf/wrapVec2h.cpp
@@ -32,7 +32,6 @@
 
 #include "pxr/base/tf/pyContainerConversions.h"
 #include "pxr/base/tf/pyUtils.h"
-#include "pxr/base/tf/pyContainerConversions.h"
 #include "pxr/base/tf/stringUtils.h"
 #include "pxr/base/tf/wrapTypeHelpers.h"
 
