7be8742e8e118a28e6bec77dd21afa4d2750eb8a
pixar-oss <pixar-oss@users.noreply.github.com>
2019-02-18 14:19:31 -0800

    -- moving point instancer to std::map version of VtMapOrCopy to remove need for reverse
    (Internal change: 1940108)

--- third_party/katana/lib/usdKatana/readPointInstancer.cpp
+++ third_party/katana/lib/usdKatana/readPointInstancer.cpp
@@ -560,20 +560,18 @@ PxrUsdKatanaReadPointInstancer(
 
 #if KATANA_VERSION_MAJOR >= 3
     // If motion is backwards, make sure to reverse time samples.
-    std::vector<float> attrSampleTimes(motionSampleTimes.size());
-    std::transform(motionSampleTimes.begin(), motionSampleTimes.end(),
-        attrSampleTimes.begin(), [&data](double relSampleTime){
-            return data.IsMotionBackward()
-                ? PxrUsdKatanaUtils::ReverseTimeSample(relSampleTime)
-                : relSampleTime;
-        });
-    if (data.IsMotionBackward()){
-        std::reverse(attrSampleTimes.begin(), attrSampleTimes.end());
-        std::reverse(xformSamples.begin(), xformSamples.end());
+    std::map<float, VtArray<GfMatrix4d>> timeToSampleMap;
+    for (size_t a = 0; a < numXformSamples; ++a) {
+        double relSampleTime = motionSampleTimes[a];
+        timeToSampleMap.insert(
+            {data.IsMotionBackward()
+                 ? PxrUsdKatanaUtils::ReverseTimeSample(relSampleTime)
+                 : relSampleTime,
+             xformSamples[a]});
     }
-    auto instanceMatrixAttr = VtKatanaMapOrCopy(attrSampleTimes, xformSamples);
-    instancesBldr.setAttrAtLocation("instances",
-            "geometry.instanceMatrix", instanceMatrixAttr);
+    auto instanceMatrixAttr = VtKatanaMapOrCopy(timeToSampleMap);
+    instancesBldr.setAttrAtLocation("instances", "geometry.instanceMatrix",
+                                    instanceMatrixAttr);
 #else
     FnKat::DoubleBuilder instanceMatrixBldr(16);
     for (size_t a = 0; a < numXformSamples; ++a) {
