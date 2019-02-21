c7ec9edc3155b2eb2089bf22f9a623abdf34a550
gitamohr <gitamohr@users.noreply.github.com>
2019-02-20 16:24:38 -0800

    arch: Define the glibc-honored yet nonstandard macros that expose constant and printf format macros for sized int types in inttypes.h.
    (Internal change: 1941120)

--- pxr/base/lib/arch/inttypes.h
+++ pxr/base/lib/arch/inttypes.h
@@ -30,12 +30,27 @@
 ///
 /// By including this file, the "standard" integer types \c int16_t,
 /// \c int32_t, and \c int64_t are all defined, as are their unsigned
-/// counterparts \c uint16_t, \c uint32_t, and \c uint64_t.
+/// counterparts \c uint16_t, \c uint32_t, and \c uint64_t.  This also
+/// includes the macros for limits, constants, and printf format specifiers.
+
+// These defines should in theory not be needed to get the related sized-int
+// macros, as this was not adopted by the C++ committee and was dropped by the C
+// committee, but glibc erroneously "respects" them so we need to have them.
+#ifndef __STDC_LIMIT_MACROS
+#define __STDC_LIMIT_MACROS
+#endif
+#ifndef __STDC_CONSTANT_MACROS
+#define __STDC_CONSTANT_MACROS
+#endif
+#ifndef __STDC_FORMAT_MACROS
+#define __STDC_FORMAT_MACROS
+#endif
+
+#include <cinttypes>
+#include <cstdint>
 
 #include "pxr/pxr.h"
 #include "pxr/base/arch/defines.h"
-#include <stdint.h>
-#include <inttypes.h>
 #include <sys/types.h>
 
 PXR_NAMESPACE_OPEN_SCOPE
