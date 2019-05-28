b945e979ba315d140a9d0f8959e221a879fbf4c8
tcauchois <tcauchois@users.noreply.github.com>
2019-05-27 22:54:06 -0700

    [HdSt] Support instance-rate primvar queries in shading code.
    Previously, if we had an instance-rate primvar "displayColor" (for example), CodeGen would generate the accessors:
    vec3 HdGet_displayColor_0();
    vec3 HdGetInstance_displayColor(int level, vec3 default);
    ... but these aren't discoverable for shading code, which is explicitly ignorant of whether the primvar is instance rate or prototype rate.
    
    Those two accessors are still provided, for the HdSt instancing shaders, but we now generate a user facing accessor:
    vec3 HdGet_displayColor() {
      return HdGet_displayColor_0();
    }
    
    Prototype primvars take precedence over instance-rate, so this is only defined if there's no prototype-rate displayColor (as tested with #if !defined(HD_HAS_displayColor)).
    
    (Internal change: 1973334)

--- pxr/imaging/lib/hdSt/codeGen.cpp
+++ pxr/imaging/lib/hdSt/codeGen.cpp
@@ -1831,6 +1831,35 @@ HdSt_CodeGen::_GenerateInstancePrimvar()
         accessors << "  return defaultValue;\n"
                   << "}\n";
     }
+    /*
+      common accessor, if the primvar is defined on the instancer but not
+      the rprim.
+
+      #if !defined(HD_HAS_translate)
+      #define HD_HAS_translate 1
+      vec3 HdGet_translate(int localIndex) {
+          // 0 is the lowest level for which this is defined
+          return HdGet_translate_0();
+      }
+      vec3 HdGet_translate() {
+          return HdGet_translate(0);
+      }
+      #endif
+    */
+    TF_FOR_ALL (it, nameAndLevels) {
+        accessors << "#if !defined(HD_HAS_" << it->first << ")\n"
+                  << "#define HD_HAS_" << it->first << " 1\n"
+                  << _GetUnpackedType(it->second.dataType, false)
+                  << " HdGet_" << it->first << "(int localIndex) {\n"
+                  << "  return HdGet_" << it->first << "_"
+                                       << it->second.levels.front() << "();\n"
+                  << "}\n"
+                  << _GetUnpackedType(it->second.dataType, false)
+                  << " HdGet_" << it->first << "() { return HdGet_"
+                  << it->first << "(0); }\n"
+                  << "#endif\n";
+    }
+        
 
     _genCommon << declarations.str()
                << accessors.str();
