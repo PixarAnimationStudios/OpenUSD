502276bb02368cba0ddea6006a84c6507b0d93fe
emeroo <emeroo@users.noreply.github.com>
2019-07-01 16:42:01 -0700

    Remove the "geo" bridge location from prototype paths in Katana. The "geo" location was created as a stub for transitioning from BuildIntermediate to PxrUsdIn as part of building out prototypes under a point instancer. Prior to change 1793604, this bridge location did not have access to PxrUsdIn private data, meaning that it couldn't represent a location coming from USD. To work around this limitation, we created the stateless "geo" location and inserted it into the original hierarchy. However, thanks to change 1793604, the bridge location can now be a USD location instead of a stateless stub. Tell BuildIntermediate to start building just above the prototype (or the "common ancestor" between it and its Looks). The common ancestor will be read as if it were any other USD location.
    Since BuildIntermediate now starts building one scope above where it used to, we can't author attrs on the common ancestor in the same way as before. Simply nix the "info.usd.sourceUsdPath" tracking attr, since it is unused. As far as the "instance source" type attr goes, it turns out that using the common ancestor as the instance source instead of the prototype doesn't work in RfK 22, so this change also includes a modification to how we tag instance sources for point instancer prototypes.
    
    When building out point instancer prototypes, identify each prototype as an instance source rather than identifying the common ancestor as an instance source. RfK 21 was more permissive about instance source rules, and allowed an instance array to have geometry.instanceSource entries that did NOT correspond to instance source locations. However, RfK 22 is not as lax, and now expects the geometry.instanceSource entries to be, as the name implies, instance sources.
    
    The classic way to identify an instance source is by changing its type to "instance source". However, this causes problems if the prototype's USD type (and therefore its Katana type) holds special meaning. So, in the cases where the prototype's native Katana type is deemed important, insert an empty group into the hierarchy to hold the "instance source" type. This requires PxrUsdIn to be aware of a new tag stored in the static scene attributes ("insertEmptyGroup"). If this attribute is present, PxrUsdIn will pass the current prim to createChild, as if it were its own child, and early exit, thereby turning the prim's original Katana location into an empty group.
    
    See #834
    See #835
    
    (Internal change: 1985251)

--- third_party/katana/plugin/pxrUsdIn/pxrUsdIn.cpp
+++ third_party/katana/plugin/pxrUsdIn/pxrUsdIn.cpp
@@ -251,6 +251,58 @@ public:
                 readerLock.lock();
             }
 
+            //
+            // If the staticScene attrs dictate that we should insert an empty
+            // group at this location, configure the interface to build out the
+            // current prim as if it were its own child and return before
+            // executing any registered ops.
+            //
+            // Note, the inserted group will have the same name as the current
+            // prim.
+            //
+            // Note, we assume that the staticScene contents are aware of the
+            // inserted group, i.e. that the current contents apply to the empty
+            // group and there is a child entry for the current prim.
+            //
+
+            FnKat::GroupAttribute staticScene =
+                    interface.getOpArg("staticScene");
+            if (FnKat::IntAttribute(staticScene.getChildByName(
+                    "a.insertEmptyGroup")).getValue(0, false) == 1)
+            {
+                // Execute any ops contained within the staticScene args
+                _ExecStaticSceneOps(interface, staticScene);
+
+                const std::string primName = prim.GetName();
+
+                // Same caveat as the one mentioned in BuildIntermediate...
+                //
+                // XXX In order for the prim's material hierarchy to get built
+                // out correctly via the PxrUsdInCore_LooksGroupOp, we'll need
+                // to override the original 'rootLocation' and 'isolatePath'
+                // UsdIn args.
+                //
+                ArgsBuilder ab;
+                ab.update(usdInArgs);
+                ab.rootLocation =
+                        interface.getOutputLocationPath() + "/" + primName;
+                ab.isolatePath = prim.GetPath().GetString();
+
+                interface.createChild(
+                        primName,
+                        "",
+                        FnKat::GroupBuilder()
+                            .update(opArgs)
+                            .set("staticScene", staticScene.getChildByName(
+                                "c." + primName))
+                            .build(),
+                        FnKat::GeolibCookInterface::ResetRootFalse,
+                        new PxrUsdKatanaUsdInPrivateData(prim, ab.build(),
+                                privateData),
+                        PxrUsdKatanaUsdInPrivateData::Delete);
+                return;
+            }
+
             // When in "as sources and instances" mode, scan for instances
             // and masters at each location that contains a payload.
             if (prim.HasAuthoredPayloads() &&
@@ -402,34 +454,9 @@ public:
             // Execute any ops contained within the staticScene args.
             //
 
-            FnKat::GroupAttribute opGroups = opArgs.getChildByName("staticScene.x");
-            if (opGroups.isValid())
-            {
-                for (int childindex = 0; childindex < opGroups.getNumberOfChildren();
-                        ++childindex)
-                {
-                    FnKat::GroupAttribute entry =
-                            opGroups.getChildByIndex(childindex);
-
-                    if (!entry.isValid())
-                    {
-                        continue;
-                    }
-
-                    FnKat::StringAttribute subOpType =
-                            entry.getChildByName("opType");
-
-                    FnKat::GroupAttribute subOpArgs =
-                            entry.getChildByName("opArgs");
-
-                    if (!subOpType.isValid() || !subOpArgs.isValid())
-                    {
-                        continue;
-                    }
-
-                    interface.execOp(subOpType.getValue("", false), subOpArgs);
-                }
-            }
+            // Re-get the latest staticScene in case anything changed
+            staticScene = interface.getOpArg("staticScene");
+            _ExecStaticSceneOps(interface, staticScene);
 
         }   // if (prim.GetPath() != SdfPath::AbsoluteRootPath())
         
@@ -972,6 +999,41 @@ private:
         }
 
         return boundsAttr;
+    }
+
+    static void
+    _ExecStaticSceneOps(
+            FnKat::GeolibCookInterface& interface,
+            FnKat::GroupAttribute& staticScene)
+    {
+        FnKat::GroupAttribute opsGroup = staticScene.getChildByName("x");
+        if (opsGroup.isValid())
+        {
+            for (int childindex = 0; childindex < opsGroup.getNumberOfChildren();
+                    ++childindex)
+            {
+                FnKat::GroupAttribute entry =
+                        opsGroup.getChildByIndex(childindex);
+
+                if (!entry.isValid())
+                {
+                    continue;
+                }
+
+                FnKat::StringAttribute subOpType =
+                        entry.getChildByName("opType");
+
+                FnKat::GroupAttribute subOpArgs =
+                        entry.getChildByName("opArgs");
+
+                if (!subOpType.isValid() || !subOpArgs.isValid())
+                {
+                    continue;
+                }
+
+                interface.execOp(subOpType.getValue("", false), subOpArgs);
+            }
+        }
     }
 
     static bool _hasSiteKinds;
