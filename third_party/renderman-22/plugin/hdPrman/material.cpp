f3e9307fae9b5c13747dc8b9b6fa6f10d4211842
blevin <blevin@users.noreply.github.com>
2019-07-15 10:09:38 -0700

    HdPrman: Support "volume" terminal in Hydra materials
    (Internal change: 1987314)

--- third_party/renderman-22/plugin/hdPrman/material.cpp
+++ third_party/renderman-22/plugin/hdPrman/material.cpp
@@ -49,6 +49,7 @@ TF_DEFINE_PRIVATE_TOKENS(
     (MaterialLayer_2)
     (bxdf)
     (displacement)
+    (volume)
     (pbsMaterialIn)
     (inputMaterial)
     (OSL)
@@ -437,7 +438,8 @@ _MapHdNodesToRileyNodes(
 
         // Create equivalent Riley shading node.
         riley::ShadingNode sn;
-        if (shaders[i]->GetContext() == _tokens->bxdf) {
+        if (shaders[i]->GetContext() == _tokens->bxdf ||
+            shaders[i]->GetContext() == SdrNodeContext->Volume) {
             sn.type = riley::ShadingNode::k_Bxdf;
         } else if (shaders[i]->GetContext() == SdrNodeContext->Pattern ||
                    shaders[i]->GetContext() == _tokens->OSL) {
@@ -752,7 +754,7 @@ _ConvertHdMaterialToRman(
     _FindShaders(mat.nodes, &shaders);
     _ExpandVstructs(&mat, shaders);
     _MapHdNodesToRileyNodes(mgr, mat, shaders, result);
-    return true;
+    return !result->empty();
 }
 
 /* virtual */
@@ -779,9 +781,10 @@ HdPrmanMaterial::Sync(HdSceneDelegate *sceneDelegate,
             HdPrman_ConvertUsdPreviewMaterial(&networkMap);
             _ApplyStudioFixes(&networkMap);
 
-            HdMaterialNetwork bxdfNet, dispNet;
+            HdMaterialNetwork bxdfNet, dispNet, volNet;
             TfMapLookup(networkMap.map, _tokens->bxdf, &bxdfNet);
             TfMapLookup(networkMap.map, _tokens->displacement, &dispNet);
+            TfMapLookup(networkMap.map, _tokens->volume, &volNet);
 
             if (TfDebug::IsEnabled(HDPRMAN_MATERIALS)) {
                 if (!bxdfNet.nodes.empty()) {
@@ -790,12 +793,16 @@ HdPrmanMaterial::Sync(HdSceneDelegate *sceneDelegate,
                 if (!dispNet.nodes.empty()) {
                     HdPrman_DumpMat("Displacement", id, dispNet);
                 }
+                if (!volNet.nodes.empty()) {
+                    HdPrman_DumpMat("Volume", id, volNet);
+                }
             }
 
             std::vector<riley::ShadingNode> nodes;
 
-            // Bxdf
-            if (_ConvertHdMaterialToRman(mgr, bxdfNet, &nodes)) {
+            // Surface/Volume
+            if (_ConvertHdMaterialToRman(mgr, bxdfNet, &nodes) ||
+                _ConvertHdMaterialToRman(mgr, volNet, &nodes)) {
                 if (_materialId == riley::MaterialId::k_InvalidId) {
                     _materialId = riley->CreateMaterial(&nodes[0],
                                                         nodes.size());
@@ -815,9 +822,9 @@ HdPrmanMaterial::Sync(HdSceneDelegate *sceneDelegate,
                 mgr->DestroyRixParamList(
                     const_cast<RixParamList*>(node.params));
             }
+            nodes.clear();
 
             // Displacement
-            nodes.clear();
             if (_ConvertHdMaterialToRman(mgr, dispNet, &nodes)) {
                 if (_displacementId == riley::DisplacementId::k_InvalidId) {
                     _displacementId = riley->CreateDisplacement(&nodes[0],
