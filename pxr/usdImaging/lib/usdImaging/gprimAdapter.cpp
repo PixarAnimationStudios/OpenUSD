3f21fc20a2d332e674cc4244334b1ed264210b03
klucknav <klucknav@users.noreply.github.com>
2019-08-05 14:11:55 -0700

    added check for non-empty extents
    (Internal change: 1989531)

--- pxr/usdImaging/lib/usdImaging/gprimAdapter.cpp
+++ pxr/usdImaging/lib/usdImaging/gprimAdapter.cpp
@@ -503,13 +503,14 @@ UsdImagingGprimAdapter::_GetExtent(UsdPrim const& prim, UsdTimeCode time) const
     HF_MALLOC_TAG_FUNCTION();
     UsdGeomGprim gprim(prim);
     VtVec3fArray extent;
-    if (gprim.GetExtentAttr().Get(&extent, time)) {
+    if (gprim.GetExtentAttr().Get(&extent, time) && extent.size() == 2) {
         // Note:
         // Usd stores extent as 2 float vecs. We do an implicit 
         // conversion to doubles
         return GfRange3d(extent[0], extent[1]);
     } else {
-        // Return empty range if no value was found.
+        // Return empty range if no value was found, or the wrong number of 
+        // extent values were provided.        
         // Note: The default empty is [FLT_MAX,-FLT_MAX].
         // TODO: Should this compute the extent based on the points instead?
         return GfRange3d();
