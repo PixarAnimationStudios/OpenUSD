e910a9db3c9ab43e8a76575d94849e35210c8208
pixar-oss <pixar-oss@users.noreply.github.com>
2020-02-04 17:34:15 -0800

    Fix color banding when using OCIO.
    (Internal change: 2039765)

--- pxr/imaging/hdx/colorCorrectionTask.cpp
+++ pxr/imaging/hdx/colorCorrectionTask.cpp
@@ -179,8 +179,8 @@ HdxColorCorrectionTask::_CreateOpenColorIOResources()
         glGetIntegerv(GL_TEXTURE_BINDING_3D, &restoreTexture);
         glGenTextures(1, &_texture3dLUT);
         glBindTexture(GL_TEXTURE_3D, _texture3dLUT);
-        glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
