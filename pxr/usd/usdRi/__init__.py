d6339f0e9111b8a3652bf81c4d047f340f4ac43a
sunyab <sunyab@users.noreply.github.com>
2020-03-20 11:44:06 -0700

    [UsdRi] Changes for Python 3 support
    This commit is based off work done by NVIDIA and SideFX.
    
    (Internal change: 2049376)

--- pxr/usd/usdRi/__init__.py
+++ pxr/usd/usdRi/__init__.py
@@ -21,7 +21,7 @@
 # KIND, either express or implied. See the Apache License for the specific
 # language governing permissions and limitations under the Apache License.
 #
-import _usdRi
+from . import _usdRi
 from pxr import Tf
 Tf.PrepareModule(_usdRi, locals())
 del Tf
