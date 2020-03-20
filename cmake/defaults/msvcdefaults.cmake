acf7c132affd6489b2f96c8b94d689e7508a90e6
sunyab <sunyab@users.noreply.github.com>
2020-03-20 11:57:21 -0700

    Disable /Zc:inline on Visual Studio 2019
    Starting in VS2019, the /Zc:inline option causes the compiler to
    discard the 'arch_ctor_<NAME>' symbols used by ARCH_CONSTRUCTOR,
    which causes the '.pxrctor' section to go missing. This causes
    expected functions to not be called during library initialization,
    leading to crashes. This has been reported at:
    
    https://developercommunity.visualstudio.com/content/problem/914943/zcinline-removes-extern-symbols-inside-anonymous-n.html
    
    In the meantime, this change disables /Zc:inline on VS2019 and
    later versions to work around the issue. Note that this does
    disable a C++11 conformance check related to inline function
    definitions. However, test builds on other platforms should
    catch these issues if they are mistakenly introduced.
    
    Fixes #1095
    
    (Internal change: 2049624)

--- cmake/defaults/msvcdefaults.cmake
+++ cmake/defaults/msvcdefaults.cmake
@@ -26,7 +26,20 @@
 set(_PXR_CXX_FLAGS "${_PXR_CXX_FLAGS} /EHsc")
 
 # Standards compliant.
-set(_PXR_CXX_FLAGS "${_PXR_CXX_FLAGS} /Zc:rvalueCast /Zc:strictStrings /Zc:inline")
+set(_PXR_CXX_FLAGS "${_PXR_CXX_FLAGS} /Zc:rvalueCast /Zc:strictStrings")
+
+# The /Zc:inline option strips out the "arch_ctor_<name>" symbols used for
+# library initialization by ARCH_CONSTRUCTOR starting in Visual Studio 2019, 
+# causing release builds to fail. Disable the option for this and later 
+# versions.
+# 
+# For more details, see:
+# https://developercommunity.visualstudio.com/content/problem/914943/zcinline-removes-extern-symbols-inside-anonymous-n.html
+if (MSVC_VERSION GREATER_EQUAL 1920)
+    set(_PXR_CXX_FLAGS "${_PXR_CXX_FLAGS} /Zc:inline-")
+else()
+    set(_PXR_CXX_FLAGS "${_PXR_CXX_FLAGS} /Zc:inline")
+endif()
 
 # Turn on all but informational warnings.
 set(_PXR_CXX_FLAGS "${_PXR_CXX_FLAGS} /W3")
