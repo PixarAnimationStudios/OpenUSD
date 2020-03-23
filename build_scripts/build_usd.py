7f64522efd0f8bd484e535793cf3ea791504f191
sunyab <sunyab@users.noreply.github.com>
2020-03-23 00:25:38 -0700

    Additional fixes to build_usd.py for Visual Studio 2019
    boost support for Visual Studio 2019 was added in version
    1.70.0, so update the version of boost installed and make
    sure we specify the correct 'toolset' setting to b2.
    For simplicity, also use 1.70.0 for older release of
    Visual Studio as well.
    
    Fixes #1041
    
    (Internal change: 2050374)

--- build_scripts/build_usd.py
+++ build_scripts/build_usd.py
@@ -575,18 +575,15 @@ elif MacOS():
     BOOST_URL = "https://downloads.sourceforge.net/project/boost/boost/1.61.0/boost_1_61_0.tar.gz"
     BOOST_VERSION_FILE = "include/boost/version.hpp"
 elif Windows():
-    # On Visual Studio 2017 we need at least boost 1.65.1
     # The default installation of boost on Windows puts headers in a versioned 
     # subdirectory, which we have to account for here. In theory, specifying 
     # "layout=system" would make the Windows install match Linux/MacOS, but that 
     # causes problems for other dependencies that look for boost.
-    if IsVisualStudio2017OrGreater():
-        BOOST_URL = "https://downloads.sourceforge.net/project/boost/boost/1.65.1/boost_1_65_1.tar.gz"
-        BOOST_VERSION_FILE = "include/boost-1_65_1/boost/version.hpp"
-    else:
-        BOOST_URL = "https://downloads.sourceforge.net/project/boost/boost/1.61.0/boost_1_61_0.tar.gz"
-        BOOST_VERSION_FILE = "include/boost-1_61/boost/version.hpp"
-
+    #
+    # boost 1.70 is required for Visual Studio 2019. For simplicity, we use
+    # this version for all older Visual Studio versions as well.
+    BOOST_URL = "https://downloads.sourceforge.net/project/boost/boost/1.70.0/boost_1_70_0.tar.gz"
+    BOOST_VERSION_FILE = "include/boost-1_70/boost/version.hpp"
 
 def InstallBoost(context, force, buildArgs):
     # Documentation files in the boost archive can have exceptionally
@@ -654,9 +651,11 @@ def InstallBoost(context, force, buildArgs):
             b2_settings.append("-a")
 
         if Windows():
+            # toolset parameter for Visual Studio documented here:
+            # https://github.com/boostorg/build/blob/develop/src/tools/msvc.jam
             if IsVisualStudio2019OrGreater():
-                pass
-            if IsVisualStudio2017OrGreater():
+                b2_settings.append("toolset=msvc-14.2")
+            elif IsVisualStudio2017OrGreater():
                 b2_settings.append("toolset=msvc-14.1")
             else:
                 b2_settings.append("toolset=msvc-14.0")
