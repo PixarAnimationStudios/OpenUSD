a54d5c9fd053dce089306f8fc24fde060baea401
c64kernal <c64kernal@users.noreply.github.com>
2020-03-27 00:41:37 -0700

    Remove unused boost headers.
    (Internal change: 2051395)

--- pxr/imaging/hd/changeTracker.h
+++ pxr/imaging/hd/changeTracker.h
@@ -34,7 +34,6 @@
 
 #include <atomic>
 #include <boost/noncopyable.hpp>
-#include <boost/shared_ptr.hpp>
 #include <boost/unordered_map.hpp>
 #include <boost/weak_ptr.hpp>
 
--- pxr/imaging/hd/vtBufferSource.h
+++ pxr/imaging/hd/vtBufferSource.h
@@ -35,7 +35,6 @@
 #include "pxr/base/vt/value.h"
 
 #include <vector>
-#include <boost/shared_ptr.hpp>
 
 #include <iosfwd>
 
