585373f956c9fe8514681b94e7a4c9a2a4e5b16f
lumonix <lumonix@users.noreply.github.com>
2020-05-14 21:49:52 -0700

    Nvidia/AMD/old OS interop fixes for MacOS
    (Internal change: 2068978)

--- pxr/imaging/hgiInterop/metal.mm
+++ pxr/imaging/hgiInterop/metal.mm
@@ -297,12 +297,18 @@ HgiInteropMetal::HgiInteropMetal(Hgi* hgi)
         "{\n"
         "    vec2 uv = vec2(texCoord.x, 1.0 - texCoord.y) * texSize;\n"
         "#if __VERSION__ >= 140\n"
+        "    vec4 encodedDepth = texture(depthTexture, uv.st);\n"
+        "#else\n"
+        "    vec4 encodedDepth = texture2DRect(depthTexture, uv.st);\n"
+        "#endif\n"
+        "    float depth = dot(encodedDepth,\n"
+        "        vec4(1.0, 1 / 255.0, 1 / 65025.0, 1 / 16581375.0) );\n"
+        "#if __VERSION__ >= 140\n"
         "    fragColor = texture(interopTexture, uv.st);\n"
-        "    gl_FragDepth = texture(depthTexture, uv.st).r;\n"
         "#else\n"
         "    gl_FragColor = texture2DRect(interopTexture, uv.st);\n"
-        "    gl_FragDepth = texture2DRect(depthTexture, uv.st).r;\n"
         "#endif\n"
+        "    gl_FragDepth = depth;\n"
         "}\n";
 
     GLuint fsColor = _compileShader(fragmentShaderColor, GL_FRAGMENT_SHADER);
@@ -338,7 +344,18 @@ HgiInteropMetal::HgiInteropMetal(Hgi* hgi)
         "{\n"
         "    if(gid.x >= texOut.get_width() || gid.y >= texOut.get_height())\n"
         "        return;\n"
-        "    texOut.write(float(texIn.read(gid)), gid);\n"
+        "    float depth = texIn.read(gid);\n"
+        // This works around an issue with AMD Vega incorrectly resolving
+        // depth buffers when there's an empty render target performing the
+        // load/resolve
+        "    float maxDepth = 1.0f - FLT_EPSILON;\n"
+        "    depth = depth == 0.0f?maxDepth:min(maxDepth, depth);\n"
+        "    float4 encodedDepth =\n"
+        "        float4(1.0f, 255.0f, 65025.0f, 16581375.0f) * depth;\n"
+        "    encodedDepth = fract(encodedDepth);\n"
+        "    encodedDepth -= encodedDepth.yzww *\n"
+        "        float4(1.0 / 255.0, 1.0 / 255.0, 1.0 / 255.0, 0.0);\n"
+        "    texOut.write(encodedDepth, gid);\n"
         "}\n"
         "\n"
         "kernel void copyColour(\n"
@@ -546,7 +563,6 @@ HgiInteropMetal::_SetAttachmentSize(int width, int height)
         width,
         height,
         kCVPixelFormatType_32BGRA,
-        //kCVPixelFormatType_64RGBAHalf,
         (__bridge CFDictionaryRef)cvBufferProperties,
         &_pixelBuffer);
     
@@ -554,7 +570,7 @@ HgiInteropMetal::_SetAttachmentSize(int width, int height)
         kCFAllocatorDefault,
         width,
         height,
-        kCVPixelFormatType_OneComponent32Float,
+        kCVPixelFormatType_32BGRA,
         (__bridge CFDictionaryRef)cvBufferProperties,
         &_depthBuffer);
     
@@ -595,7 +611,6 @@ HgiInteropMetal::_SetAttachmentSize(int width, int height)
         _cvmtlTextureCache,
         _pixelBuffer,
         (__bridge CFDictionaryRef)metalTextureProperties,
-        //MTLPixelFormatRGBA16Float,
         MTLPixelFormatBGRA8Unorm,
         width,
         height,
@@ -614,7 +629,7 @@ HgiInteropMetal::_SetAttachmentSize(int width, int height)
         _cvmtlTextureCache,
         _depthBuffer,
         (__bridge CFDictionaryRef)metalTextureProperties,
-        MTLPixelFormatR32Float,
+        MTLPixelFormatBGRA8Unorm,
         width,
         height,
         0,
