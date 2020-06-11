d7dd991f3cecd7fa17bd97942fee4b392b8dbcd5
unhyperbolic <unhyperbolic@users.noreply.github.com>
2020-06-11 10:24:02 -0700

    Making GlimRgDrawTargetTask use HgiGraphicsCmdsDesc to bind textures from HdStDrawTarget as render target instead of binding a GlfDrawTarget when the env var to enable the Storm texture system for draw targets is true. This way Glim geometry can be rendered into a draw target even if the Storm texture system is used.
    (Internal change: 2075794)

--- pxr/imaging/hdx/drawTargetTask.cpp
+++ pxr/imaging/hdx/drawTargetTask.cpp
@@ -472,12 +472,9 @@ HdxDrawTargetTask::Sync(HdSceneDelegate* delegate,
     *dirtyBits = HdChangeTracker::Clean;
 }
 
-// Compute attachment descriptors for the draw target so that
-// the draw target textures are used as render targets.
-//
-static
 HgiGraphicsCmdsDesc
-_MakeGraphicsCmdsDesc(HdStDrawTarget * const drawTarget)
+HdxDrawTargetTask::MakeGraphicsCmdsDesc(HdStDrawTarget * const drawTarget,
+    const bool clear)
 {
     static const HgiTextureHandle empty;
 
@@ -503,7 +500,8 @@ _MakeGraphicsCmdsDesc(HdStDrawTarget * const drawTarget)
         
         HgiAttachmentDesc attachmentDesc;
         attachmentDesc.loadOp =
-            HgiAttachmentLoadOpClear;
+            clear ? HgiAttachmentLoadOpClear
+                  : HgiAttachmentLoadOpLoad;
         attachmentDesc.storeOp =
             isMultisampled ? HgiAttachmentStoreOpDontCare
                            : HgiAttachmentStoreOpStore;
@@ -547,7 +545,8 @@ HdxDrawTargetTask::Prepare(HdTaskContext* ctx,
 
             // Make render pass use these textures as render targets.
             info.renderPassState->SetCustomGraphicsCmdsDesc(
-                _MakeGraphicsCmdsDesc(info.target));
+                MakeGraphicsCmdsDesc(info.target,
+                                     /* clear = */ true));
         }
     }
 }
