30607c0bac7a0a9b29d3e8eae0ddce1a671ad571
tallytalwar <tallytalwar@users.noreply.github.com>
2020-07-09 09:54:29 -0700

    Cleanup top level CmakeLists to check for cmake min version before applying project policy.
    Also updates CXXDefaults to match on "Clang" to include both "Clang" and "AppleClang".
    Thanks to @foundry-markf for the suggestion.
    
    Fixes #1241
    
    (Internal change: 2082096)

--- cmake/defaults/CXXDefaults.cmake
+++ cmake/defaults/CXXDefaults.cmake
@@ -32,7 +32,7 @@ set(CMAKE_CXX_EXTENSIONS OFF)
 
 if (CMAKE_COMPILER_IS_GNUCXX)
     include(gccdefaults)
-elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+elseif ("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
     include(clangdefaults)
 elseif(MSVC)
     include(msvcdefaults)
