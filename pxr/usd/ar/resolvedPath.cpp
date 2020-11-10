df313e267f6ab5f812108fbceb317ffb80404360
sunyab <sunyab@users.noreply.github.com>
2020-11-09 16:29:44 -0800

    [ar 2.0] Add ArResolvedPath
    This object will be used in Ar 2.0 whereever a resolved
    path is required instead of a std::string. This reduces
    confusion around whether a particular function requires
    a resolved path via the type system.
    
    (Internal change: 2124829)

--- pxr/usd/ar/resolvedPath.cpp
+++ pxr/usd/ar/resolvedPath.cpp
@@ -1,5 +1,5 @@
 //
-// Copyright 2016 Pixar
+// Copyright 2020 Pixar
 //
 // Licensed under the Apache License, Version 2.0 (the "Apache License")
 // with the following modification; you may not use this file except in
@@ -21,23 +21,6 @@
 // KIND, either express or implied. See the Apache License for the specific
 // language governing permissions and limitations under the Apache License.
 //
-
 #include "pxr/pxr.h"
-#include "pxr/base/tf/pyModule.h"
 
-PXR_NAMESPACE_USING_DIRECTIVE
-
-TF_WRAP_MODULE
-{
-    TF_WRAP(ResolvedPath);
-
-    TF_WRAP(Resolver);
-    TF_WRAP(ResolverContext);
-    TF_WRAP(ResolverContextBinder);
-    TF_WRAP(ResolverScopedCache);
-
-    TF_WRAP(DefaultResolver);
-    TF_WRAP(DefaultResolverContext);
-
-    TF_WRAP(PackageUtils);
-}
+#include "pxr/usd/ar/resolvedPath.h"
