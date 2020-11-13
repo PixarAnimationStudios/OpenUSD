74a851daa627453e1edbb2b824fbea7312ad59c0
mattyjams <mattyjams@users.noreply.github.com>
2020-11-13 13:13:04 -0800

    [MaterialX] consider both static and dynamic libraries when finding MATERIALX_LIB_DIRS
    (Internal change: 2126171)

--- cmake/modules/FindMaterialX.cmake
+++ cmake/modules/FindMaterialX.cmake
@@ -67,13 +67,16 @@ find_path(MATERIALX_INCLUDE_DIRS
 )
 
 if (WIN32)
-    set(MATERIALX_CORE_LIB_NAME MaterialXCore.lib)
+    set(MATERIALX_CORE_STATIC_LIB_NAME MaterialXCore.lib)
+    set(MATERIALX_CORE_DYNAMIC_LIB_NAME MaterialXCore.dll)
 else()
-    set(MATERIALX_CORE_LIB_NAME libMaterialXCore.a)
+    set(MATERIALX_CORE_STATIC_LIB_NAME libMaterialXCore.a)
+    set(MATERIALX_CORE_DYNAMIC_LIB_NAME libMaterialXCore.so)
 endif()
 
 find_path(MATERIALX_LIB_DIRS 
-    "${MATERIALX_CORE_LIB_NAME}"
+    "${MATERIALX_CORE_STATIC_LIB_NAME}"
+    "${MATERIALX_CORE_DYNAMIC_LIB_NAME}"
     HINTS
         "${MATERIALX_ROOT}"
         "$ENV{MATERIALX_ROOT}"        
