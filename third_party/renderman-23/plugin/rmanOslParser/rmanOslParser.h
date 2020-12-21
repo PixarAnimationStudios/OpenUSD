1ae156dac3378d86f59a496e1fd44ec9a56845ce
sunyab <sunyab@users.noreply.github.com>
2020-12-20 19:32:28 -0800

    Fix bug that caused parameters from a shader to be incorrectly carried over to the next shader that was parsed.
    This was due to RmanOslParserPlugin reusing the same
    RixShaderQuery for each shader it handled. Although the
    documentation for RixShaderQuery/RixShaderInfo state
    that clients can re-use the object by calling Open
    multiple times with different shaders, there appears
    to be a bug where doing so causes parameters from
    the first shader to get carried over to the next one,
    and so on. The fix simply creates a new RixShaderQuery
    object for each shader it parses.
    
    Fixes #1396
    
    (Internal change: 2131039)

--- third_party/renderman-23/plugin/rmanOslParser/rmanOslParser.h
+++ third_party/renderman-23/plugin/rmanOslParser/rmanOslParser.h
@@ -124,11 +124,13 @@ private:
     // Gets a vector of properties that are present on the specified OSL
     // query object
     NdrPropertyUniquePtrVec _getNodeProperties(
+        const RixShaderQuery* sq,
         const NdrNodeDiscoveryResult& discoveryResult) const;
 
     // Gets all metadata for the node that's present on the specified OSL
     // query object
     NdrTokenMap _getNodeMetadata(
+        const RixShaderQuery* sq,
         const NdrTokenMap &baseMetadata) const;
 
     // Gets all metadata for the specified OSL parameter
@@ -152,10 +154,6 @@ private:
         size_t arraySize,
         const NdrTokenMap& metadata
     ) const;
-
-private:
-    RixShaderQuery* m_sq;
-
 };
 
 PXR_NAMESPACE_CLOSE_SCOPE
