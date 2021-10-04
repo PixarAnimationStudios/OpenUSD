9ac848acb72364d42b46a5809bd9bc4e5dd192ca
Paul Molodowitch <paul@molodowitch.com>
2021-10-04 12:49:09 -0700

    fix for importing pxr.Tf on windows anaconda 3.8+ interpreters
    cpython changed the way the dlls are loaded on windows for python-3.8+.
    USD implemented a workaround for this change, installed into
    pxr.__init__ (and Tf.__init__).  However, anaconda python interpreters
    were patched to behave more like pre-3.8:
    
       https://github.com/conda-forge/python-feedstock/blob/8195ba1178041b7461238e8c5680eee62f5ea9d0/recipe/patches/0020-Add-CondaEcosystemModifyDllSearchPath.patch#L37
    
    So we now check for anaconda interpreters before using the 3.8+ only
    fix.
    
    Fixes issue #1602

--- build_scripts/pypi/package_files/setup.py
+++ build_scripts/pypi/package_files/setup.py
@@ -74,8 +74,15 @@ import os, sys
 dllPath = os.path.split(os.path.realpath(__file__))[0]
 if sys.version_info >= (3, 8, 0):
     os.environ['PXR_USD_WINDOWS_DLL_PATH'] = dllPath
-else:
-    os.environ['PATH'] = dllPath + os.pathsep + os.environ['PATH']
+# Note that we ALWAYS modify the PATH, even for python-3.8+. This is because:
+#    - Anaconda python interpreters are modified to use the old, pre-3.8, PATH-
+#      based method of loading dlls
+#    - extra calls to os.add_dll_directory won't hurt these anaconda
+#      interpreters
+#    - similarly, adding the extra PATH entry shouldn't hurt standard python
+#      interpreters
+#    - there's no canonical/bulletproof way to check for an anaconda interpreter
+os.environ['PATH'] = dllPath + os.pathsep + os.environ['PATH']
 ''')
 
 # Get the readme text
