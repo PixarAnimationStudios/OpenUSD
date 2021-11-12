a8859a197bcdec8d4732212b4cab2cc5f6cfe211
pixar-oss <pixar-oss@users.noreply.github.com>
2021-11-12 10:11:40 -0800

    Enable rendering of in-memory VDB volumes in hdPrman. The ability to render a VDB volume defined by a .vdb file on disk is already supported in hdPrman. This change introduces support for vdb asset paths that specify something other than a file path and are resolvable by the ArAsset system. In this case, the memory addresses of OpenVDB grids held by an ArAsset are given to prman (using the RixStorage interface) and then rendered.
    (Internal change: 2199060)

--- pxr/imaging/hioOpenVDB/utils.h
+++ pxr/imaging/hioOpenVDB/utils.h
@@ -22,4 +22,29 @@
 // language governing permissions and limitations under the Apache License.
 //
 
-#include "pxr/imaging/hioOpenVDB/vdbAssetInterface.h"
+#ifndef PXR_IMAGING_HIO_OPENVDB_UTILS_H
+#define PXR_IMAGING_HIO_OPENVDB_UTILS_H
+
+/// \file hioOpenVDB/utils.h
+
+#include "pxr/pxr.h"
+#include "openvdb/openvdb.h"
+
+#include <string>
+
+PXR_NAMESPACE_OPEN_SCOPE
+
+/// Return a shared pointer to an OpenVDB grid with \p name (or nullptr
+/// if no grid matching \p name exists), given an \p assetPath.
+openvdb::GridBase::Ptr
+HioOpenVDBGridFromAsset(const std::string& name, const std::string& assetPath);
+
+/// Return a shared pointer to a vector of OpenVDB grids,
+/// given an \p assetPath.
+openvdb::GridPtrVecPtr
+HioOpenVDBGridsFromAsset(const std::string& assetPath);
+
+
+PXR_NAMESPACE_CLOSE_SCOPE
+
+#endif // PXR_IMAGING_HIO_OPENVDB_UTILS_H
