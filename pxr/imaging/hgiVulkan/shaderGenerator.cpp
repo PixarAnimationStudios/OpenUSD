ff97a96eb8b5a96e7ef69fee928f1674d6b0c53f
davidgyu <davidgyu@users.noreply.github.com>
2021-12-06 20:30:39 -0800

    [HgiVulkan] Fixed typo in shaderGenerator
    Fixes #1702
    
    (Internal change: 2204371)

--- pxr/imaging/hgiVulkan/shaderGenerator.cpp
+++ pxr/imaging/hgiVulkan/shaderGenerator.cpp
@@ -164,7 +164,7 @@ HgiVulkanShaderGenerator::_WriteInOuts(
         "gl_FragDepth"
     };
     const static std::map<std::string, std::string> takenInParams {
-        { HgiShaderKeywordTokens->hdPosition, "gl_Postiion"},
+        { HgiShaderKeywordTokens->hdPosition, "gl_Position"},
         { HgiShaderKeywordTokens->hdGlobalInvocationID, "gl_GlobalInvocationID"}
     };
 
