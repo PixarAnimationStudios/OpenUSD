29f23d5247d1ef947579d6e92359e5f54a794958
comand <comand@users.noreply.github.com>
2022-01-05 11:49:23 -0800

    [py3] fix pylint issue in testUsdPhysicsRigidBodyAPI.
    (Internal change: 2207692)

--- pxr/usd/usdPhysics/testenv/testUsdPhysicsRigidBodyAPI.py
+++ pxr/usd/usdPhysics/testenv/testUsdPhysicsRigidBodyAPI.py
@@ -22,6 +22,8 @@
 # KIND, either express or implied. See the Apache License for the specific
 # language governing permissions and limitations under the Apache License.
 
+from __future__ import print_function
+
 import sys, os, unittest
 from pxr import Tf, Usd, Sdf, UsdGeom, UsdShade, Gf, UsdPhysics
 
