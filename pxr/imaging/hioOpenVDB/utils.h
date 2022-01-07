3c1431c62b920443155ddfe3ae1dfd4068f41bd6
sunyab <sunyab@users.noreply.github.com>
2022-01-07 13:46:54 -0800

    Add missing symbol exports. This fixes build issues on Windows when building hdPrman with OpenVDB support enabled. This should have been part of change 2199060 but was missed because we were mistakenly not testing this build configuration.
    (Internal change: 2208616)

--- pxr/imaging/hioOpenVDB/utils.h
+++ pxr/imaging/hioOpenVDB/utils.h
@@ -28,22 +28,24 @@
 /// \file hioOpenVDB/utils.h
 
 #include "pxr/pxr.h"
-#include "openvdb/openvdb.h"
+#include "pxr/imaging/hioOpenVDB/api.h"
 
+#include "openvdb/openvdb.h"
 #include <string>
 
 PXR_NAMESPACE_OPEN_SCOPE
 
 /// Return a shared pointer to an OpenVDB grid with \p name (or nullptr
 /// if no grid matching \p name exists), given an \p assetPath.
+HIOOPENVDB_API
 openvdb::GridBase::Ptr
 HioOpenVDBGridFromAsset(const std::string& name, const std::string& assetPath);
 
 /// Return a shared pointer to a vector of OpenVDB grids,
 /// given an \p assetPath.
+HIOOPENVDB_API
 openvdb::GridPtrVecPtr
 HioOpenVDBGridsFromAsset(const std::string& assetPath);
-
 
 PXR_NAMESPACE_CLOSE_SCOPE
 
