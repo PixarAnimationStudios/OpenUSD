71ec813641e4e763db5f674d78efe533997eac1a
Benjamin A. Beasley <code@musicinmybrain.net>
2022-06-29 14:15:26 -0400

    Simplified patch with compatibility PyFrame_GetCode()
    Based on suggestions in https://docs.python.org/3.11/whatsnew/3.11.html#id6

--- pxr/base/tf/pyTracing.cpp
+++ pxr/base/tf/pyTracing.cpp
@@ -106,29 +106,29 @@ static void _SetTraceFnEnabled(bool enable) {
 }
 
 
+#if PY_VERSION_HEX < 0x030900B1
+// Define PyFrame_GetCode() on Python 3.8 and older:
+// https://docs.python.org/3.11/whatsnew/3.11.html#id6
+static inline PyCodeObject* PyFrame_GetCode(PyFrameObject *frame)
+{
+    Py_INCREF(frame->f_code);
+    return frame->f_code;
+}
+#endif
+
+
 static int _TracePythonFn(PyObject *, PyFrameObject *frame,
                           int what, PyObject *arg)
 {
     // Build up a trace info struct.
     TfPyTraceInfo info;
-#if PY_VERSION_HEX >= 0x030a00f0
-    // PyFrame_GetCode introduced in 3.9, added to Stable ABI in 3.10;
-    // PyFrameObject is opaque in 3.11
     PyCodeObject * code = PyFrame_GetCode(frame);
-#else
-    // Use direct access to PyFrameObject fields.
-    PyCodeObject * code = frame->f_code;
-    // Create a strong reference as PyFrame_GetCode() would, so we do not have
-    // to conditionalize decrementing the reference count.
-    Py_INCREF((PyObject *)code);
-#endif
     info.arg = arg;
     info.funcName = TfPyString_AsString(code->co_name);
     info.fileName = TfPyString_AsString(code->co_filename);
     info.funcLine = code->co_firstlineno;
     info.what = what;
-    // PyFrame_GetCode returned a strong reference
-    Py_DECREF((PyObject *)code);
+    Py_DECREF(code);
 
     _InvokeTraceFns(info);
 
