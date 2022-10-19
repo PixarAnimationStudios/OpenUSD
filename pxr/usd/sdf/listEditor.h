a072198974bc203ec88ab20488a1ae500bf2725b
sunyab <sunyab@users.noreply.github.com>
2022-10-19 12:16:10 -0700

    Add ability to remove duplicates in SdfListOp::ModifyOperations and update list editing proxies. This avoids coding errors that would occur if the modification callback produced duplicate values.
    The Sdf_ListEditor proxies are where the check for duplicate
    values currently happen, so these proxies all opt in to remove
    duplicates. SdfListOp itself does not do this by default to
    maintain current behavior, since it does not itself restrict
    duplicate values in lists.
    
    This change builds off of PR #1282 from @marktucker.
    
    Fixes #1282
    
    (Internal change: 2252577)

--- pxr/usd/sdf/listEditor.h
+++ pxr/usd/sdf/listEditor.h
@@ -126,7 +126,9 @@ public:
     /// Modifies the operations stored in all operation lists.
     /// \p callback is called for every key.  If the returned key is
     /// invalid then the key is removed, otherwise it's replaced with the
-    /// returned key.
+    /// returned key. If the returned key matches a key that was previously
+    /// returned for the list being processed, the returned key will be
+    /// removed.
     virtual void ModifyItemEdits(const ModifyCallback& cb) = 0;
 
     typedef std::function<
