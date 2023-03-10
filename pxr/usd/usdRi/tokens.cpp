594c0effbb9548b7de0117368e2fe847e5d66747
pixar-oss <pixar-oss@users.noreply.github.com>
2023-03-10 09:42:50 -0800

    Adding RiRenderPassAPI, which adds cameraVibility and matte collections
    (Internal change: 2266167)
    (Internal change: 2266309)

--- pxr/usd/usdRi/tokens.cpp
+++ pxr/usd/usdRi/tokens.cpp
@@ -27,10 +27,13 @@ PXR_NAMESPACE_OPEN_SCOPE
 
 UsdRiTokensType::UsdRiTokensType() :
     bspline("bspline", TfToken::Immortal),
+    cameraVisibility("cameraVisibility", TfToken::Immortal),
     catmullRom("catmull-rom", TfToken::Immortal),
+    collectionCameraVisibilityIncludeRoot("collection:cameraVisibility:includeRoot", TfToken::Immortal),
     constant("constant", TfToken::Immortal),
     interpolation("interpolation", TfToken::Immortal),
     linear("linear", TfToken::Immortal),
+    matte("matte", TfToken::Immortal),
     outputsRiDisplacement("outputs:ri:displacement", TfToken::Immortal),
     outputsRiSurface("outputs:ri:surface", TfToken::Immortal),
     outputsRiVolume("outputs:ri:volume", TfToken::Immortal),
@@ -39,14 +42,18 @@ UsdRiTokensType::UsdRiTokensType() :
     spline("spline", TfToken::Immortal),
     values("values", TfToken::Immortal),
     RiMaterialAPI("RiMaterialAPI", TfToken::Immortal),
+    RiRenderPassAPI("RiRenderPassAPI", TfToken::Immortal),
     RiSplineAPI("RiSplineAPI", TfToken::Immortal),
     StatementsAPI("StatementsAPI", TfToken::Immortal),
     allTokens({
         bspline,
+        cameraVisibility,
         catmullRom,
+        collectionCameraVisibilityIncludeRoot,
         constant,
         interpolation,
         linear,
+        matte,
         outputsRiDisplacement,
         outputsRiSurface,
         outputsRiVolume,
@@ -55,6 +62,7 @@ UsdRiTokensType::UsdRiTokensType() :
         spline,
         values,
         RiMaterialAPI,
+        RiRenderPassAPI,
         RiSplineAPI,
         StatementsAPI
     })
