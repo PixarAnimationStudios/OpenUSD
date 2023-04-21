8737760e44c5ab87c8c3122f11601efa52b46ff6
unhyperbolic <unhyperbolic@users.noreply.github.com>
2023-04-21 11:03:28 -0700

    Fixing header guards in HdStTerminalsResolvingSceneIndexPlugin
    (Internal change: 2272590)

--- pxr/imaging/hdSt/terminalsResolvingSceneIndexPlugin.cpp
+++ pxr/imaging/hdSt/terminalsResolvingSceneIndexPlugin.cpp
@@ -20,7 +20,7 @@
 // distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 // KIND, either express or implied. See the Apache License for the specific
 // language governing permissions and limitations under the Apache License.
-#include "terminalsResolvingSceneIndexPlugin.h"
+#include "pxr/imaging/hdSt/terminalsResolvingSceneIndexPlugin.h"
 
 #include "pxr/imaging/hdsi/terminalsResolvingSceneIndex.h"
 
