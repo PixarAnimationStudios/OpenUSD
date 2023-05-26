ef17bb74af3de75c998c1b78be2f1f6f106f85fa
cvj <cvj@users.noreply.github.com>
2023-05-26 09:32:44 -0700

    Make HdStRenderPassState::ComputeViewport use _GetFramebufferHeight when computing the viewport.
    (Internal change: 2278263)

--- pxr/imaging/hdSt/renderPassState.h
+++ pxr/imaging/hdSt/renderPassState.h
@@ -179,7 +179,7 @@ public:
     /// This is either using the modern camera framing, which is always y-down,
     /// or the legacy viewport.
     HDST_API
-    GfVec4i ComputeViewport(const HgiGraphicsCmdsDesc &desc) const;
+    GfVec4i ComputeViewport() const;
 
     // Helper to get graphics cmds descriptor describing textures
     // we render into and the blend state, constructed from
