808085aff1d51541d96e8b8dbd0e2cb7e9d48b1c
Thor Hjalmarsson <ihjalmarsson@apple.com>
2023-06-27 16:19:49 +0100

    Fix floating point error caused by FMA conversion

--- cmake/defaults/clangdefaults.cmake
+++ cmake/defaults/clangdefaults.cmake
@@ -26,6 +26,12 @@ include(gccclangshareddefaults)
 
 set(_PXR_CXX_FLAGS "${_PXR_GCC_CLANG_SHARED_CXX_FLAGS}")
 
+# Prevent floating point result discrepancies on Apple platforms
+# due to multiplication+additions being converted to FMA
+if (APPLE)
+    add_compile_options(-ffp-contract=off)
+endif()
+
 # clang annoyingly warns about the -pthread option if it's only linking.
 if(CMAKE_USE_PTHREADS_INIT)
     _disable_warning("unused-command-line-argument")
