8a2d71159f3e25a18e4fd9496a48c24ae4e8ab1e
Matt Kuruc <mkuruc@nvidia.com>
2023-07-11 22:09:14 -0700

    Remove unused `tf/preprocessorUtils.h` dependency

--- pxr/usd/sdf/types.h
+++ pxr/usd/sdf/types.h
@@ -58,7 +58,6 @@
 #include "pxr/base/gf/vec4h.h"
 #include "pxr/base/gf/vec4i.h"
 #include "pxr/base/tf/enum.h"
-#include "pxr/base/tf/preprocessorUtils.h"
 #include "pxr/base/tf/staticTokens.h"
 #include "pxr/base/tf/token.h"
 #include "pxr/base/tf/type.h"
