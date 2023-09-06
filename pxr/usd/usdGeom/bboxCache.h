f7a79a76d5394279cbc2286bb2ba6c10e80482c8
Matt Kuruc <mkuruc@nvidia.com>
2023-09-06 15:46:32 -0700

    Replace `boost::shared_array` with `std::shared_ptr` of C-style array

--- pxr/usd/usdGeom/bboxCache.h
+++ pxr/usd/usdGeom/bboxCache.h
@@ -35,7 +35,6 @@
 #include "pxr/base/work/dispatcher.h"
 
 #include <boost/optional.hpp>
-#include <boost/shared_array.hpp>
 
 PXR_NAMESPACE_OPEN_SCOPE
 
@@ -487,7 +486,7 @@ private:
 
         // Queries for attributes that need to be re-computed at each
         // time for this entry. This will be invalid for non-varying entries.
-        boost::shared_array<UsdAttributeQuery> queries;
+        std::shared_ptr<UsdAttributeQuery[]> queries;
 
         // Computed purpose info of the prim that's associated with the entry.
         // This data includes the prim's actual computed purpose as well as
