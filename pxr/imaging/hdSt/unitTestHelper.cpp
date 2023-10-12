7ebfcce5e8560525e08467f26448401127cf3d11
kapoorv <vipul.kapoor@autodesk.com>
2023-10-12 14:13:49 -0400

    Provides Unit test support to hgiVulkan backend.

--- pxr/imaging/hdSt/unitTestHelper.cpp
+++ pxr/imaging/hdSt/unitTestHelper.cpp
@@ -26,7 +26,7 @@
 #include "pxr/imaging/hdSt/resourceRegistry.h"
 #include "pxr/imaging/hdSt/textureUtils.h"
 #include "pxr/imaging/hd/vtBufferSource.h"
-
+#include "pxr/base/tf/envSetting.h"
 #include "pxr/base/tf/staticTokens.h"
 
 #include <iostream>
@@ -716,4 +716,3 @@ HdSt_TextureTestDriver::_PrintCompileErrors()
 }
 
 PXR_NAMESPACE_CLOSE_SCOPE
-
