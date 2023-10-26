ceb3699a622984eefd2876357ace96f554df04c4
matthewcpp <matthewcpp@users.noreply.github.com>
2023-10-26 13:34:05 -0700

    Refactor UsdUtils_UsdzPackage into common base class.
    This new class, UsdUtils_AssetLocalizationPackage will act as a base class for shared packaging functionality used by CreateNewUsdzPackage and to new forthcoming UsdUtilsLocalizaeAsset method.
    
    This base class handles the following tasks in a generic way:
    - Pprocessing dependencies and determining a relative package path
    - Performing remapping in the case that a non-reative path is encountered during localization.
    - Handling layers that were modified as part of the localization process
    - Invoke a virtual function to add a resolved AR path to a package relative path
    
    (Internal change: 2301707)

--- pxr/usd/usdUtils/debugCodes.cpp
+++ pxr/usd/usdUtils/debugCodes.cpp
@@ -31,8 +31,8 @@ PXR_NAMESPACE_OPEN_SCOPE
 
 TF_REGISTRY_FUNCTION(TfDebug)
 {
-    TF_DEBUG_ENVIRONMENT_SYMBOL(USDUTILS_CREATE_USDZ_PACKAGE, 
-                                "UsdUtils USDZ package creation details");
+    TF_DEBUG_ENVIRONMENT_SYMBOL(USDUTILS_CREATE_PACKAGE, 
+                                "UsdUtils asset localization details");
 }
 
 PXR_NAMESPACE_CLOSE_SCOPE
