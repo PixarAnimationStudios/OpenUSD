PixarAnimationStudios/OpenUSD

commit 28116f1390ee756cfcd25f4e26cd4e2707cb77f2

Support multiple command buffers in Metal to handle the Storm use case where we may have a blitCmds and a computeCmds recording at the same time.
We try to minimize the creation of additional command buffers in Metal for efficiency as they are heavy weight object. Encoders are lightweight, but we can only have one active encoder per command buffer. So we cannot have a blitCmds encoder and a computeCmds encoder recording at the same time unless we create an additional command buffer.

(Internal change: 2099174)

pxr/imaging/hgiMetal/computeCmds.h
@@ -82,8 +82,11 @@ private:
     HgiMetalComputeCmds & operator=(const HgiMetalComputeCmds&) = delete;
     HgiMetalComputeCmds(const HgiMetalComputeCmds&) = delete;
 
+    void _CreateEncoder();
+    
     HgiMetal* _hgi;
     HgiMetalComputePipeline* _pipelineState;
+    id<MTLCommandBuffer> _commandBuffer;
     id<MTLComputeCommandEncoder> _encoder;
     bool _hasWork;
 };

pxr/imaging/hgiMetal/computeCmds.mm
@@ -38,8 +38,10 @@ HgiMetalComputeCmds::HgiMetalComputeCmds(HgiMetal* hgi)
     : HgiComputeCmds()
     , _hgi(hgi)
     , _pipelineState(nullptr)
+    , _commandBuffer(nil)
+    , _encoder(nil)
 {
-    _encoder = [_hgi->GetCommandBuffer(false) computeCommandEncoder];
+    _CreateEncoder();
 }
 
 HgiMetalComputeCmds::~HgiMetalComputeCmds()
@@ -48,8 +50,22 @@ HgiMetalComputeCmds::~HgiMetalComputeCmds()
 }
 
 void
+HgiMetalComputeCmds::_CreateEncoder()
+{
+    if (!_encoder) {
+        id<MTLCommandBuffer> commandBuffer = _hgi->GetPrimaryCommandBuffer();
+        if (commandBuffer == nil) {
+            _commandBuffer = _hgi->GetSecondaryCommandBuffer();
+            commandBuffer = _commandBuffer;
+        }
+        _encoder = [commandBuffer computeCommandEncoder];
+    }
+}
+
+void
 HgiMetalComputeCmds::BindPipeline(HgiComputePipelineHandle pipeline)
 {
+    _CreateEncoder();
     _pipelineState = static_cast<HgiMetalComputePipeline*>(pipeline.Get());
     _pipelineState->BindPipeline(_encoder);
 }
@@ -60,6 +76,7 @@ HgiMetalComputeCmds::BindResources(HgiResourceBindingsHandle r)
     if (HgiMetalResourceBindings* rb=
         static_cast<HgiMetalResourceBindings*>(r.Get()))
     {
+        _CreateEncoder();
         rb->BindResources(_encoder);
     }
 }
@@ -71,6 +88,7 @@ HgiMetalComputeCmds::SetConstantValues(
     uint32_t byteSize,
     const void* data)
 {
+    _CreateEncoder();
     [_encoder setBytes:data
                 length:byteSize
                atIndex:bindIndex];
@@ -104,6 +122,7 @@ HgiMetalComputeCmds::Dispatch(int dimX, int dimY)
 void
 HgiMetalComputeCmds::PushDebugGroup(const char* label)
 {
+    _CreateEncoder();
     HGIMETAL_DEBUG_LABEL(_encoder, label)
 }
 
@@ -115,12 +134,36 @@ HgiMetalComputeCmds::PopDebugGroup()
 bool
 HgiMetalComputeCmds::_Submit(Hgi* hgi, HgiSubmitWaitType wait)
 {
+    bool submittedWork = false;
     if (_encoder) {
         [_encoder endEncoding];
         _encoder = nil;
-        return true;
+        submittedWork = true;
+
+        HgiMetal::CommitCommandBufferWaitType waitType;
+        switch(wait) {
+            case HgiSubmitWaitTypeNoWait:
+                waitType = HgiMetal::CommitCommandBuffer_NoWait;
+                break;
+            case HgiSubmitWaitTypeWaitUntilCompleted:
+                waitType = HgiMetal::CommitCommandBuffer_WaitUntilCompleted;
+                break;
+        }
+
+        if (_commandBuffer) {
+            _hgi->CommitSecondaryCommandBuffer(_commandBuffer, waitType);
+        }
+        else {
+            _hgi->CommitPrimaryCommandBuffer(waitType);
+        }
     }
-    return false;
+    
+    if (_commandBuffer) {
+        _hgi->ReleaseSecondaryCommandBuffer(_commandBuffer);
+        _commandBuffer = nil;
+    }
+
+    return submittedWork;
 }
 
 PXR_NAMESPACE_CLOSE_SCOPE

pxr/imaging/hgiMetal/graphicsCmds.mm
@@ -185,7 +185,8 @@ void
 HgiMetalGraphicsCmds::_CreateEncoder()
 {
     if (!_encoder) {
-        _encoder = [_hgi->GetCommandBuffer(false)
+        _encoder = [
+            _hgi->GetPrimaryCommandBuffer(false)
             renderCommandEncoderWithDescriptor:_renderPassDescriptor];
         
         if (_debugLabel) {
@@ -383,6 +384,18 @@ HgiMetalGraphicsCmds::_Submit(Hgi* hgi, HgiSubmitWaitType wait)
     if (_encoder) {
         [_encoder endEncoding];
         _encoder = nil;
+
+        HgiMetal::CommitCommandBufferWaitType waitType;
+        switch(wait) {
+            case HgiSubmitWaitTypeNoWait:
+                waitType = HgiMetal::CommitCommandBuffer_NoWait;
+                break;
+            case HgiSubmitWaitTypeWaitUntilCompleted:
+                waitType = HgiMetal::CommitCommandBuffer_WaitUntilCompleted;
+                break;
+        }
+
+        _hgi->CommitPrimaryCommandBuffer(waitType);
     }
 
     return _hasWork;

pxr/imaging/hgiMetal/hgi.h
@@ -51,7 +51,7 @@ public:
     enum CommitCommandBufferWaitType {
         CommitCommandBuffer_NoWait = 0,
         CommitCommandBuffer_WaitUntilScheduled,
-        CommitCommandBuffer_WaitUntilCompleted,
+        CommitCommandBuffer_WaitUntilCompleted
     };
     
     HGIMETAL_API
@@ -151,32 +151,39 @@ public:
     id<MTLDevice> GetPrimaryDevice() const;
 
     HGIMETAL_API
-    id<MTLCommandQueue> GetQueue() const {
-        return _commandQueue;
-    }
+    id<MTLCommandQueue> GetQueue() const;
+
+    // Metal Command buffers are heavy weight, while encoders are lightweight.
+    // But we cannot have more than one active encoder at a time per cmd buf.
+    // (Ideally we would have created on encoder for each HgiCmds)
+    // So for the sake of efficiency, we try to create only one cmd buf and
+    // only use the secondary command buffer when the client code requires it.
+    // For example, the client code may record in a HgiBlitCmds and a
+    // HgiComputeCmds at the same time.
+    HGIMETAL_API
+    id<MTLCommandBuffer> GetPrimaryCommandBuffer(bool flush = true);
+
+    HGIMETAL_API
+    id<MTLCommandBuffer> GetSecondaryCommandBuffer();
+
+    HGIMETAL_API
+    int GetAPIVersion() const;
     
     HGIMETAL_API
-    id<MTLCommandBuffer> GetCommandBuffer(bool flush = true) {
-        if (flush) {
-            _workToFlush = true;
-        }
-        return _commandBuffer;
-    }
+    HgiMetalCapabilities const & GetCapabilities() const;
     
     HGIMETAL_API
-    int GetAPIVersion() const {
-        return _apiVersion;
-    }
-    
-    HGIMETAL_API
-    HgiMetalCapabilities const & GetCapabilities() const {
-        return *_capabilities;
-    }
-    
-    HGIMETAL_API
-    void CommitCommandBuffer(
+    void CommitPrimaryCommandBuffer(
         CommitCommandBufferWaitType waitType = CommitCommandBuffer_NoWait,
         bool forceNewBuffer = false);
+
+    HGIMETAL_API
+    void CommitSecondaryCommandBuffer(
+        id<MTLCommandBuffer> commandBuffer,
+        CommitCommandBufferWaitType waitType);
+
+    HGIMETAL_API
+    void ReleaseSecondaryCommandBuffer(id<MTLCommandBuffer> commandBuffer);
 
 protected:
     HGIMETAL_API
@@ -198,6 +205,7 @@ private:
     id<MTLCommandQueue> _commandQueue;
     id<MTLCommandBuffer> _commandBuffer;
     id<MTLCaptureScope> _captureScopeFullFrame;
+    HgiCmds* _currentCmds;
 
     std::unique_ptr<HgiMetalCapabilities> _capabilities;
 

pxr/imaging/hgiMetal/hgi.mm
@@ -67,6 +67,7 @@ static int _GetAPIVersion()
 
 HgiMetal::HgiMetal(id<MTLDevice> device)
 : _device(device)
+, _currentCmds(nullptr)
 , _frameDepth(0)
 , _apiVersion(_GetAPIVersion())
 , _workToFlush(false)
@@ -136,13 +137,21 @@ HgiMetal::CreateGraphicsCmds(
 HgiComputeCmdsUniquePtr
 HgiMetal::CreateComputeCmds()
 {
-    return HgiComputeCmdsUniquePtr(new HgiMetalComputeCmds(this));
+    HgiComputeCmds* computeCmds = new HgiMetalComputeCmds(this);
+    if (!_currentCmds) {
+        _currentCmds = computeCmds;
+    }
+    return HgiComputeCmdsUniquePtr(computeCmds);
 }
 
 HgiBlitCmdsUniquePtr
 HgiMetal::CreateBlitCmds()
 {
-    return HgiBlitCmdsUniquePtr(new HgiMetalBlitCmds(this));
+    HgiMetalBlitCmds* blitCmds = new HgiMetalBlitCmds(this);
+    if (!_currentCmds) {
+        _currentCmds = blitCmds;
+    }
+    return HgiBlitCmdsUniquePtr(blitCmds);
 }
 
 HgiTextureHandle
@@ -287,7 +296,7 @@ HgiMetal::StartFrame()
             // We need to grab a new command buffer otherwise the previous one
             // (if it was allocated at the end of the last frame) won't appear in
             // this frame's capture, and it will confuse us!
-            CommitCommandBuffer(CommitCommandBuffer_NoWait, true);
+            CommitPrimaryCommandBuffer(CommitCommandBuffer_NoWait, true);
         }
     }
 }
@@ -300,27 +309,80 @@ HgiMetal::EndFrame()
     }
 }
 
+id<MTLCommandQueue>
+HgiMetal::GetQueue() const
+{
+    return _commandQueue;
+}
+
+id<MTLCommandBuffer>
+HgiMetal::GetPrimaryCommandBuffer(bool flush)
+{
+    if (_workToFlush) {
+        if (_currentCmds) {
+            return nil;
+        }
+    }
+    if (flush) {
+        _workToFlush = true;
+    }
+    return _commandBuffer;
+}
+
+id<MTLCommandBuffer>
+HgiMetal::GetSecondaryCommandBuffer()
+{
+    id<MTLCommandBuffer> commandBuffer = [_commandQueue commandBuffer];
+    [commandBuffer retain];
+    return commandBuffer;
+}
+
+int
+HgiMetal::GetAPIVersion() const
+{
+    return _apiVersion;
+}
+
+HgiMetalCapabilities const &
+HgiMetal::GetCapabilities() const
+{
+    return *_capabilities;
+}
+
 void
-HgiMetal::CommitCommandBuffer(CommitCommandBufferWaitType waitType,
+HgiMetal::CommitPrimaryCommandBuffer(CommitCommandBufferWaitType waitType,
                               bool forceNewBuffer)
 {
     if (!_workToFlush && !forceNewBuffer) {
         return;
     }
 
-    [_commandBuffer commit];
-    if (waitType == CommitCommandBuffer_WaitUntilScheduled) {
-        [_commandBuffer waitUntilScheduled];
-    }
-    else if (waitType == CommitCommandBuffer_WaitUntilCompleted) {
-        [_commandBuffer waitUntilCompleted];
-    }
+    CommitSecondaryCommandBuffer(_commandBuffer, waitType);
     [_commandBuffer release];
-
     _commandBuffer = [_commandQueue commandBuffer];
     [_commandBuffer retain];
-    
+
     _workToFlush = false;
+}
+
+void
+HgiMetal::CommitSecondaryCommandBuffer(
+    id<MTLCommandBuffer> commandBuffer,
+    CommitCommandBufferWaitType waitType)
+{
+    [commandBuffer commit];
+    if (waitType == CommitCommandBuffer_WaitUntilScheduled) {
+        [commandBuffer waitUntilScheduled];
+    }
+    else if (waitType == CommitCommandBuffer_WaitUntilCompleted) {
+        [commandBuffer waitUntilCompleted];
+    }
+}
+
+void
+HgiMetal::ReleaseSecondaryCommandBuffer(id<MTLCommandBuffer> commandBuffer)
+{
+    [commandBuffer release];
 }
 
 bool
@@ -330,19 +392,10 @@ HgiMetal::_SubmitCmds(HgiCmds* cmds, HgiSubmitWaitType wait)
 
     if (cmds) {
         _workToFlush = Hgi::_SubmitCmds(cmds, wait);
+        if (cmds == _currentCmds) {
+            _currentCmds = nullptr;
+        }
     }
-
-    CommitCommandBufferWaitType waitType;
-    switch(wait) {
-        case HgiSubmitWaitTypeNoWait:
-            waitType = CommitCommandBuffer_NoWait;
-            break;
-        case HgiSubmitWaitTypeWaitUntilCompleted:
-            waitType = CommitCommandBuffer_WaitUntilCompleted;
-            break;
-    }
-
-    CommitCommandBuffer(waitType);
 
     return _workToFlush;
 }

pxr/imaging/hgiInterop/metal.mm
@@ -758,7 +758,7 @@ HgiInteropMetal::_RestoreOpenGlState()
     }
     glBindBuffer(GL_ARRAY_BUFFER, _restoreVbo);
     
-    if (!_restoreVao) {
+    if (!_restoreVao && _restoreVbo) {
         for (int i = 0; i < 2; i++) {
             VertexAttribState &state(_restoreVertexAttribState[i]);
             if (state.enabled) {
@@ -899,7 +899,7 @@ HgiInteropMetal::CompositeToInterop(
         depthTexture = metalDepth->GetTextureId();
     }
 
-    id<MTLCommandBuffer> commandBuffer = _hgiMetal->GetCommandBuffer();
+    id<MTLCommandBuffer> commandBuffer = _hgiMetal->GetPrimaryCommandBuffer();
     
     id<MTLComputeCommandEncoder> computeEncoder;
     
@@ -974,7 +974,7 @@ HgiInteropMetal::CompositeToInterop(
 
     // We wait until the work is scheduled for execution so that future OpenGL
     // calls are guaranteed to happen after the Metal work encoded above
-    _hgiMetal->CommitCommandBuffer(
+    _hgiMetal->CommitPrimaryCommandBuffer(
         HgiMetal::CommitCommandBuffer_WaitUntilScheduled);
 
     if (glShaderIndex != -1) {

pxr/imaging/hgiMetal/blitCmds.h
@@ -84,8 +84,10 @@ private:
     void _CreateEncoder();
 
     HgiMetal* _hgi;
+    id<MTLCommandBuffer> _commandBuffer;
     id<MTLBlitCommandEncoder> _blitEncoder;
     NSString* _label;
+    bool _secondaryCommandBuffer;
 
     // BlitCmds is used only one frame so storing multi-frame state on BlitCmds
     // will not survive.

pxr/imaging/hgiMetal/blitCmds.mm
@@ -39,8 +39,10 @@ PXR_NAMESPACE_OPEN_SCOPE
 
 HgiMetalBlitCmds::HgiMetalBlitCmds(HgiMetal *hgi)
     : _hgi(hgi)
+    , _commandBuffer(nil)
     , _blitEncoder(nil)
     , _label(nil)
+    , _secondaryCommandBuffer(false)
 {
 }
 
@@ -58,7 +60,13 @@ void
 HgiMetalBlitCmds::_CreateEncoder()
 {
     if (!_blitEncoder) {
-        _blitEncoder = [_hgi->GetCommandBuffer() blitCommandEncoder];
+        _commandBuffer = _hgi->GetPrimaryCommandBuffer();
+        if (_commandBuffer == nil) {
+            _commandBuffer = _hgi->GetSecondaryCommandBuffer();
+            _secondaryCommandBuffer = true;
+        }
+        _blitEncoder = [_commandBuffer blitCommandEncoder];
+
         if (_label) {
             if (HgiMetalDebugEnabled()) {
                 _blitEncoder.label = _label;
@@ -173,7 +181,7 @@ HgiMetalBlitCmds::CopyTextureGpuToCpu(
     // bytes to copy
     size_t byteSize = copyOp.destinationBufferByteSize;
 
-    [_hgi->GetCommandBuffer() addCompletedHandler:^(id<MTLCommandBuffer> buffer)
+    [_commandBuffer addCompletedHandler:^(id<MTLCommandBuffer> buffer)
         {
             memcpy(dst, src, byteSize);
         }];
@@ -322,7 +330,7 @@ HgiMetalBlitCmds::CopyBufferGpuToCpu(HgiBufferGpuToCpuOp const& copyOp)
     // bytes to copy
     size_t size = copyOp.byteSize;
 
-    [_hgi->GetCommandBuffer() addCompletedHandler:^(id<MTLCommandBuffer> buffer)
+    [_commandBuffer addCompletedHandler:^(id<MTLCommandBuffer> buffer)
         {
             memcpy(dst, src, size);
         }];
@@ -342,12 +350,36 @@ HgiMetalBlitCmds::GenerateMipMaps(HgiTextureHandle const& texture)
 bool
 HgiMetalBlitCmds::_Submit(Hgi* hgi, HgiSubmitWaitType wait)
 {
+    bool submittedWork = false;
     if (_blitEncoder) {
         [_blitEncoder endEncoding];
         _blitEncoder = nil;
-        return true;
+        submittedWork = true;
+
+        HgiMetal::CommitCommandBufferWaitType waitType;
+        switch(wait) {
+            case HgiSubmitWaitTypeNoWait:
+                waitType = HgiMetal::CommitCommandBuffer_NoWait;
+                break;
+            case HgiSubmitWaitTypeWaitUntilCompleted:
+                waitType = HgiMetal::CommitCommandBuffer_WaitUntilCompleted;
+                break;
+        }
+
+        if (_secondaryCommandBuffer) {
+            _hgi->CommitSecondaryCommandBuffer(_commandBuffer, waitType);
+        }
+        else {
+            _hgi->CommitPrimaryCommandBuffer(waitType);
+        }
     }
-    return false;
+    
+    if (_secondaryCommandBuffer) {
+        _hgi->ReleaseSecondaryCommandBuffer(_commandBuffer);
+    }
+    _commandBuffer = nil;
+
+    return submittedWork;
 }
 
 PXR_NAMESPACE_CLOSE_SCOPE

commit 5da4e928007e0a8637ea4943c8b96f998346eb6b

Correct the behavior when we want to submit a command. This will also make WaitTypeWaitUntilCompleted work properly.
The issue was introduced in change 28116f1. Before the change, the command buffer is committed in HgiMetal::_SubmitCmds(), and before the buffer commit, HgiMetal::_workToFlush was set to indicate if there is dispatching work in compute encoder or drawing work in graphics encoder. So when command buffer is committed, if _workToFlush is true, it will commit the commands to GPU.
But in change 28116f1, because they want to introduce the secondary command buffer, the commit of the command buffer is moved to HgiCmds::_Submit (in _hgi->CommitPrimaryCommandBuffer(waitType)), which is before the set of _workToFlush. So even if the encoder contains dispatching work or drawing work, the command buffer will not be committed to GPU. This has two drawbacks. For HgiSubmitWaitTypeNoWait waitType, we push the work to GPU too late. This didn't make the most use of GPU. For HgiSubmitWaitTypeWaitUntilCompleted waitType, it doesn't wait at all.

As you can see, the problem is that _workToFlush is set after the commit of command buffer. So in this change, I add an API HgiMetal::HasWork(). It will set _workToFlush to true. When graphics encoder encodes the draw command or the compute encoder encodes the dispatch command, it can call the HasWork API to indicate there is already work command. So when command buffer is committed, _workToFlush is correctly set.

pxr/imaging/hgiMetal/computeCmds.h
@@ -101,7 +101,6 @@ private:
     id<MTLBuffer> _argumentBuffer;
     id<MTLComputeCommandEncoder> _encoder;
     bool _secondaryCommandBuffer;
-    bool _hasWork;
     HgiComputeDispatch _dispatchMethod;
 };
 

pxr/imaging/hgiMetal/computeCmds.mm
@@ -44,7 +44,6 @@ HgiMetalComputeCmds::HgiMetalComputeCmds(
     , _argumentBuffer(nil)
     , _encoder(nil)
     , _secondaryCommandBuffer(false)
-    , _hasWork(false)
     , _dispatchMethod(desc.dispatchMethod)
 {
     _CreateEncoder();
@@ -152,7 +151,8 @@ HgiMetalComputeCmds::Dispatch(int dimX, int dimY)
         threadsPerThreadgroup:MTLSizeMake(MIN(thread_width, dimX),
                                           MIN(thread_height, dimY), 1)];
 
-    _hasWork = true;
+    if(!_secondaryCommandBuffer)
+        _hgi->SetHasWork();
     _argumentBuffer = nil;
 }
 
@@ -191,11 +191,9 @@ HgiMetalComputeCmds::GetDispatchMethod() const
 bool
 HgiMetalComputeCmds::_Submit(Hgi* hgi, HgiSubmitWaitType wait)
 {
-    bool submittedWork = false;
     if (_encoder) {
         [_encoder endEncoding];
         _encoder = nil;
-        submittedWork = true;
 
         HgiMetal::CommitCommandBufferWaitType waitType;
         switch(wait) {
@@ -221,7 +219,7 @@ HgiMetalComputeCmds::_Submit(Hgi* hgi, HgiSubmitWaitType wait)
     _commandBuffer = nil;
     _argumentBuffer = nil;
 
-    return submittedWork;
+    return true;
 }
 
 HGIMETAL_API

pxr/imaging/hgiMetal/graphicsCmds.h
@@ -170,7 +170,6 @@ private:
     uint32_t _primitiveIndexSize;
     uint32_t _drawBufferBindingIndex;
     NSString* _debugLabel;
-    bool _hasWork;
     bool _viewportSet;
     bool _scissorRectSet;
     bool _enableParallelEncoder;

pxr/imaging/hgiMetal/graphicsCmds.mm
@@ -85,7 +85,6 @@ HgiMetalGraphicsCmds::HgiMetalGraphicsCmds(
     , _primitiveIndexSize(0)
     , _drawBufferBindingIndex(0)
     , _debugLabel(nil)
-    , _hasWork(false)
     , _viewportSet(false)
     , _scissorRectSet(false)
     , _enableParallelEncoder(false)
@@ -566,7 +565,7 @@ HgiMetalGraphicsCmds::Draw(
         }
     }
 
-    _hasWork = true;
+    _hgi->SetHasWork();
 }
 
 void
@@ -636,6 +635,7 @@ HgiMetalGraphicsCmds::DrawIndirect(
             });
         }
     });
+    _hgi->SetHasWork();
 }
 
 void
@@ -685,7 +685,7 @@ HgiMetalGraphicsCmds::DrawIndexed(
                           baseInstance:baseInstance];
     }
 
-    _hasWork = true;
+    _hgi->SetHasWork();
 }
 
 void
@@ -778,6 +778,7 @@ HgiMetalGraphicsCmds::DrawIndexedIndirect(
             });
         }
     });
+    _hgi->SetHasWork();
 }
 
 void
@@ -875,7 +876,7 @@ HgiMetalGraphicsCmds::_Submit(Hgi* hgi, HgiSubmitWaitType wait)
     _encoders.clear();
     _CachedEncState.ResetCachedEncoderState();
     
-    return _hasWork;
+    return true;
 }
 
 PXR_NAMESPACE_CLOSE_SCOPE

pxr/imaging/hgiMetal/hgi.h
@@ -204,6 +204,8 @@ public:
     HGIMETAL_API
     id<MTLBuffer> GetArgBuffer();
 
+    inline void SetHasWork() { _workToFlush = true; }
+
 protected:
     HGIMETAL_API
     bool _SubmitCmds(HgiCmds* cmds, HgiSubmitWaitType wait) override;

pxr/imaging/hgiMetal/hgi.mm
@@ -515,7 +515,7 @@ HgiMetal::_SubmitCmds(HgiCmds* cmds, HgiSubmitWaitType wait)
     TRACE_FUNCTION();
 
     if (cmds) {
-        _workToFlush = Hgi::_SubmitCmds(cmds, wait);
+        Hgi::_SubmitCmds(cmds, wait);
         if (cmds == _currentCmds) {
             _currentCmds = nullptr;
         }