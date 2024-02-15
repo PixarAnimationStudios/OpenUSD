5a963356f71013c8644fb81fd2016ffa206823ad
Dhruv Govil <dgovil2@apple.com>
2024-02-15 14:58:49 -0800

    Upgrade MaterialX to 1.38.8

--- build_scripts/build_usd.py
+++ build_scripts/build_usd.py
@@ -1484,7 +1484,7 @@ DRACO = Dependency("Draco", InstallDraco, "include/draco/compression/decode.h")
 ############################################################
 # MaterialX
 
-MATERIALX_URL = "https://github.com/materialx/MaterialX/archive/v1.38.7.zip"
+MATERIALX_URL = "https://github.com/materialx/MaterialX/archive/v1.38.8.zip"
 
 def InstallMaterialX(context, force, buildArgs):
     with CurrentWorkingDirectory(DownloadURL(MATERIALX_URL, context, force)):
