031eaea483523a1ceecf053c034be509a0586d2a
davidgyu <davidgyu@users.noreply.github.com>
2024-04-04 12:40:07 -0700

    [hdSt] Fixed two issues with faceVarying primvar refinement
    - FaceVarying primvar topologies are validated in two separate
    places in the implementation and we need to make sure that we
    don't try to refine any primvar that we've already determined
    are invalid when gathering faceVarying topologies.
    
    - Refinement can produce an empty patch table and we want to
    correctly convert such a patch table into empty buffer sources.
    
    (Internal change: 2323243)

--- pxr/imaging/hdSt/subdivision.cpp
+++ pxr/imaging/hdSt/subdivision.cpp
@@ -1203,7 +1203,7 @@ HdSt_OsdIndexComputation::Resolve()
         _topology->RefinesToBoxSplineTrianglePatches()) {
 
         // Bundle groups of 12 or 16 patch control vertices.
-        int const arraySize = patchTable
+        int const arraySize = (ptableSize > 0)
             ? patchTable->GetPatchArrayDescriptor(0).GetNumControlVertices()
             : 0;
 
@@ -1453,15 +1453,12 @@ HdSt_OsdFvarIndexComputation::Resolve()
         return true;
     }
 
-    VtIntArray fvarIndices = subdivision->GetRefinedFvarIndices(_channel);
-    if (!TF_VERIFY(!fvarIndices.empty())) {
-        _SetResolved();
-        return true;
-    }
-
-    Far::Index const * firstIndex = fvarIndices.cdata();
     Far::PatchTable const * patchTable = subdivision->GetPatchTable();
-    size_t numPatches = patchTable ? patchTable->GetNumPatchesTotal() : 0;
+    size_t const numPatches = patchTable ? patchTable->GetNumPatchesTotal() : 0;
+
+    VtIntArray fvarIndices = subdivision->GetRefinedFvarIndices(_channel);
+    Far::Index const * firstIndex =
+        !fvarIndices.empty() ? fvarIndices.cdata() : nullptr;
 
     TfToken const & scheme = _topology->GetScheme();
 
@@ -1469,7 +1466,7 @@ HdSt_OsdFvarIndexComputation::Resolve()
         _topology->RefinesToBoxSplineTrianglePatches()) {
 
         // Bundle groups of 12 or 16 patch control vertices
-        int const arraySize = patchTable ?
+        int const arraySize = (numPatches > 0) ?
             patchTable->GetFVarPatchDescriptor(_channel).GetNumControlVertices()
             : 0;
 
