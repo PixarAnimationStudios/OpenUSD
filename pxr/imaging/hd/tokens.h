1ad4c6c546df50f2743beb00023ae60322591004
blevin <blevin@users.noreply.github.com>
2024-04-10 16:00:19 -0700

    Code review note: clean up disused tokens now we decided not to add Hydra1 support for render pass prims.
    (Internal change: 2323856)

--- pxr/imaging/hd/tokens.h
+++ pxr/imaging/hd/tokens.h
@@ -445,10 +445,6 @@ TfToken HdAovTokensMakeShader(TfToken const& shader);
     /* interactive vs offline */                      \
     (enableInteractive)
 
-#define HD_RENDER_PASS_PRIM_TOKENS                    \
-    (passType)                                        \
-    (renderSource)
-
 #define HD_RENDER_SETTINGS_PRIM_TOKENS                \
     (active)                                          \
     (namespacedSettings)                              \
@@ -495,8 +491,6 @@ TF_DECLARE_PUBLIC_TOKENS(HdPrimTypeTokens, HD_API, HD_PRIMTYPE_TOKENS);
 TF_DECLARE_PUBLIC_TOKENS(HdPrimvarRoleTokens, HD_API, HD_PRIMVAR_ROLE_TOKENS);
 TF_DECLARE_PUBLIC_TOKENS(HdAovTokens, HD_API, HD_AOV_TOKENS);
 TF_DECLARE_PUBLIC_TOKENS(HdRenderSettingsTokens, HD_API, HD_RENDER_SETTINGS_TOKENS);
-TF_DECLARE_PUBLIC_TOKENS(HdRenderPassPrimTokens, HD_API,
-                         HD_RENDER_PASS_PRIM_TOKENS);
 TF_DECLARE_PUBLIC_TOKENS(HdRenderSettingsPrimTokens, HD_API,
                          HD_RENDER_SETTINGS_PRIM_TOKENS);
 TF_DECLARE_PUBLIC_TOKENS(HdAspectRatioConformPolicyTokens, HD_API, 
