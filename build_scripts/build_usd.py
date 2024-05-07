a857b24e320f2a4e68b600aca56cfea592fc2a58
Joshua Miller <jomiller@nvidia.com>
2024-05-06 17:08:00 -0700

    [build_usd.py] Update libpng to 1.6.43

--- build_scripts/build_usd.py
+++ build_scripts/build_usd.py
@@ -1140,7 +1140,7 @@ TIFF = Dependency("TIFF", InstallTIFF, "include/tiff.h")
 ############################################################
 # PNG
 
-PNG_URL = "https://github.com/glennrp/libpng/archive/refs/tags/v1.6.38.zip"
+PNG_URL = "https://github.com/glennrp/libpng/archive/refs/tags/v1.6.43.zip"
 
 def InstallPNG(context, force, buildArgs):
     with CurrentWorkingDirectory(DownloadURL(PNG_URL, context, force)):
