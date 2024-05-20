9c9af74409e2689f78e295aeb1906ab55d547cad
rajabala <rajabala@users.noreply.github.com>
2024-05-20 13:35:33 -0700

    [hd, hdsi, hdPrman] Add light linking scene index. hdsi: - Add scene index to perform light linking when the light linking collections are transported as data sources on the light/light filter prim. For legacy scene delegates (e.g. UsdImagingDelegate) where this isn't the case, the scene index should not affect the transported category opinions on the light or geometry prims. Support is currently limited to linking non-instanced geometry.
    hdPrman:
    - Add light linking scene index plugin with an env setting guard for registration (HDPRMAN_ENABLE_LIGHT_LINKING_SCENE_INDEX). It is enabled by default.
    - Fork the 3 light linking tests. They're all disabled for now with reasons stated in the testSpecs.xml
    
    (Internal change: 2328043)
    (Internal change: 2328438)
    (Internal change: 2328445)

--- pxr/imaging/hdsi/version.h
+++ pxr/imaging/hdsi/version.h
@@ -27,7 +27,8 @@
 // 10 -> 11: Adding HdsiPrimManagingSceneIndexObserver and
 //           HdsiPrimTypeNoticeBatchingSceneIndex
 // 11 -> 12: Adding HdsiPrimManagingSceneIndexObserver::GetTypedPrim.
+// 12 -> 13: Adding HdsiLightLinkingSceneIndex.
 
-#define HDSI_API_VERSION 12
+#define HDSI_API_VERSION 13
 
 #endif // PXR_IMAGING_HDSI_VERSION_H
