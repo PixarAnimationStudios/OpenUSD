c0a497b5db83709cc034ec11e7dd48328293f823
Paul Molodowitch <pmolodowitch@nvidia.com>
2024-08-15 13:58:50 -0700

    [hdEmbree][build_usd] add to build_usd.py status message

--- build_scripts/build_usd.py
+++ build_scripts/build_usd.py
@@ -2730,6 +2730,7 @@ summaryMsg += """\
       OpenVDB support:          {enableOpenVDB}
       OpenImageIO support:      {buildOIIO} 
       OpenColorIO support:      {buildOCIO} 
+      Embree support:           {buildEmbree}
       PRMan support:            {buildPrman}
       Vulkan support:           {enableVulkan}
     UsdImaging                  {buildUsdImaging}
@@ -2794,6 +2795,7 @@ summaryMsg = summaryMsg.format(
     enableOpenVDB=("On" if context.enableOpenVDB else "Off"),
     buildOIIO=("On" if context.buildOIIO else "Off"),
     buildOCIO=("On" if context.buildOCIO else "Off"),
+    buildEmbree=("On" if context.buildEmbree else "Off"),
     buildPrman=("On" if context.buildPrman else "Off"),
     buildUsdImaging=("On" if context.buildUsdImaging else "Off"),
     buildUsdview=("On" if context.buildUsdview else "Off"),
