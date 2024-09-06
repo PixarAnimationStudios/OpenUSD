762f2ea83462552cae8077ef20c0a29bc42c1579
rajabala <rajabala@users.noreply.github.com>
2024-09-05 22:48:18 -0700

    [hd,hdsi,hdPrman] Improvements in light linking using the scene index. - Add support for non-nested instance linking (relevant for native instancing). - Fix dirty propagation in emulation. - Fix invalidation handling in hdPrman to update the categories on the instances. - Enable instance linking unit test cases.
    (Internal change: 2339821)

--- pxr/imaging/hdsi/lightLinkingSceneIndex.h
+++ pxr/imaging/hdsi/lightLinkingSceneIndex.h
@@ -64,10 +64,9 @@ TF_DECLARE_REF_PTRS(HdsiLightLinkingSceneIndex);
 ///   c'tor argument by providing a HdTokenArrayDataSourceHandle for
 ///   \p geometryPrimTypes.
 ///
-/// \note Current support does *not* handle instanced prims (be it natively
-///       instanced, or prototype prims under point instancers) due to
-///       limitations in the HdCollectionExpressionEvaluator.
-///       This will be addressed in a future change.
+/// \note Current support for instancing is limited to linking non-nested
+///       instance prims. Linking to instance proxy prims, nested instances and
+///       point instancers is not yet supported.
 ///
 /// \note For legacy scene delegates that implement light linking (e.g.
 ///       UsdImagingDelegate) and don't transport the light linking collections,
