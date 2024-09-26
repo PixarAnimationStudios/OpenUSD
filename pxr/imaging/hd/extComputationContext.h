dbad9c45ba2f2307404fe5cf82d421f3888322c9
unhyperbolic <unhyperbolic@users.noreply.github.com>
2024-09-26 11:55:53 -0700

    Updates to HdExtComputationContext
    (Internal change: 2342271)

--- pxr/imaging/hd/extComputationContext.h
+++ pxr/imaging/hd/extComputationContext.h
@@ -25,24 +25,48 @@ public:
     ///
     /// Obtains the value of an named input to the computation.
     ///
-    /// The code will issue a coding error and return a empty array
-    /// if the input is missing or of a different type.
+    /// The code will issue a coding error and return a empty value
+    /// if the input is missing.
     ///
     virtual const VtValue &GetInputValue(const TfToken &name) const = 0;
 
     ///
     /// Obtains the value of an named input to the computation.
     ///
-    /// If the input isn't present or of a different type
-    /// nullptr will be returned.
+    /// The code will issue a coding error and return a default constructed
+    /// value if the input is missing or is of the wrong type.
+    ///
+    template <typename T>
+    T GetTypedInputValue(const TfToken &name) const;
+    
+    ///
+    /// Obtains the value of an named input to the computation.
+    ///
+    /// If the input isn't present, a nullptr will be returned.
     ///
     virtual const VtValue *GetOptionalInputValuePtr(
                                                  const TfToken &name) const = 0;
 
     ///
+    /// Obtains the value of an named input to the computation.
+    ///
+    /// If the input isn't present, a nullptr will be returned. If the input
+    /// is of the wrong type, a coding error will be issued and a nullptr will
+    /// be returned.
+    ///
+    template <typename T>
+    const T *GetOptionalTypedInputValuePtr(const TfToken &name) const;
+
+    ///
     /// Set the value of the specified output.
     ///
     virtual void SetOutputValue(const TfToken &name, const VtValue &output) = 0;
+
+    ///
+    /// Set the value of the specified output.
+    ///
+    template <typename T>
+    void SetTypedOutputValue(const TfToken &name, const T &output);
 
     ///
     /// Called to indicate an error occurred while executing a computation
@@ -55,6 +79,50 @@ private:
     HdExtComputationContext &operator = (const HdExtComputationContext &)
                                                                        = delete;
 };
+
+template <typename T>
+T
+HdExtComputationContext::GetTypedInputValue(const TfToken &name) const
+{
+    const VtValue &v = GetInputValue(name);
+    if (!v.IsHolding<T>()) {
+        TF_CODING_ERROR(
+            "HdExtComputationContext::GetTypedInputValue<T> called with type T"
+            "not matching the type of the input value for '%s'.",
+            name.GetText());
+        return {};
+    }
+
+    return v.UncheckedGet<T>();
+}
+
+template <typename T>
+const T *
+HdExtComputationContext::GetOptionalTypedInputValuePtr(const TfToken &name) const
+{
+    const VtValue * const v = GetOptionalInputValuePtr(name);
+    if (!v) {
+        return nullptr;
+    }
+   
+    if (!v->IsHolding<T>()) {
+        TF_CODING_ERROR(
+            "HdExtComputationContext::GetOptionalTypedInputValue<T> called with "
+            "type T not matching the type of the input value for '%s'.",
+            name.GetText());
+        return nullptr;
+    }
+
+    return &(v->UncheckedGet<T>());
+}
+
+template <typename T>
+void
+HdExtComputationContext::
+SetTypedOutputValue(const TfToken &name, const T &output)
+{
+    SetOutputValue(name, VtValue(output));
+}
 
 PXR_NAMESPACE_CLOSE_SCOPE
 
