wabiverse/USD

Add Framework build support

pxr/base/plug/initConfig.cpp
@@ -93,6 +93,10 @@ ARCH_CONSTRUCTOR(Plug_InitConfig, 2, void)
     _AppendPathList(&result, buildLocation, binaryPath);
     _AppendPathList(&result, pluginBuildLocation, binaryPath);
 
+#ifdef PXR_BUILD_APPLE_FRAMEWORK
+    _AppendPathList(&result, "Libraries/usd", binaryPath);
+#endif
+
 #ifdef PXR_INSTALL_LOCATION
     _AppendPathList(&result, installLocation, binaryPath);
 #endif // PXR_INSTALL_LOCATION

pxr/usd/usdMtlx/utils.cpp
@@ -16,6 +16,8 @@
 #include "pxr/usd/sdf/types.h"
 #include "pxr/usd/sdr/shaderProperty.h"
 #include "pxr/base/arch/fileSystem.h"
+#include "pxr/base/arch/symbols.h"
+#include "pxr/base/arch/systemInfo.h"
 #include "pxr/base/gf/matrix3d.h"
 #include "pxr/base/gf/matrix4d.h"
 #include "pxr/base/gf/vec2f.h"
@@ -182,6 +184,21 @@ _ComputeStdlibSearchPaths()
     stdlibSearchPaths =
         _MergeSearchPaths(stdlibSearchPaths, { PXR_MATERIALX_STDLIB_DIR });
 #endif
+
+#ifdef PXR_BUILD_APPLE_FRAMEWORK
+    std::string binaryPath;
+    if (!ArchGetAddressInfo(
+        reinterpret_cast<void*>(&_ComputeStdlibSearchPaths), &binaryPath,
+            nullptr, nullptr, nullptr)
+            && binaryPath.empty())
+    {
+        binaryPath = ArchGetExecutablePath();
+        binaryPath = TfGetPathName(binaryPath);
+    }
+    stdlibSearchPaths =
+        _MergeSearchPaths(stdlibSearchPaths, { TfStringCatPaths(binaryPath, "../Libraries/materialx") });
+#endif
+
     return stdlibSearchPaths;
 }
 

PixarAnimationStudios/OpenUSD

pxr/base/plug/initConfig.cpp
@@ -93,6 +93,10 @@ ARCH_CONSTRUCTOR(Plug_InitConfig, 2, void)
     _AppendPathList(&result, buildLocation, binaryPath);
     _AppendPathList(&result, pluginBuildLocation, binaryPath);
 
+#ifdef PXR_BUILD_APPLE_FRAMEWORK
+    _AppendPathList(&result, "Resources/usd", binaryPath);
+#endif
+
 #ifdef PXR_INSTALL_LOCATION
     _AppendPathList(&result, installLocation, binaryPath);
 #endif // PXR_INSTALL_LOCATION

pxr/usd/usdMtlx/utils.cpp
@@ -16,6 +16,8 @@
 #include "pxr/usd/sdf/types.h"
 #include "pxr/usd/sdr/shaderProperty.h"
 #include "pxr/base/arch/fileSystem.h"
+#include "pxr/base/arch/symbols.h"
+#include "pxr/base/arch/systemInfo.h"
 #include "pxr/base/gf/matrix3d.h"
 #include "pxr/base/gf/matrix4d.h"
 #include "pxr/base/gf/vec2f.h"
@@ -182,6 +184,21 @@ _ComputeStdlibSearchPaths()
     stdlibSearchPaths =
         _MergeSearchPaths(stdlibSearchPaths, { PXR_MATERIALX_STDLIB_DIR });
 #endif
+
+#ifdef PXR_BUILD_APPLE_FRAMEWORK
+    std::string binaryPath;
+    if (!ArchGetAddressInfo(
+        reinterpret_cast<void*>(&_ComputeStdlibSearchPaths), &binaryPath,
+            nullptr, nullptr, nullptr)
+            && binaryPath.empty())
+    {
+        binaryPath = ArchGetExecutablePath();
+        binaryPath = TfGetPathName(binaryPath);
+    }
+    stdlibSearchPaths =
+        _MergeSearchPaths(stdlibSearchPaths, { TfStringCatPaths(binaryPath, "../Resources/materialx") });
+#endif
+
     return stdlibSearchPaths;
 }
 