4e463481c722840663964fd1fd7731e407ddbc21
Dhruv Govil <dgovil2@apple.com>
2025-03-27 11:20:13 -0700

    Add test for dictionary erase with iterator incrementing

--- pxr/base/vt/testenv/testVtCpp.cpp
+++ pxr/base/vt/testenv/testVtCpp.cpp
@@ -889,6 +889,23 @@ testDictionaryIterators()
                 "have equal values.");
         }
     }
+
+
+    // Check a dictionaries erase method allows iterator incrementing
+    {
+        VtDictionary a = {key1, key2, key3};
+        for (auto it = a.begin(); it != a.end();) {
+            it = a.erase(it);
+            if (it != a.end()) {
+                it = it++;
+            }
+        }
+
+        if (!a.empty()) {
+            die("VtDictionary::erase iterator did not remove all items");
+        }
+    }
+
 }
 
 static void
