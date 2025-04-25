bfeb1494522dd8d4bfd8c32a07b7dff35863d1b9
Roggie Zhang <rozhang@nvidia.com>
2025-04-26 00:26:56 +0800

    Add tests for ArchGetFileName with utf8 path

--- pxr/base/arch/testenv/testFileSystem.cpp
+++ pxr/base/arch/testenv/testFileSystem.cpp
@@ -98,7 +98,15 @@ int main()
     std::string filePath = ArchGetFileName(firstFile);
     ARCH_AXIOM(std::filesystem::equivalent(filePath, firstName));
     fclose(firstFile);
-
+    
+    // Test utf-8 path
+    std::string secondName = ArchMakeTmpFileName("测试");
+    ARCH_AXIOM((firstFile = ArchOpenFile(secondName.c_str(), "w")) != NULL);
+    filePath = ArchGetFileName(firstFile);
+    ARCH_AXIOM(std::filesystem::equivalent(std::filesystem::u8path(filePath),
+               std::filesystem::u8path(secondName)));
+    fclose(firstFile);
+    
     // Map the file and assert the bytes are what we expect they are.
     ARCH_AXIOM((firstFile = ArchOpenFile(firstName.c_str(), "rb")) != NULL);
     ArchConstFileMapping cfm = ArchMapFileReadOnly(firstFile);
