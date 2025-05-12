767a951d0fff209c7f490d18e4c4a3bc7ca36855
Matt Kuruc <mkuruc@nvidia.com>
2025-05-12 16:25:41 -0400

    Reference `arch_{c,d}tor`'s address to prevent inline removal

--- cmake/defaults/msvcdefaults.cmake
+++ cmake/defaults/msvcdefaults.cmake
@@ -9,7 +9,7 @@
 set(_PXR_CXX_FLAGS "${_PXR_CXX_FLAGS} /EHsc")
 
 # Standards compliant.
-set(_PXR_CXX_FLAGS "${_PXR_CXX_FLAGS} /Zc:rvalueCast /Zc:strictStrings")
+set(_PXR_CXX_FLAGS "${_PXR_CXX_FLAGS} /Zc:rvalueCast /Zc:strictStrings /Zc:inline")
 
 # Visual Studio sets the value of __cplusplus to 199711L regardless of
 # the C++ standard actually being used, unless /Zc:__cplusplus is enabled.
@@ -17,19 +17,6 @@ set(_PXR_CXX_FLAGS "${_PXR_CXX_FLAGS} /Zc:rvalueCast /Zc:strictStrings")
 # For more details, see:
 # https://learn.microsoft.com/en-us/cpp/build/reference/zc-cplusplus
 set(_PXR_CXX_FLAGS "${_PXR_CXX_FLAGS} /Zc:__cplusplus")
-
-# The /Zc:inline option strips out the "arch_ctor_<name>" symbols used for
-# library initialization by ARCH_CONSTRUCTOR starting in Visual Studio 2019, 
-# causing release builds to fail. Disable the option for this and later 
-# versions.
-# 
-# For more details, see:
-# https://developercommunity.visualstudio.com/content/problem/914943/zcinline-removes-extern-symbols-inside-anonymous-n.html
-if (MSVC_VERSION GREATER_EQUAL 1920)
-    set(_PXR_CXX_FLAGS "${_PXR_CXX_FLAGS} /Zc:inline-")
-else()
-    set(_PXR_CXX_FLAGS "${_PXR_CXX_FLAGS} /Zc:inline")
-endif()
 
 # Turn on all but informational warnings.
 set(_PXR_CXX_FLAGS "${_PXR_CXX_FLAGS} /W3")
