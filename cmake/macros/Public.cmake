3d1a9938b723d5ceaf029f9d430904bb2b2a55a8
Dhruv Govil <dgovil2@apple.com>
2025-07-08 11:22:11 -0700

    Copy back Framework build script

--- cmake/macros/Public.cmake
+++ cmake/macros/Public.cmake
@@ -1234,6 +1234,11 @@ function(pxr_toplevel_epilogue)
     # Setup the plugins in the top epilogue to ensure that everybody has had a
     # chance to update PXR_EXTRA_PLUGINS with their plugin paths.
     pxr_setup_plugins()
+
+    if (PXR_BUILD_APPLE_FRAMEWORK)
+        pxr_create_apple_framework()
+    endif ()
+
 endfunction() # pxr_toplevel_epilogue
 
 function(pxr_monolithic_epilogue)
