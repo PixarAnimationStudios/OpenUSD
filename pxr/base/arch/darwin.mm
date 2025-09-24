a207a4fb6dbabaafed1bb6766665584ea3148480
Dhruv Govil <dgovil2@apple.com>
2025-09-24 11:40:29 -0700

    Do not use strcpy for Darwin Temp Dir

--- pxr/base/arch/darwin.mm
+++ pxr/base/arch/darwin.mm
@@ -1,5 +1,5 @@
 //
-// Copyright 2025 Pixar
+// Copyright 2025 Apple
 //
 // Licensed under the terms set forth in the LICENSE.txt file available at
 // https://openusd.org/license.
@@ -13,13 +13,8 @@ PXR_NAMESPACE_OPEN_SCOPE
 
 
 const char* Arch_DarwinGetTemporaryDirectory() {
-    char* temporaryDirectory;
-
-    const char *nsTmpDir = [NSTemporaryDirectory() UTF8String];
-    temporaryDirectory = new char[strlen(nsTmpDir) + 1];
-    strcpy(temporaryDirectory, nsTmpDir);
-
-    return temporaryDirectory;
+    std::string tmpDir = [NSTemporaryDirectory() UTF8String];
+    return tmpDir.c_str();
 }
 
 PXR_NAMESPACE_CLOSE_SCOPE
