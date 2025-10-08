42722eefa32a43104a583c81a70ea625234af26a
David Aguilar <davvid@gmail.com>
2025-10-08 08:35:43 -0700

    cmake: update install structure for find_package(pxr)
    Windows expects cmake packaging files in the cmake/ directory.
    Linux/UNIX expects cmake packaging files in $libdir/cmake/<name>/.
    
    Export targets into a pxr:: namespace so that the targets
    are named "pxr::arch", "pxr::tf", and so on.
    
    This structure allows find_package(pxr) to find a custom usd
    installation by when its install prefix has been added to the
    CMAKE_PREFIX_PATH environment variable.
    
    Ref: https://cmake.org/cmake/help/latest/command/find_package.html#config-mode-search-procedure

--- cmake/defaults/Options.cmake
+++ cmake/defaults/Options.cmake
@@ -107,6 +107,16 @@ set(PXR_INSTALL_LOCATION ""
     "Intended final location for plugin resource files."
 )
 
+if (WIN32)
+    set(pxr_cmake_dir "cmake")
+else()
+    set(pxr_cmake_dir "${CMAKE_INSTALL_LIBDIR}/cmake/pxr")
+endif()
+set(PXR_CMAKE_INSTALL_DESTINATION "${pxr_cmake_dir}"
+    CACHE
+    STRING
+    "Destination within the installation for CMake package files")
+
 set(PXR_OVERRIDE_PLUGINPATH_NAME ""
     CACHE
     STRING
