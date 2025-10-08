e071edefbde5cfa99d660628d2dd4299ebab7073
David Aguilar <davvid@gmail.com>
2025-10-08 08:35:42 -0700

    cmake: add support for staged install dirs
    Add DESTDIR support to the documentation steps.

--- cmake/macros/Public.cmake
+++ cmake/macros/Public.cmake
@@ -1392,9 +1392,9 @@ function(pxr_build_python_documentation)
             COMMAND ${PYTHON_EXECUTABLE} ${CONVERT_DOXYGEN_TO_PYTHON_DOCS_SCRIPT} \
                 --package pxr --module ${pxrPythonModulesStr} \
                 --inputIndex ${BUILT_XML_DOCS}/index.xml \
-                --pythonPath ${_pythonInstallDirAbs} \
+                --pythonPath \$ENV{DESTDIR}${_pythonInstallDirAbs} \
                 ${DLL_PATH_FLAG} \
-                --output ${INSTALL_PYTHON_PXR_ROOT})
+                --output \$ENV{DESTDIR}${INSTALL_PYTHON_PXR_ROOT})
         if (NOT \${convert_doxygen_return_code} EQUAL \"0\")
             message( FATAL_ERROR \"Error generating python docstrings - ${CONVERT_DOXYGEN_TO_PYTHON_DOCS_SCRIPT} return code: \${convert_doxygen_return_code} \")
         endif()
