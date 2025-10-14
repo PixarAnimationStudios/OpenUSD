bd75d76d25415aba59b6b678b68898282e85b96a
Miguel Hernandez Sanchez <miguelh@nvidia.com>
2025-10-14 10:24:51 +0200

    Address comments

--- cmake/macros/Public.cmake
+++ cmake/macros/Public.cmake
@@ -1202,20 +1202,26 @@ function(pxr_toplevel_epilogue)
             )
         endif()
 
-        # When building a monolithic library (static or shared) we want all API functions to be
-        # exported. So add FOO_EXPORTS=1 for every library in PXR_OBJECT_LIBS,
-        # where FOO is the uppercase version of the library name, to every
-        # library in PXR_OBJECT_LIBS.
+        # When building a monolithic library (static or shared) we want all
+        # API functions to be exported. So add FOO_EXPORTS=1 for every
+        #library in PXR_OBJECT_LIBS, where FOO is the uppercase version
+        #of the library name, to every library in PXR_OBJECT_LIBS.
         set(exports "")
         foreach(lib ${PXR_OBJECT_LIBS})
             string(TOUPPER ${lib} uppercaseName)
             list(APPEND exports "${uppercaseName}_EXPORTS=1")
         endforeach()
 
+        if (TARGET python)
+            # The boost python target uses a different export macro so
+            # add that as well.
+            list(APPEND exports "PXR_BOOST_PYTHON_SOURCE=1")
+        endif()
+
         foreach(lib ${PXR_OBJECT_LIBS})
             target_compile_definitions(${lib} PRIVATE ${exports})
         endforeach()
-    
+
         if(BUILD_SHARED_LIBS)
             target_link_libraries(usd_m
                 PUBLIC
