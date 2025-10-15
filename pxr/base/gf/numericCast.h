be75edf23ec766387b71c59aadb12847ed82a87c
Joshua Miller <jomiller@nvidia.com>
2025-10-15 12:23:11 -0700

    Simplify bool -> int conversion in GfNumericCast

--- pxr/base/gf/numericCast.h
+++ pxr/base/gf/numericCast.h
@@ -103,7 +103,7 @@ GfNumericCast(From from, GfNumericCastFailureType *failType = nullptr)
     // bool -> int
     if constexpr (std::is_same_v<From, bool> && std::is_integral_v<To>) {
         // No need to range check bool to int
-        return from ? static_cast<To>(1) : static_cast<To>(0);
+        return To{from};
     }
     // int -> bool
     else if constexpr (std::is_integral_v<From> && std::is_same_v<To, bool>) {
