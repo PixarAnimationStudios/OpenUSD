23977377cd80ca9f6eecd4ce716e1b770f6d1ee5
Tom <tcauchois@gmail.com>
2026-01-13 11:54:12 -0800

    Add HdPrimTypeTokens->particleField

--- pxr/imaging/hd/tokens.h
+++ pxr/imaging/hd/tokens.h
@@ -276,6 +276,7 @@ PXR_NAMESPACE_OPEN_SCOPE
     (nurbsPatch)                                \
     (basisCurves)                               \
     (nurbsCurves)                               \
+    (particleField)                             \
     (plane)                                     \
     (points)                                    \
     (sphere)                                    \
