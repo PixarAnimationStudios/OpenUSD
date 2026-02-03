d0adea2cc00ff398b34ba5cbf8073e814886678d
Rob Pieké <robp@sidefx.com>
2026-02-03 12:17:57 +0000

    Initial commit of a basic API schema adapter for the MotionAPI, addressing issue #3970

---
+++ pxr/usdImaging/usdImaging/motionAPIAdapter.h
@@ -0,0 +1,37 @@
+//
+// Copyright 2026 Pixar
+//
+// Licensed under the terms set forth in the LICENSE.txt file available at
+// https://openusd.org/license.
+//
+#ifndef PXR_USD_IMAGING_USD_IMAGING_MOTION_API_ADAPTER_H
+#define PXR_USD_IMAGING_USD_IMAGING_MOTION_API_ADAPTER_H
+
+#include "pxr/usdImaging/usdImaging/apiSchemaAdapter.h"
+
+PXR_NAMESPACE_OPEN_SCOPE
+
+class UsdImagingMotionAPIAdapter : public UsdImagingAPISchemaAdapter
+{
+public:
+    using BaseAdapter = UsdImagingAPISchemaAdapter;
+
+    USDIMAGING_API
+    HdContainerDataSourceHandle GetImagingSubprimData(
+        UsdPrim const& prim,
+        TfToken const& subprim,
+        TfToken const& appliedInstanceName,
+        const UsdImagingDataSourceStageGlobals &stageGlobals) override;
+
+    USDIMAGING_API
+    HdDataSourceLocatorSet InvalidateImagingSubprim(
+        UsdPrim const& prim,
+        TfToken const& subprim,
+        TfToken const& appliedInstanceName,
+        TfTokenVector const& properties,
+        UsdImagingPropertyInvalidationType invalidationType) override;
+};
+
+PXR_NAMESPACE_CLOSE_SCOPE
+
+#endif // PXR_USD_IMAGING_USD_IMAGING_MOTION_API_ADAPTER_H
