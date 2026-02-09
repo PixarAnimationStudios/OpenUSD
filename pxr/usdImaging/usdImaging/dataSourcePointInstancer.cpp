dd7e3074d05c97cb8c9e3a4b7bd4725c2a5a1a6b
Chris Rydalch <crydalch@gmail.com>
2026-02-09 12:07:14 -0600

    Makes the 'ids' for a UsdGeomPointInstancer available for shading variation.

--- pxr/usdImaging/usdImaging/dataSourcePointInstancer.cpp
+++ pxr/usdImaging/usdImaging/dataSourcePointInstancer.cpp
@@ -204,6 +204,11 @@ _GetCustomPrimvarMappings(const UsdPrim &usdPrim)
             HdTokens->angularVelocities,
             UsdGeomTokens->angularVelocities,
             HdPrimvarSchemaTokens->instance
+        },
+        {
+            HdTokens->ids,
+            UsdGeomTokens->ids,
+            HdPrimvarSchemaTokens->instance
         }
     };
 
