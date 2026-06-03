PixarAnimationStudios/OpenUSD

commit 810f061928b99ad6d06fabf66c654f173ac4c281

For geom subsets. Adds functions to HdStMesh to process geom subsets, including creation of geom subset draw items, which are stored in HdRepr after the "main" draw items.
These geom subset draw items undergo much of the same process as the main draw items, such as setting their material shader and geometric shader, but require some additional handling. The geom subset draw items will generally use the same shared data as the rest of the draw items except for their topology BAR, which will be unique to each geom subset draw item.

Because each geom subset draw item will have a different topology BAR, we additionally need to adjust the topology drawing coord index for each geom subset draw item, as well as the instance primvar drawing coord index for all draw items.

(Internal change: 2181788)

pxr/imaging/hdSt/mesh.h
@@ -103,10 +103,17 @@ protected:
                 bool * isFirstInstance,
                 HdStResourceRegistrySharedPtr const &resourceRegistry) const;
 
-    bool _UseQuadIndices(const HdRenderIndex &renderIndex,
-                         HdSt_MeshTopologySharedPtr const & topology) const;
+    bool _MaterialHasPtex(const HdRenderIndex &renderIndex, 
+                          const SdfPath &materialId) const;
 
-    bool _UseLimitRefinement(const HdRenderIndex &renderIndex) const;
+    bool _UseQuadIndices(const HdRenderIndex &renderIndex,
+                         const HdSt_MeshTopologySharedPtr &topology) const;
+
+    bool _MaterialHasLimitSurface(const HdRenderIndex &renderIndex, 
+                                  const SdfPath &materialId) const;
+
+    bool _UseLimitRefinement(const HdRenderIndex &renderIndex,
+                             const HdMeshTopology &topology) const;
 
     bool _UseSmoothNormals(HdSt_MeshTopologySharedPtr const& topology) const;
 
@@ -116,14 +123,18 @@ protected:
                          HdRenderParam *renderParam,
                          HdStDrawItem *drawItem,
                          HdDirtyBits *dirtyBits,
+                         const TfToken &reprToken,
+                         const HdReprSharedPtr &repr,
                          const HdMeshReprDesc &desc,
                          bool requireSmoothNormals,
-                         bool requireFlatNormals);
+                         bool requireFlatNormals,
+                         int geomSubsetDescIndex);
 
     void _UpdateDrawItemGeometricShader(HdSceneDelegate *sceneDelegate,
                                         HdRenderParam *renderParam,
                                         HdStDrawItem *drawItem,
-                                        const HdMeshReprDesc &desc);
+                                        const HdMeshReprDesc &desc,
+                                        const SdfPath &materialId);
 
     void _UpdateShadersForAllReprs(HdSceneDelegate *sceneDelegate,
                                    HdRenderParam *renderParam,
@@ -134,8 +145,19 @@ protected:
                            HdRenderParam *renderParam,
                            HdStDrawItem *drawItem,
                            HdDirtyBits *dirtyBits,
-                           const HdMeshReprDesc &desc);
+                           const TfToken &reprToken,
+                           const HdReprSharedPtr &repr,
+                           const HdMeshReprDesc &desc,
+                           int geomSubsetDescIndex);
 
+    void _UpdateDrawItemsForGeomSubsets(HdSceneDelegate *sceneDelegate,
+                                        HdRenderParam *renderParam,
+                                        HdStDrawItem *drawItem,
+                                        const TfToken &reprToken,
+                                        const HdReprSharedPtr &repr,
+                                        const HdGeomSubsets &geomSubsets,
+                                        size_t oldNumGeomSubsets);
+    
     void _CreateTopologyRangeForGeomSubset(
         HdStResourceRegistrySharedPtr resourceRegistry,
         HdChangeTracker &changeTracker, 
@@ -147,31 +169,42 @@ protected:
         HdBufferSourceSharedPtr geomSubsetFaceIndicesHelperSource,
         const VtIntArray &faceIndices,
         bool refined);
-        
-    void _PopulateAdjacency(
-        HdStResourceRegistrySharedPtr const &resourceRegistry);
 
     void _GatherFaceVaryingTopologies(HdSceneDelegate *sceneDelegate,
+                                      const HdReprSharedPtr &repr,
+                                      const HdMeshReprDesc &desc,
                                       HdStDrawItem *drawItem,
+                                      int geomSubsetDescIndex,
                                       HdDirtyBits *dirtyBits,
                                       const SdfPath &id,
                                       HdSt_MeshTopologySharedPtr topology);
-
+    
+    void _PopulateAdjacency(
+        HdStResourceRegistrySharedPtr const &resourceRegistry);
+        
     void _PopulateVertexPrimvars(HdSceneDelegate *sceneDelegate,
                                  HdRenderParam *renderParam,
+                                 const HdReprSharedPtr &repr,
+                                 const HdMeshReprDesc &desc,
                                  HdStDrawItem *drawItem,
+                                 int geomSubsetDescIndex,
                                  HdDirtyBits *dirtyBits,
                                  bool requireSmoothNormals);
 
     void _PopulateFaceVaryingPrimvars(HdSceneDelegate *sceneDelegate,
                                       HdRenderParam *renderParam,
+                                      const HdReprSharedPtr &repr,
+                                      const HdMeshReprDesc &desc,
                                       HdStDrawItem *drawItem,
-                                      HdDirtyBits *dirtyBits,
-                                      const HdMeshReprDesc &desc);
+                                      int geomSubsetDescIndex,
+                                      HdDirtyBits *dirtyBits);
 
     void _PopulateElementPrimvars(HdSceneDelegate *sceneDelegate,
                                   HdRenderParam *renderParam,
+                                  const HdReprSharedPtr &repr,
+                                  const HdMeshReprDesc &desc,
                                   HdStDrawItem *drawItem,
+                                  int geomSubsetDescIndex,
                                   HdDirtyBits *dirtyBits,
                                   bool requireFlatNormals);
 
@@ -275,7 +308,10 @@ private:
     enum DrawingCoord {
         HullTopology = HdDrawingCoord::CustomSlotsBegin,
         PointsTopology,
-        InstancePrimvar // has to be at the very end
+        FreeSlot // If the mesh topology has geom subsets, we might place
+                 // them here as geom subsets are processed before instance 
+                 // primvars. The instance primvars will follow after. If there
+                 // are no geom subsets, instance primvars start here.
     };
 
     enum DirtyBits : HdDirtyBits {

pxr/imaging/hd/repr.cpp
@@ -123,7 +123,7 @@ HdReprSelector::operator[](size_t topologyIndex) const
     }
 }
 
-HdRepr::HdRepr() = default;
+HdRepr::HdRepr() : _geomSubsetsStart(0) {};
 
 HdRepr::~HdRepr() = default;
 

pxr/imaging/hd/repr.h
@@ -170,8 +170,11 @@ public:
     }
 
     /// Transfers ownership of a draw item to this repr.
+    /// Do not use for adding geom subset draw items.
     void AddDrawItem(std::unique_ptr<HdDrawItem> &&item) {
-        _drawItems.push_back(std::move(item));
+        _drawItems.insert(_drawItems.begin() + _geomSubsetsStart, 
+            std::move(item));
+        _geomSubsetsStart++;
     }
 
     /// Returns the draw item at the requested index.
@@ -182,13 +185,55 @@ public:
         return _drawItems[index].get();
     }
 
+    /// HdRepr can hold geom subset draw items, which are unique in that they
+    /// not created at the time the repr is created, but instead when populating
+    /// the mesh topology of a mesh rprim. The number of geom subset draw items
+    /// in a repr can change over time. 
+    /// We make some assumptions when using these geom subset related functions.
+    /// We assume the geom subset draw items will only be added (or cleared) 
+    /// after all of the main draw items for a repr have been added.
+    /// We also assume that the geom subset draw items for a repr desc are all
+    /// added one after the other before moving onto the next repr desc. 
+    /// Thus the order of draw items in the _drawItems member might go something
+    /// like (assuming three geom subsets in this example):
+    /// [ main DI for desc 1, main DI for desc 2, GS1 DI for desc 1, 
+    ///   GS2 DI for desc 1, GS3 DI for desc 1, GS1 DI for desc 2, 
+    ///   GS2 DI for desc 2, GS3 DI for desc 2 ]
+    /// It is also possible for there to exist a main draw item for a particular
+    /// repr desc but no geom subsets for that repr desc, while having geom
+    /// subsets exist for a different repr desc.
+
+    /// Transfers ownership of a draw item to this repr.
+    /// To be used only for geom subset draw items.
+    void AddGeomSubsetDrawItem(std::unique_ptr<HdDrawItem> &&item) {
+        _drawItems.push_back(std::move(item));
+    }
+
+    /// Utility similar to GetDrawItem for getting geom subset draw items.
+    HdDrawItem* GetDrawItemForGeomSubset(size_t reprDescIndex, 
+        size_t numGeomSubsets, size_t geomSubsetIndex) const {        
+        return _drawItems[_geomSubsetsStart + reprDescIndex * numGeomSubsets + 
+            geomSubsetIndex].get();
+    }
+
+    /// Removes all of the geom subset draw items from the repr.
+    void ClearGeomSubsetDrawItems() {
+        _drawItems.erase(
+            _drawItems.begin() + _geomSubsetsStart, _drawItems.end());
+    }
+
 private:
     // Noncopyable
     HdRepr(const HdRepr&) = delete;
     HdRepr& operator=(const HdRepr&) = delete;
 
 private:
+    // Contains normal draw items first, potentially followed by geom subset
+    // draw items
     DrawItemUniquePtrVector _drawItems;
+
+    // Index into _drawItems indicating where the geom subset draw items begin.
+    size_t _geomSubsetsStart; 
 };
 
 

pxr/imaging/hdSt/drawItemsCache.cpp
@@ -61,6 +61,15 @@ _GetMaterialTagsVersion(HdRenderIndex *renderIndex)
     return size_t(stRenderParam->GetMaterialTagsVersion());
 }
 
+static size_t
+_GetGeomSubsetDrawItemsVersion(HdRenderIndex *renderIndex)
+{
+    HdStRenderParam *stRenderParam = static_cast<HdStRenderParam*>(
+        renderIndex->GetRenderDelegate()->GetRenderParam());
+
+    return size_t(stRenderParam->GetGeomSubsetDrawItemsVersion());
+}
+
 //------------------------------------------------------------------------------
 
 HdDrawItemConstPtrVectorSharedPtr
@@ -159,10 +168,14 @@ HdSt_DrawItemsCache::_IsCacheEntryStale(
         val.renderTagsVersion != tracker.GetRenderTagVersion();
     const bool materialTagsVersionChanged =
         val.materialTagsVersion != _GetMaterialTagsVersion(renderIndex);
+    const bool geomSubsetDrawItemsChanged =
+        val.geomSubsetDrawItemsVersion != _GetGeomSubsetDrawItemsVersion(
+            renderIndex);
 
     return collectionVersionChanged ||
            renderTagsVersionChanged ||
-           materialTagsVersionChanged;
+           materialTagsVersionChanged ||
+           geomSubsetDrawItemsChanged;
 }
 
 void
@@ -180,6 +193,8 @@ HdSt_DrawItemsCache::_UpdateCacheEntry(
         tracker.GetCollectionVersion(collection.GetName());
     val->renderTagsVersion = tracker.GetRenderTagVersion();
     val->materialTagsVersion = _GetMaterialTagsVersion(renderIndex);
+    val->geomSubsetDrawItemsVersion = _GetGeomSubsetDrawItemsVersion(
+        renderIndex);
 
     const HdStRenderParam * const renderParam =
         static_cast<HdStRenderParam *>(

pxr/imaging/hdSt/drawItemsCache.h
@@ -121,12 +121,14 @@ private:
         _CacheValue()
         : collectionVersion(0),
           renderTagsVersion(0),
-          materialTagsVersion(0) {}
+          materialTagsVersion(0),
+          geomSubsetDrawItemsVersion(0) {}
         
         HdDrawItemConstPtrVectorSharedPtr drawItems;
         size_t collectionVersion;
         size_t renderTagsVersion;
         size_t materialTagsVersion;
+        size_t geomSubsetDrawItemsVersion;
     };
 
     bool _IsCacheEntryStale(_CacheValue const &val,

pxr/imaging/hdSt/meshTopology.cpp
@@ -142,7 +142,9 @@ HdSt_MeshTopology::HdSt_MeshTopology(
  , _subdivision(nullptr)
  , _osdTopologyBuilder()
  , _osdBaseFaceToRefinedFacesMap()
+ , _nonSubsetFaces(nullptr)
 {
+    SanitizeGeomSubsets();
 }
 
 HdSt_MeshTopology::~HdSt_MeshTopology() = default;
@@ -315,6 +317,68 @@ HdSt_MeshTopology::GetOsdTopologyComputation(SdfPath const &id)
         _subdivision->CreateTopologyComputation(this, id);
     _osdTopologyBuilder = builder; // retain weak ptr
     return builder;
+}
+
+void
+HdSt_MeshTopology::SanitizeGeomSubsets()
+{
+    const HdGeomSubsets &geomSubsets = GetGeomSubsets();
+    if (geomSubsets.empty()) {
+        return;
+    }
+    const size_t numFaces = GetNumFaces();
+
+    // Keep track of faces that are used within the geom subsets
+    std::vector<bool> unusedFaces(numFaces, true);
+    size_t numUnusedFaces = numFaces;
+
+    HdGeomSubsets sanitizedGeomSubsets;
+    for (const HdGeomSubset &geomSubset : geomSubsets) {
+        HdGeomSubset sanitizedGeomSubset = geomSubset;
+
+        // We only care about subsets that will with non-empty indices and
+        // material id
+        const VtIntArray faceIndices = geomSubset.indices;
+        if (!faceIndices.empty() && !geomSubset.materialId.IsEmpty()) {                    
+            VtIntArray sanitizedFaceIndices;
+            for (size_t i = 0; i < faceIndices.size(); ++i) {
+                const int index = faceIndices[i];
+                // Skip out-of-bound face indices.
+                if (index >= (int)numFaces) {
+                    TF_WARN("Geom subset index %d is larger than number of "
+                        "faces (%d), removing.", index, (int)numFaces);
+                    continue;
+                }
+                sanitizedFaceIndices.push_back(index);
+                if (unusedFaces[index]) {
+                    unusedFaces[index] = false;
+                    numUnusedFaces--;
+                } else {
+                    // Warn about duplicated face indices.
+                    TF_WARN("Face index %d is repeated between geom subsets", 
+                        index);;
+                }
+            }
+            sanitizedGeomSubset.indices = sanitizedFaceIndices;
+            sanitizedGeomSubsets.push_back(sanitizedGeomSubset);
+        }
+    }
+
+    _nonSubsetFaces = std::make_unique<std::vector<int>>();
+    _nonSubsetFaces->resize(numUnusedFaces);
+
+    if (numUnusedFaces) {
+        size_t count = 0;
+        for (size_t i = 0; i < unusedFaces.size() && count < numUnusedFaces; 
+             ++i) {
+            if (unusedFaces[i]) {
+                (*_nonSubsetFaces)[count] = i;
+                count++;
+            }
+        }
+    }
+    
+    SetGeomSubsets(sanitizedGeomSubsets);
 }
 
 HdBufferSourceSharedPtr

pxr/imaging/hdSt/meshTopology.h
@@ -225,6 +225,10 @@ public:
     ///
     /// \name Geom Subsets
     /// @{
+    
+    /// Processes geom subsets to remove those with empty indices or empty 
+    /// material id. Will initialize _nonSubsetFaces if there are geom subsets.
+    void SanitizeGeomSubsets();
 
     /// Returns the indices subset computation for unrefined indices.
     HdBufferSourceSharedPtr GetIndexSubsetComputation(
@@ -252,6 +256,10 @@ public:
     ///
     /// \name Face-varying Topologies
     /// @{
+    /// Returns the face indices of faces not used in any geom subsets.
+    std::vector<int> const *GetNonSubsetFaces() const {
+        return _nonSubsetFaces.get();
+    }
 
     /// Sets the face-varying topologies.
     void SetFvarTopologies(std::vector<VtIntArray> const &fvarTopologies) {
@@ -281,6 +289,10 @@ private:
     HdBufferSourceWeakPtr _osdBaseFaceToRefinedFacesMap;
 
     std::vector<VtIntArray> _fvarTopologies;
+
+    // When using geom subsets, the indices of faces that are not contained
+    // within the geom subsets. Populated by SanitizeGeomSubsets().
+    std::unique_ptr<std::vector<int>> _nonSubsetFaces;
 
     // Must be created through factory
     explicit HdSt_MeshTopology(

commit a6b8c7a34bb6bd2e881c453436bed02584344443

hdSt: Fix missing fvarIndices buffers on geom-subset draw items.
When a subdivided mesh has both geom subsets (materialBind family) and
multiple face-varying primvars whose index buffers differ (multiple
fv-topology channels), the geom-subsets path of HdStMesh::_PopulateTopology
was only adding the LAST channel's fvarIndices buffer to each subset
draw item's topology BAR.

The local fvarIndicesSource variable in the channel-walk loop was
overwritten each iteration and then passed by value to
_CreateTopologyRangeForGeomSubset, which only ever inserted that single
source into the BAR.  Meanwhile codeGen continued to emit
HdGet_fvarIndices<N>() calls for every channel a primvar uses, so any
draw item whose primvar lived on a missing channel produced a Metal
shader compile error of the form:

  error: use of undeclared identifier 'HdGet_fvarIndices0';
         did you mean 'HdGet_fvarIndices1'?

Plumb a HdBufferSourceSharedPtrVector through
_CreateTopologyRangeForGeomSubset so that every channel's indices source
(and its chained fvarPatchParam<N> buffer, which
HdSt_IndexSubsetComputation already preserves) ends up in the topology
BAR for both the original draw item and each geom subset draw item.
The non-subset path (mesh.cpp:825-832) already collected all sources
correctly and is unchanged.

Bug introduced in commit 810f061928 ("For geom subsets..."), Aug 5 2021.

pxr/imaging/hdSt/mesh.cpp
@@ -895,7 +895,7 @@ HdStMesh::_PopulateTopology(HdSceneDelegate *sceneDelegate,
         } else {
             // Geom subsets case
             HdBufferSourceSharedPtr indicesSource;
-            HdBufferSourceSharedPtr fvarIndicesSource;
+            HdBufferSourceSharedPtrVector fvarIndicesSources;
 
             bool refined = false;
             bool quadrangulated = false;
@@ -910,9 +910,10 @@ HdStMesh::_PopulateTopology(HdSceneDelegate *sceneDelegate,
                     for (size_t i = 0; 
                            i < _fvarTopologyTracker->GetNumTopologies(); 
                             ++i) {
-                        fvarIndicesSource = 
+                        HdBufferSourceSharedPtr fvarIndicesSource = 
                             _topology->GetOsdFvarIndexBuilderComputation(i);
                         resourceRegistry->AddSource(fvarIndicesSource);
+                        fvarIndicesSources.push_back(fvarIndicesSource);
                     }
                 }
 
@@ -954,7 +955,7 @@ HdStMesh::_PopulateTopology(HdSceneDelegate *sceneDelegate,
                 _topology->GetNonSubsetFaces();
             _CreateTopologyRangeForGeomSubset(resourceRegistry, changeTracker, 
                 renderParam, drawItem, indexToken, indicesSource,
-                fvarIndicesSource, geomSubsetFaceIndicesHelperSource,
+                fvarIndicesSources, geomSubsetFaceIndicesHelperSource,
                 VtIntArray(nonSubsetFaces->begin(), nonSubsetFaces->end()), 
                 refined);
 
@@ -967,7 +968,7 @@ HdStMesh::_PopulateTopology(HdSceneDelegate *sceneDelegate,
                         geomSubsetDescIndex, numGeomSubsets, i));
                 _CreateTopologyRangeForGeomSubset(resourceRegistry, 
                     changeTracker, renderParam, subsetDrawItem, indexToken, 
-                    indicesSource, fvarIndicesSource, 
+                    indicesSource, fvarIndicesSources, 
                     geomSubsetFaceIndicesHelperSource, geomSubset.indices, 
                     refined);
             }
@@ -983,7 +984,7 @@ void HdStMesh::_CreateTopologyRangeForGeomSubset(
     HdStDrawItem *drawItem, 
     const TfToken &indexToken,
     HdBufferSourceSharedPtr indicesSource, 
-    HdBufferSourceSharedPtr fvarIndicesSource, 
+    HdBufferSourceSharedPtrVector const &fvarIndicesSources, 
     HdBufferSourceSharedPtr geomSubsetFaceIndicesHelperSource,
     const VtIntArray &faceIndices,
     bool refined)
@@ -1013,11 +1014,14 @@ void HdStMesh::_CreateTopologyRangeForGeomSubset(
                     indicesSource, geomSubsetFaceIndicesSource);
             sources.push_back(subsetSource);
 
-            if (fvarIndicesSource) {
-                HdBufferSourceSharedPtr fvarSubsetSource = 
-                    _topology->GetRefinedIndexSubsetComputation(
-                        fvarIndicesSource, geomSubsetFaceIndicesSource);
-                sources.push_back(fvarSubsetSource);
+            if (fvarIndicesSources.empty() == false) {
+                for (HdBufferSourceSharedPtr const &fvarIndicesSource :
+                        fvarIndicesSources) {
+                    HdBufferSourceSharedPtr fvarSubsetSource = 
+                        _topology->GetRefinedIndexSubsetComputation(
+                            fvarIndicesSource, geomSubsetFaceIndicesSource);
+                    sources.push_back(fvarSubsetSource);
+                }
             }
         } else {
             HdBufferSourceSharedPtr subsetSource = 

pxr/imaging/hdSt/mesh.h
@@ -19,6 +19,7 @@
 #include "pxr/base/vt/array.h"
 
 #include <memory>
+#include <vector>
 
 PXR_NAMESPACE_OPEN_SCOPE
 
@@ -29,6 +30,7 @@ class HdSceneDelegate;
 using HdSt_VertexAdjacencyBuilderSharedPtr =
         std::shared_ptr<class HdSt_VertexAdjacencyBuilder>;
 using HdBufferSourceSharedPtr = std::shared_ptr<class HdBufferSource>;
+using HdBufferSourceSharedPtrVector = std::vector<HdBufferSourceSharedPtr>;
 using HdSt_MeshTopologySharedPtr = std::shared_ptr<class HdSt_MeshTopology>;
 
 using HdStResourceRegistrySharedPtr =
@@ -156,7 +158,7 @@ protected:
         HdStDrawItem *drawItem, 
         const TfToken &indexToken,
         HdBufferSourceSharedPtr indicesSource, 
-        HdBufferSourceSharedPtr fvarIndicesSource, 
+        HdBufferSourceSharedPtrVector const &fvarIndicesSources, 
         HdBufferSourceSharedPtr geomSubsetFaceIndicesHelperSource,
         const VtIntArray &faceIndices,
         bool refined);