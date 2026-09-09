72339674892affe0e134a0c1dded1a28624d3f49
Aleksi Sapon <aleksi.sapon@autodesk.com>
2026-09-09 09:54:14 -0700

    Autodesk: [hgiMetal] Disable depth-slope for points and lines
    `slopeScale` on points seems to cause incorrect depth values on some Metal implementations.
    Metal's slopeScale depth bias is only meaningful for triangle primitives, so force it to 0.0 for points and lines.
    
    Closes #4193
    
    (Internal change: 2427206)

--- pxr/imaging/hgiMetal/graphicsPipeline.mm
+++ pxr/imaging/hgiMetal/graphicsPipeline.mm
@@ -444,9 +444,19 @@ HgiMetalGraphicsPipeline::BindPipeline(id<MTLRenderCommandEncoder> renderEncoder
     //
     HgiDepthStencilState const & dsState = _descriptor.depthState;
     if (_descriptor.depthState.depthBiasEnabled) {
+        const bool primitiveSupportsDepthBias =
+                !(_descriptor.primitiveType == HgiPrimitiveTypePointList ||
+                  _descriptor.primitiveType == HgiPrimitiveTypeLineList  ||
+                  _descriptor.primitiveType == HgiPrimitiveTypeLineStrip);
+        
+        // Points and lines have no depth gradient, so a slope scaled bias
+        // should have no effect, but instead it causes randomly broken
+        // depth values.
+        const float slopeScale =
+                primitiveSupportsDepthBias ? dsState.depthBiasSlopeFactor : 0.0f;
         [renderEncoder
             setDepthBias: dsState.depthBiasConstantFactor
-              slopeScale: dsState.depthBiasSlopeFactor
+              slopeScale: slopeScale
                    clamp: 0.0f];
     }
 
